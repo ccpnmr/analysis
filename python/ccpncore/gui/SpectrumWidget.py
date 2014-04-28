648719b5a3e186868dbbcbb3f878218927c46e7f
Simon Skinner <ss885@leicester.ac.uk>
2014-04-28 16:48:45 +0000

    mouse commands added to SpectrumWidget.py
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@7621 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpncore/gui/SpectrumWidget.py
+++ python/ccpncore/gui/SpectrumWidget.py
@@ -4,7 +4,7 @@ import numpy as np
 from pyqtgraph.Point import Point
 import pyqtgraph.console
 import sys
-
+import pprint
 
 class SpectrumWidget:
 
@@ -12,10 +12,21 @@ class SpectrumWidget:
 
     pg.setConfigOption('background', 'w')
     pg.setConfigOption('foreground', 'k')
+    # if self.useOpenGL:
+    #   pg.setConfigOption('useOpenGL', 'True')
     self.widget = pg.PlotWidget()
     self.createCrossHair()
     self.viewBox = self.widget.plotItem.vb
+    self.viewBox.setMenuDisabled()
     self.widget.scene().sigMouseMoved.connect(self.mouseMoved)
+    self.widget.scene().sigMouseClicked.connect(self.mouseClicked)
+
+    # self.widget.scene().sigMouseClicked.connect(self.mouseDragged)
+    self.widget.plotItem.axes['left']['item'].hide()
+    self.widget.plotItem.axes['right']['item'].show()
+    self.widget.plotItem.axes['right']['item'].orientation = 'left'
+    self.widget.plotItem.axes['bottom']['item'].orientation = 'top'
+    self.viewBox.mouseDragEvent = self.newMouseDragEvent
 
 
   def createCrossHair(self):
@@ -25,12 +36,76 @@ class SpectrumWidget:
     self.widget.addItem(self.hLine)
 
   def mouseMoved(self, event):
-    position = event  ## using signal proxy turns original arguments into a tuple
+    position = event
     if self.widget.sceneBoundingRect().contains(position):
         mousePoint = self.viewBox.mapSceneToView(position)
         self.vLine.setPos(mousePoint.x())
         self.hLine.setPos(mousePoint.y())
 
+
+  def mouseClicked(self, event):
+
+    if event.button() == QtCore.Qt.LeftButton and not event.modifiers():
+      event.accept()
+      print("leftclick")
+
+    elif (event.button() == QtCore.Qt.LeftButton) and (event.modifiers() & QtCore.Qt.ControlModifier):
+      position = event.scenePos()  ## using signal proxy turns original arguments into a tuple
+      mousePoint = self.viewBox.mapSceneToView(position)
+      print(mousePoint)
+
+    elif (event.button() == QtCore.Qt.LeftButton) and (event.modifiers() & QtCore.Qt.ShiftModifier):
+      print('Add Select')
+
+    elif event.button() == QtCore.Qt.MiddleButton and not event.modifiers():
+      event.accept()
+      print('Pick and Assign')
+
+    elif event.button() == QtCore.Qt.RightButton and not event.modifiers():
+      event.accept()
+      print('Context Menu to be activated here')
+
+
+
+  def newMouseDragEvent(self, event):
+    if (event.button() == QtCore.Qt.RightButton) and (event.modifiers() & QtCore.Qt.ShiftModifier) and not (
+              event.modifiers() & QtCore.Qt.ControlModifier):
+      print("RightDrag + Shift")
+      if event.isStart():
+        position = event.buttonDownPos()
+        print("start ",position)
+
+      elif event.isFinish():
+        position = event.pos()
+        print("end ",position)
+
+      event.accept()
+
+    elif (event.button() == QtCore.Qt.LeftButton) and (event.modifiers()
+                                                       & QtCore.Qt.ControlModifier) and (
+              event.modifiers() & QtCore.Qt.ShiftModifier):
+        # Pick in area
+      print('LeftDrag + Control + Shift')
+      if event.isStart():
+        position = event.buttonDownPos()
+        print("start ",position)
+      elif event.isFinish():
+        position = event.pos()
+        print("end ",position)
+      event.accept()
+
+    elif (event.button() == QtCore.Qt.LeftButton) and (event.modifiers() & QtCore.Qt.ShiftModifier) and not (
+              event.modifiers() & QtCore.Qt.ControlModifier):
+       # Add select area
+      print('LeftDrag + Shift')
+      if event.isStart():
+        position = event.buttonDownPos()
+        print("start ",position)
+      elif event.isFinish():
+        position = event.pos()
+        print("end ",position)
+
+      event.accept()
 
 
 ###For testing

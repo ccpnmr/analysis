540c035981146634c9d7af3309707d75e32f87ec
Wayne Boucher <wb104@cam.ac.uk>
2014-05-15 13:07:57 +0000

    Add more tests
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@7653 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpn/testing/Test_Project.py
+++ python/ccpn/testing/Test_Project.py
@@ -2,16 +2,33 @@
 # print('sys.path=',sys.path)
 from ccpn.testing.Testing import Testing
 
+from ccpncore.api.ccp.nmr.Nmr import NmrProject
+
 class OpenProjectTest(Testing):
 
   def __init__(self, *args, **kw):
     Testing.__init__(self, 'CcpnCourse1a', *args, **kw)
 
+  def test_id(self):
+    print(self.project.id)
+    
+  def test_nmrproject_not_none(self):
+      assert self.project.nmrProject is not None
+
+  def test_project_is_nmrproject(self):
+    assert isinstance(self.project.nmrProject, NmrProject)
+    
   def test_name(self):
-    #assert self.project.name == 'CcpnCourse1a'
-    assert self.project.name == 'CcpnDemo001'
+    assert self.project.name == self.project.nmrProject.name
 
   def test_spectra(self):
-    print('number of spectra = %d' % len(self.project.spectra))
+    assert len(self.project.spectra) == 3
+  
+  def test_chains(self):
+    assert len(self.project.chains) == 0
+
+  def test_atoms(self):
+    assert len(self.project.atoms) == 0
+
 
 

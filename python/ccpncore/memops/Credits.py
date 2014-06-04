ed500e76f1985a5372fe617d5d2884d6900c714e
Rasmus Fogh <rhf22@cam.ac.uk>
2014-06-04 17:13:10 +0000

    Further fixes to Standard file header
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@7686 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpncore/memops/Credits.py
+++ python/ccpncore/memops/Credits.py
@@ -1,12 +1,28 @@
-"""Module Documentation here"""
-__copyright__ = "Copyright (C) CCPN project 2014 - $Date$"
-__license__ = "CCPN license. See ccpncore.memops.Credits.CcpnLicense for license text"
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - $Date$"
 __credits__ = "Wayne Boucher, Rasmus H Fogh, Simon Skinner, Geerten Vuister"
-__reference__ = "For publications, please reference ccpncore.memops.Credits.CcpNmrReference"
+__license__ = ("CCPN license. See www.ccpn.ac.uk/license"
+               "or ccpncore.memops.Credits.CcpnLicense for license text")
+__reference__ = ("For publications, please use reference from www.ccpn.ac.uk/license"
+                 " or ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
 # Last code modification:
+#=========================================================================================
 __author__ = "$Author$"
 __date__ = "$Date$"
 __version__ = "$Revision$"
+
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+
 
 
 # Please use this reference for use of CcpNmr suite software and other CCPN applications

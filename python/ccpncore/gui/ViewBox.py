143ffa120f3270890dfa0132941c75ebdf660f2d
Simon Skinner <ss885@leicester.ac.uk>
2014-06-17 16:36:17 +0000

    check in viewbox
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@7701 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ python/ccpncore/gui/ViewBox.py
@@ -0,0 +1,105 @@
+
+import pyqtgraph as pg
+from PySide import QtCore
+from pyqtgraph.Point import Point
+
+class ViewBox(pg.ViewBox):
+
+  def __init__(self, *args, **kwds):
+        pg.ViewBox.__init__(self, *args, **kwds)
+        self.setMenuDisabled()
+
+  def mouseClickEvent(self, event):
+
+    if event.button() == QtCore.Qt.LeftButton and not event.modifiers():
+      event.accept()
+      print("Left Click Event")
+
+    elif (event.button() == QtCore.Qt.LeftButton) and (
+              event.modifiers() & QtCore.Qt.ControlModifier) and not (
+    event.modifiers() & QtCore.Qt.ShiftModifier):
+      position = event.scenePos()
+      mousePoint = self.mapSceneToView(position)
+      print(mousePoint)
+
+    elif (event.button() == QtCore.Qt.LeftButton) and (
+              event.modifiers() & QtCore.Qt.ShiftModifier) and not (
+    event.modifiers() & QtCore.Qt.ControlModifier):
+      print('Add Select')
+
+    elif event.button() == QtCore.Qt.MiddleButton and not event.modifiers():
+      event.accept()
+      print('Pick and Assign')
+
+    elif event.button() == QtCore.Qt.RightButton and not event.modifiers():
+      event.accept()
+      print('Context Menu to be activated here')
+
+    elif event.button() == QtCore.Qt.RightButton and (event.modifiers() & QtCore.Qt.ShiftModifier):
+      event.accept()
+      self.autoRange()
+
+      print('Context Menu to be activated here')
+
+    if event.double():
+      event.accept()
+      print("Double Click event")
+
+
+
+  def mouseDragEvent(self, event, axis=None):
+
+    if event.button() == QtCore.Qt.LeftButton and not event.modifiers():
+      pg.ViewBox.mouseDragEvent(self, event)
+
+
+    elif (event.button() == QtCore.Qt.RightButton) and (
+              event.modifiers() & QtCore.Qt.ShiftModifier) and not (
+              event.modifiers() & QtCore.Qt.ControlModifier) or event.button() == QtCore.Qt.MidButton:
+      print("Zoom into area")
+      if event.isFinish():  ## This is the final move in the drag; change the view scale now
+        print("finish")
+        self.rbScaleBox.hide()
+        ax = QtCore.QRectF(Point(event.buttonDownPos(event.button())), Point(event.pos()))
+        ax = self.childGroup.mapRectFromParent(ax)
+        self.showAxRect(ax)
+        self.axHistoryPointer += 1
+        self.axHistory = self.axHistory[:self.axHistoryPointer] + [ax]
+      else:
+        self.updateScaleBox(event.buttonDownPos(), event.pos())
+
+      event.accept()
+
+    elif (event.button() == QtCore.Qt.LeftButton) and (
+              event.modifiers() & QtCore.Qt.ControlModifier) and (
+              event.modifiers() & QtCore.Qt.ShiftModifier):
+        # Pick in area
+      print('LeftDrag + Control + Shift')
+
+      if event.isFinish():
+
+        startPosition = event.buttonDownPos()
+        endPosition = event.pos()
+        print(startPosition,endPosition)
+
+      event.accept()
+
+
+
+    elif (event.button() == QtCore.Qt.LeftButton) and (
+              event.modifiers() & QtCore.Qt.ShiftModifier) and not (
+              event.modifiers() & QtCore.Qt.ControlModifier):
+       # Add select area
+      print('LeftDrag + Shift')
+      if event.isStart():
+        startPosition = event.buttonDownPos()
+        print("start ",startPosition)
+      elif event.isFinish():
+        endPosition = event.pos()
+        print("end ",endPosition)
+
+      event.accept()
+    ## above events remove pan abilities from plot window,
+    ## need to re-implement them without changing mouseMode
+    else:
+      event.ignore()

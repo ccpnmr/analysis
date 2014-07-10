9b4e53fad2c15a40de62a1be0fa2aed6e7e6be1f
Rasmus Fogh <rhf22@cam.ac.uk>
2014-07-10 13:52:27 +0000

    Added missing utility class
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@7723 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ python/ccpncore/util/Classes.py
@@ -0,0 +1,32 @@
+"""Utility Classes, independent of data model
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - $Date$"
+__credits__ = "Wayne Boucher, Rasmus H Fogh, Simon Skinner, Geerten Vuister"
+__license__ = ("CCPN license. See www.ccpn.ac.uk/license"
+               "or ccpncore.memops.Credits.CcpnLicense for license text")
+__reference__ = ("For publications, please use reference from www.ccpn.ac.uk/license"
+                 " or ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification:
+#=========================================================================================
+__author__ = "$Author$"
+__date__ = "$Date$"
+__version__ = "$Revision$"
+
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+from collections import namedtuple
+
+class DottedNamedTuple(namedtuple):
+  """namedtuple variant with simplified str function"""
+  def __str__(self):
+    return '.'.join(str(x) for x in self)
+
+NmrAtom = DottedNamedTuple('NmrAtom', ('chain', 'sequence', 'residue', 'atom'))

84e2bb3b5f09fd5cea034935eaed250a372f3fa4
Wayne Boucher <wb104@cam.ac.uk>
2014-09-23 13:12:05 +0000

    Add functions to API
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@7863 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpncore/lib/__init__.py
+++ python/ccpncore/lib/__init__.py
@@ -1,1 +1,7 @@
-pass
+import os
+from ccpncore.util import ApiFunc
+
+thisDirectory = os.path.dirname(__file__)
+for subDirectory in ('memops', 'ccp',):
+  directory = os.path.join(thisDirectory, subDirectory)
+  ApiFunc.addDirectoryFunctionsToApi(directory, rootDirectory=thisDirectory)

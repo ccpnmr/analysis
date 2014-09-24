4588f53283fdfa75869981c9986638be410e63a0
Wayne Boucher <wb104@cam.ac.uk>
2014-09-24 14:53:59 +0000

    Remove ApiFunc call
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@7864 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpncore/lib/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-from ccpncore.util import ApiFunc
-
-thisDirectory = os.path.dirname(__file__)
-for subDirectory in ('memops', 'ccp',):
-  directory = os.path.join(thisDirectory, subDirectory)
-  ApiFunc.addDirectoryFunctionsToApi(directory, rootDirectory=thisDirectory)

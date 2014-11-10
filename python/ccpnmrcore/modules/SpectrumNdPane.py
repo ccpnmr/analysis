6f74cd4f1d38693c5e045167a4b437ea8a11b718
Simon Skinner <ss885@leicester.ac.uk>
2014-11-10 16:09:02 +0000

    added phase correction, still needs testing. Modified traces to be in both dimensions
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@7892 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpnmrcore/modules/SpectrumNdPane.py
+++ python/ccpnmrcore/modules/SpectrumNdPane.py
@@ -31,11 +31,13 @@ from ccpnmrcore.modules.spectrumPane.SpectrumNdItem import SpectrumNdItem
 from ccpnmrcore.modules.spectrumPane.Spectrum1dItem import Spectrum1dItem
 from ccpncore.gui.Menu import Menu
 from ccpncore.gui.Icon import Icon
+from ccpncore.gui.Label import Label
 from ccpn.lib import Spectrum as LibSpectrum
 import numpy
 import math
 from functools import partial
 import pyqtgraph as pg
+from scipy import signal
 
 class SpectrumNdPane(SpectrumPane):
 
@@ -59,6 +61,7 @@ class SpectrumNdPane(SpectrumPane):
     self.traceMarkers = []
     self.traceScale = 1e6
     self.traces = []
+    self.phaseButtonShown = False
 
           
   ##### functions used externally #####
@@ -176,33 +179,27 @@ class SpectrumNdPane(SpectrumPane):
     spectrumItem.widget.setFixedSize(60,30)
     self.spectrumItems.append(spectrumItem)
     self.current.spectrum = spectrum
-
-    # spectrumItem.posColors =
     spectrum.spectrumItem = spectrumItem
 
   def decreaseTraceScale(self):
     self.traceScale*=1.41
-    self.removeItem(self.spectrumItemTrace)
-    self.plotTrace(self.spectrumItemTrace.dim,self.spectrumItemTrace.position)
+    for trace in self.traces:
+      self.removeItem(trace.spectrumItemTrace)
+      self.plotTrace(trace.spectrumItemTrace.dim,trace.spectrumItemTrace.position, traceMarker=trace)
 
-    print(self.traceScale)
 
   def increaseTraceScale(self):
-    # xData = self.spectrumItemTrace.xData
-    # newData = self.spectrumItemTrace.yData/10
-    # self.spectrumItemTrace.setData(xData,newData)
     self.traceScale/=1.41
-    self.removeItem(self.spectrumItemTrace)
-    self.plotTrace(self.spectrumItemTrace.dim,self.spectrumItemTrace.position)
-    print(self.traceScale)
+    for trace in self.traces:
+      self.removeItem(trace.spectrumItemTrace)
+      self.plotTrace(trace.spectrumItemTrace.dim,trace.spectrumItemTrace.position, traceMarker=trace)
 
-
-  def updateTrace(self, xData, yData):
-    self.removeItem(self.spectrumItemTrace)
-    self.newSpectrumItemTrace = pg.PlotDataItem(xData,yData)
-    self.addItem(self.newSpectrumItemTrace)
-    self.spectrumItemTrace = self.newSpectrumItemTrace
-
+  #
+  # def updateTrace(self, xData, yData):
+  #   for trace in self.traces:
+  #     self.removeItem(trace)
+  #     trace.newSpectrumItemTrace = pg.PlotDataItem(xData,yData)
+  #     self.addItem(trace.SpectrumItemTrace)
 
   def upBy2(self):
     self.current.spectrum.spectrumItem.baseLevel*=1.4
@@ -221,53 +218,87 @@ class SpectrumNdPane(SpectrumPane):
     self.current.spectrum.spectrumItem.levels = self.current.spectrum.spectrumItem.getLevels()
 
   def addHTraceMarker(self):
-    # if orientation == 'horizontal':
     traceMarker = pg.InfiniteLine(angle=0, movable=True, pos=self.mousePoint)
     self.addItem(traceMarker)
     dim = 1
     self.traceMarkers.append(traceMarker)
-    trace = self.plotTrace(dim, position=self.mousePoint.toTuple())
+    trace = self.plotTrace(dim, position=self.mousePoint.toTuple(), traceMarker=traceMarker)
     self.traces.append(trace)
     traceMarker.sigPositionChanged.connect(self.markerMoved)
     proxy = pg.SignalProxy(traceMarker.sigPositionChanged, slot=(self.markerMoved))
+    if self.phaseButtonShown == False:
+      self.phasingAction = QtGui.QAction("phasing", self, checkable=True)
+      self.phasingAction.toggled.connect(self.togglePhasingBar)
+      self.spectrumUtilToolbar.addAction(self.phasingAction)
+      self.phaseButtonShown = True
 
   def addVTraceMarker(self):
-      traceMarker = pg.InfiniteLine(angle=90, movable=True, pos=self.mousePoint)
-      self.addItem(traceMarker)
-      dim = 0
-      self.traceMarkers.append(traceMarker)
-      trace = self.plotTrace(dim, position=self.mousePoint.toTuple())
-      self.traces.append(trace)
-      traceMarker.sigPositionChanged.connect(self.markerMoved)
-      proxy = pg.SignalProxy(traceMarker.sigPositionChanged, slot=(self.markerMoved))
+    traceMarker = pg.InfiniteLine(angle=90, movable=True, pos=self.mousePoint)
+    self.addItem(traceMarker)
+    dim = 0
+    self.traceMarkers.append(traceMarker)
+    trace = self.plotTrace(dim, position=self.mousePoint.toTuple(), traceMarker=traceMarker)
+    self.traces.append(trace)
+    traceMarker.sigPositionChanged.connect(self.markerMoved)
+    proxy = pg.SignalProxy(traceMarker.sigPositionChanged, slot=(self.markerMoved))
+    if self.phaseButtonShown == False:
+      self.phasingAction = QtGui.QAction("phasing", self, checkable=True)
+      self.phasingAction.toggled.connect(self.togglePhasingBar)
+      self.spectrumUtilToolbar.addAction(self.phasingAction)
+      self.phaseButtonShown = True
 
 
   def markerMoved(self, traceMarker):
-    self.removeItem(self.spectrumItemTrace)
+    self.removeItem(traceMarker.spectrumItemTrace)
     if traceMarker.angle== 0:
       dim = 1
       positions = [traceMarker.getXPos(),traceMarker.getYPos()]
-      # self.plotTrace(dim=dim, position=positions, module=module)
     elif traceMarker.angle== 90:
       positions = [traceMarker.getXPos(),traceMarker.getYPos()]
       dim = 0
-    # print(positions)
-    self.plotTrace(dim=dim, position=positions)
+    self.plotTrace(dim=dim, position=positions, traceMarker=traceMarker)
 
-  def plotTrace(self, dim, position=None):
+  def phasingZeroOrder(self, value):
+    for trace in self.traces:
+
+      transformedData = signal.hilbert(trace.data)
+      # print(transformedData)
+      p0 = math.radians(value)
+      # print('deg',value,'rad',p0)
+      phasedData = (transformedData.real * math.cos(p0)) - (transformedData.imag * math.sin(p0))
+      # print('npcos',numpy.cos(p0))
+      # print(phasedData)
+      # phasedData2 = numpy.real(phasedData)
+      # print(phasedData2[0:9])
+      self.removeItem(trace.spectrumItemTrace)
+      self.plotTrace(trace.spectrumItemTrace.dim,trace.spectrumItemTrace.position, traceMarker=trace, data=phasedData)
+    #   p0 = p0 * pi / 180.  # convert to radians
+    #   p1 = p1 * pi / 180.
+    #   size = data.shape[-1]
+    #   apod = np.exp(1.0j * (p0 + (p1 * np.arange(size) / size))
+    #               ).astype(data.dtype)
+    # if inv:
+    #     apod = 1 / apod
+    # return apod * data
+
+  def phasingFirstOrder(self):
+    pass
+
+  def plotTrace(self, dim, position=None, traceMarker=None, data=None):
+
     positions = []
     if position is None:
       position = self.mousePoint.toTuple()
     else:
       position = position
     for i in range(len(position)):
-      dataDimRef = self.current.spectrum.ccpnSpectrum.sortedDataDims()[i].findFirstDataDimRef()
       positions.append(math.floor(LibSpectrum.getDimPointFromValue(self.current.spectrum, i, position[i])))
 
     spectrum = self.current.spectrum
 
     if dim == 0:
-      data = LibSpectrum.getSliceData(spectrum.ccpnSpectrum,position=positions, sliceDim=1)
+      if data is None:
+        data = LibSpectrum.getSliceData(spectrum.ccpnSpectrum,position=positions, sliceDim=1)
       dataDimRef = self.current.spectrum.ccpnSpectrum.sortedDataDims()[1].findFirstDataDimRef()
       firstPoint = dataDimRef.pointToValue(0)
       pointCount = len(data)
@@ -275,32 +306,55 @@ class SpectrumNdPane(SpectrumPane):
       pointSpacing = (lastPoint-firstPoint)/pointCount
       positions2 = numpy.array([firstPoint + n*pointSpacing for n in range(pointCount)],dtype=numpy.float32)
       spectrumData = numpy.array([positions2,data], dtype=numpy.float32)
-      spectrumItemTrace = Spectrum1dItem(self,spectrum, spectralData=spectrumData)
+      data2 = (data/self.traceScale)
+      data3 = numpy.array([x+position[0] for x in data2])
+      traceMarker.spectrumItemTrace = pg.PlotDataItem(data3,positions2)
+      traceMarker.spectrumItemTrace.dim = dim
+      traceMarker.data = data
+      traceMarker.spectrumItemTrace.position = position
+      self.addItem(traceMarker.spectrumItemTrace)
 
     if dim == 1:
       dataDimRef = self.current.spectrum.ccpnSpectrum.sortedDataDims()[0].findFirstDataDimRef()
-      data = LibSpectrum.getSliceData(spectrum.ccpnSpectrum,position=positions, sliceDim=0)
+      if data is None:
+        data = LibSpectrum.getSliceData(spectrum.ccpnSpectrum,position=positions, sliceDim=0)
+      # print(data)
       firstPoint = dataDimRef.pointToValue(0)
       pointCount = len(data)
       lastPoint = dataDimRef.pointToValue(pointCount)
       pointSpacing = (lastPoint-firstPoint)/pointCount
       positions2 = numpy.array([firstPoint + n*pointSpacing for n in range(pointCount)],dtype=numpy.float32)
-      spectrumData = numpy.array([positions2,data], dtype=numpy.float32)
+      # spectrumData = numpy.array([positions2,data], dtype=numpy.float32)
       data2 = (data/self.traceScale)*-1
       data3 = numpy.array([x+position[1] for x in data2])
-      spectrumItemTrace = pg.PlotDataItem(positions2,data3)
-      spectrumItemTrace.dim = dim
-      spectrumItemTrace.position = position
-      self.addItem(spectrumItemTrace)
-    return spectrumItemTrace
+      traceMarker.spectrumItemTrace = pg.PlotDataItem(positions2,data3)
+      traceMarker.spectrumItemTrace.dim = dim
+      traceMarker.data = data
+      traceMarker.spectrumItemTrace.position = position
+      self.addItem(traceMarker.spectrumItemTrace)
 
+    return traceMarker
 
+  def changeValue(self, label, value):
+
+    floatValue = float(value)
+    label.setText(str(floatValue))
+
+  def changeSliderValue(self, slider, value):
+
+    floatValue = float(value)
+    slider.setValue(floatValue)
 
   def clearTraceMarkers(self):
     for item in self.traceMarkers:
       self.removeItem(item)
+      self.removeItem(item.spectrumItemTrace)
     for item in self.traces:
       self.removeItem(item)
+      self.removeItem(item)
+    self.spectrumUtilToolbar.removeAction(self.phasingAction)
+    self.phaseButtonShown == False
+    self.traces = []
 
   def fillToolBar(self):
     plusOneAction = self.spectrumUtilToolbar.addAction("+1", self.addOne)
@@ -332,4 +386,48 @@ class SpectrumNdPane(SpectrumPane):
     self.spectrumUtilToolbar.addAction(self.hTraceAction)
     self.spectrumUtilToolbar.addAction(self.vTraceAction)
 
+
+
+  def togglePhasingBar(self):
+    if self.phasingAction.isChecked() == True:
+      self.raisePhasingToolBar()
+    if self.phasingAction.isChecked() == False:
+      self.phasingToolBar.hide()
+
+  def raisePhasingToolBar(self):
+    self.phasingToolBar = QtGui.QToolBar()
+    zeroPhaseSlider  = QtGui.QSlider(self)
+    zeroPhaseSlider.setOrientation(QtCore.Qt.Horizontal)
+    zeroPhaseSlider.setRange(-180.0, 180.0)
+    zeroPhaseSlider.setValue(0)
+    zeroPhaseSlider.setFixedWidth(350)
+    zeroPhaseSlider.setTickInterval(0.1)
+    zeroPhaseLabel = QtGui.QLineEdit(self)
+    zeroPhaseLabel.setFixedWidth(50)
+    zeroLabel = Label(self,text="P0")
+    zeroLabel.setFixedWidth(30)
+    self.phasingToolBar.addWidget(zeroLabel)
+    self.phasingToolBar.addWidget(zeroPhaseSlider)
+    zeroPhaseLabel.setText(str(zeroPhaseSlider.value()))
+    zeroPhaseSlider.valueChanged.connect(partial(self.changeValue,zeroPhaseLabel))
+    zeroPhaseLabel.textChanged.connect(partial(self.changeSliderValue,zeroPhaseSlider))
+    zeroPhaseSlider.valueChanged.connect(self.phasingZeroOrder)
+    self.phasingToolBar.addWidget(zeroPhaseLabel)
+    firstPhaseSlider  = QtGui.QSlider(self)
+    firstPhaseSlider.setFixedWidth(350)
+    firstPhaseSlider.setOrientation(QtCore.Qt.Horizontal)
+    firstPhaseSlider.setRange(-360.0, 360.0)
+    firstPhaseSlider.setValue(0)
+    firstPhaseSlider.setTickInterval(0.1)
+    firstLabel = Label(self,text="P1")
+    firstLabel.setFixedWidth(30)
+    firstPhaseLabel = QtGui.QLineEdit(self)
+    firstPhaseLabel.setFixedWidth(50)
+    firstPhaseLabel.setText(str(firstPhaseSlider.value()))
+    firstPhaseSlider.valueChanged.connect(partial(self.changeValue,firstPhaseLabel))
+    firstPhaseLabel.textChanged.connect(partial(self.changeSliderValue,firstPhaseSlider))
+    self.phasingToolBar.addWidget(firstLabel)
+    self.phasingToolBar.addWidget(firstPhaseSlider)
+    self.phasingToolBar.addWidget(firstPhaseLabel)
+    self.dock.addWidget(self.phasingToolBar, 3, 0, 1, 11)
 

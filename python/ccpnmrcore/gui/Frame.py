ee9beff001fff2a65a34015d7d3855f8aa19df30
Wayne Boucher <wb104@cam.ac.uk>
2015-01-16 18:13:49 +0000

    add
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@7994 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ python/ccpnmrcore/gui/Frame.py
@@ -0,0 +1,11 @@
+from PySide import QtGui
+
+from ccpncore.gui.Frame import Frame as CoreFrame
+from ccpnmrcore.Base import Base as GuiBase
+
+class Frame(CoreFrame, GuiBase):
+
+  def __init__(self, parent=None, **kw):
+
+    CoreFrame.__init__(self, parent)
+    GuiBase.__init__(self, **kw)

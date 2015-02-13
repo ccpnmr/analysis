04ad3641893d54eaf7bd5118969d34aac8238b00
Rasmus Fogh <rhf22@cam.ac.uk>
2015-02-13 14:43:17 +0000

    New changes. Should OVERRIDE parallel changes from Simon
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8046 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpnmrcore/modules/GuiStripDisplayNd.py
+++ python/ccpnmrcore/modules/GuiStripDisplayNd.py
@@ -1,16 +1,14 @@
 __author__ = 'simon'
 
-import re
-
-from PySide import QtGui, QtCore
-
 from ccpncore.gui.VerticalLabel import VerticalLabel
-
 from ccpn.lib import Spectrum as LibSpectrum
-
 from ccpnmrcore.modules.GuiSpectrumDisplay import GuiSpectrumDisplay
+<<<<<<< .mine
+=======
 from ccpnmrcore.modules.GuiSpectrumViewNd import GuiSpectrumViewNd
 from ccpnmrcore.modules.GuiStripNd import GuiStripNd
+>>>>>>> .r8045
+
 
 def _findPpmRegion(spectrum, axisDim, spectrumDim):
   
@@ -27,74 +25,73 @@ def _findPpmRegion(spectrum, axisDim, spectrumDim):
   
 class GuiStripDisplayNd(GuiSpectrumDisplay):
 
-  def __init__(self, dockArea, apiSpectrumDisplayNd):
-    if not apiSpectrumDisplayNd.strips:
-      apiSpectrumDisplayNd.newStripNd()
+  def __init__(self):
+    # if not apiSpectrumDisplayNd.strips:
+    #   apiSpectrumDisplayNd.newStripNd()
     
     self.viewportDict = {} # maps QGLWidget to GuiStripNd
 
-    GuiSpectrumDisplay.__init__(self, dockArea, apiSpectrumDisplayNd)
+    GuiSpectrumDisplay.__init__(self)
     self.fillToolBar()
     self.addSpinSystemSideLabel()
-    self.appBase.current.pane = self
+    # self._appBase.current.pane = self
 
-  def addSpectrum(self, spectrum):
+  # def addSpectrum(self, spectrum):
+  #
+  #   apiSpectrumDisplay = self.apiSpectrumDisplay
+  #
+  #   #axisCodes = spectrum.axisCodes
+  #   axisCodes = LibSpectrum.getAxisCodes(spectrum)
+  #   if axisCodes != self.apiSpectrumDisplay.axisCodes:
+  #     raise Exception('Cannot overlay that spectrum on this display')
+  #
+  #   apiDataSource = spectrum._wrappedData
+  #   apiSpectrumView = apiSpectrumDisplay.findFirstSpectrumView(dataSource=apiDataSource)
+  #   if apiSpectrumView:
+  #     raise Exception('Spectrum already in display')
+  #
+  #   dimensionCount = spectrum.dimensionCount
+  #   dimensionOrdering = range(1, dimensionCount+1)
+  #   apiSpectrumView = apiSpectrumDisplay.newSpectrumView(spectrumName=apiDataSource.name,
+  #                         dimensionOrdering=dimensionOrdering)
+  #   guiSpectrumView = GuiSpectrumViewNd(self, apiSpectrumView)
+  #
+  #   # at this point likely there is only one guiStrip???
+  #   if not apiSpectrumDisplay.axes: # need to create these since not done automatically
+  #     # TBD: assume all strips the same and the strip direction is the Y direction
+  #     for m, axisCode in enumerate(apiSpectrumDisplay.axisCodes):
+  #       position, width = _findPpmRegion(spectrum, m, dimensionOrdering[m]-1) # -1 because dimensionOrdering starts at 1
+  #       for n, guiStrip in enumerate(self.guiStrips):
+  #         if m == 1: # Y direction
+  #           if n == 0:
+  #             apiSpectrumDisplay.newFrequencyAxis(code=axisCode, position=position, width=width, stripSerial=1)
+  #         else: # other directions
+  #           apiSpectrumDisplay.newFrequencyAxis(code=axisCode, position=position, width=width, stripSerial=1) # TBD: non-frequency axis; TBD: should have stripSerial=0 but that not working
+  #
+  #         viewBox = guiStrip.viewBox
+  #         region = (position-0.5*width, position+0.5*width)
+  #         if m == 0:
+  #           viewBox.setXRange(*region)
+  #         elif m == 1:
+  #           viewBox.setYRange(*region)
+  #
+  #   for guiStrip in self.guiStrips:
+  #     guiStrip.addSpectrum(guiSpectrumView)
+  #
+  # def addStrip(self):
+  #
+  #   apiStrip = self.apiSpectrumDisplay.newStripNd()
+  #   n = len(self.apiSpectrumDisplay.strips) - 1
+  #   guiStrip = GuiStripNd(self.stripFrame, apiStrip, grid=(1, n), stretch=(0, 1))
+  #   guiStrip.addPlaneToolbar(self.stripFrame, n)
+  #   guiStrip.addSpinSystemLabel(self.stripFrame, n)
+  #   if n > 0:
+  #     prevGuiStrip = self.guiStrips[n-1]
+  #     prevGuiStrip.axes['right']['item'].hide()
+  #     guiStrip.setYLink(prevGuiStrip)
 
-    apiSpectrumDisplay = self.apiSpectrumDisplay
-    
-    #axisCodes = spectrum.axisCodes
-    axisCodes = LibSpectrum.getAxisCodes(spectrum)
-    if axisCodes != self.apiSpectrumDisplay.axisCodes:
-      raise Exception('Cannot overlay that spectrum on this display')
-      
-    apiDataSource = spectrum._wrappedData
-    apiSpectrumView = apiSpectrumDisplay.findFirstSpectrumView(dataSource=apiDataSource)
-    if apiSpectrumView:
-      raise Exception('Spectrum already in display')
-      
-    dimensionCount = spectrum.dimensionCount
-    dimensionOrdering = range(1, dimensionCount+1)
-    apiSpectrumView = apiSpectrumDisplay.newSpectrumView(spectrumName=apiDataSource.name,
-                          dimensionOrdering=dimensionOrdering)
-    guiSpectrumView = GuiSpectrumViewNd(self, apiSpectrumView)
-
-    # at this point likely there is only one guiStrip???
-    if not apiSpectrumDisplay.axes: # need to create these since not done automatically
-      # TBD: assume all strips the same and the strip direction is the Y direction
-      for m, axisCode in enumerate(apiSpectrumDisplay.axisCodes):
-        position, width = _findPpmRegion(spectrum, m, dimensionOrdering[m]-1) # -1 because dimensionOrdering starts at 1
-        for n, guiStrip in enumerate(self.guiStrips):
-          if m == 1: # Y direction
-            if n == 0:
-              apiSpectrumDisplay.newFrequencyAxis(code=axisCode, position=position, width=width, stripSerial=1)
-          else: # other directions
-            apiSpectrumDisplay.newFrequencyAxis(code=axisCode, position=position, width=width, stripSerial=1) # TBD: non-frequency axis; TBD: should have stripSerial=0 but that not working
-        
-          viewBox = guiStrip.viewBox
-          region = (position-0.5*width, position+0.5*width)
-          if m == 0:
-            viewBox.setXRange(*region)
-          elif m == 1:
-            viewBox.setYRange(*region)
-        
-    for guiStrip in self.guiStrips:
-      guiStrip.addSpectrum(guiSpectrumView)
-    
-  def addStrip(self):
-
-    apiStrip = self.apiSpectrumDisplay.newStripNd()
-    n = len(self.apiSpectrumDisplay.strips) - 1
-    guiStrip = GuiStripNd(self.stripFrame, apiStrip, grid=(1, n), stretch=(0, 1))
-    guiStrip.addPlaneToolbar(self.stripFrame, n)
-    guiStrip.addSpinSystemLabel(self.stripFrame, n)
-    if n > 0:
-      prevGuiStrip = self.guiStrips[n-1]
-      prevGuiStrip.axes['right']['item'].hide()
-      guiStrip.setYLink(prevGuiStrip)
-
-
-  def fillToolBar(self):
-    GuiSpectrumDisplay.fillToolBar(self)
+  # def fillToolBar(self):
+  #   GuiSpectrumDisplay.fillToolBar(self)
     # self.spectrumUtilToolBar.addAction(QtGui.QAction('HS', self, triggered=self.hideSpinSystemLabel))
     # self.spectrumUtilToolBar.addAction(QtGui.QAction("SS", self, triggered=self.showSpinSystemLabel))
 
@@ -102,6 +99,7 @@ class GuiStripDisplayNd(GuiSpectrumDisplay):
     self.spinSystemSideLabel.show()
 
   def addSpinSystemSideLabel(self):
+    self.spinSystemSideLabel = VerticalLabel(self, text=None)
     self.spinSystemSideLabel = VerticalLabel(self, text=None)
     self.spinSystemSideLabel.setText('A.147.ALA.HA')
     # spinSystemSideLabel.setText()

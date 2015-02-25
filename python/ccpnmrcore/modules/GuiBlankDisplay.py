7bf68a5f07016009304275b43131211b944bf486
Wayne Boucher <wb104@cam.ac.uk>
2015-02-25 15:34:34 +0000

    Replace our code with wrapper call
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8137 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpnmrcore/modules/GuiBlankDisplay.py
+++ python/ccpnmrcore/modules/GuiBlankDisplay.py
@@ -42,55 +42,8 @@ class GuiBlankDisplay(DropBase, Dock): # DropBase needs to be first, else the dr
     if isinstance(dropObject, Spectrum):
       spectrum = dropObject
       spectrumDisplay = self.dockArea.guiWindow.createSpectrumDisplay(spectrum)
-      # spectrumDisplay.addSpectrumToDisplay(spectrum)
       spectrumView = self.getWrapperObject(spectrumDisplay._wrappedData.findFirstSpectrumView(dataSource=spectrum._wrappedData))
-
-      dimensionCount = spectrum.dimensionCount
-      dimensionOrdering = range(1, dimensionCount+1)
-      apiSpectrumDisplay = spectrumDisplay._wrappedData
-      if apiSpectrumDisplay.axes:
-        for m, axisCode in enumerate(apiSpectrumDisplay.axisCodes):
-          position, width = _findPpmRegion(spectrum, m, dimensionOrdering[m]-1) # -1 because dimensionOrdering starts at 1
-          for n, strip in enumerate(spectrumView.strips):
-            if m == 1: # Y direction
-              if n == 0:
-                axis = apiSpectrumDisplay.findFirstAxis(code=axisCode)
-                axis.position = position
-                axis.width = width
-            else: # other directions
-              axis = apiSpectrumDisplay.findFirstAxis(code=axisCode)
-              axis.position = position
-              axis.width = width
-
-            viewBox = strip.viewBox
-            region = (position-0.5*width, position+0.5*width)
-            if m == 0:
-              viewBox.setXRange(*region)
-              print(*region)
-            elif m == 1:
-              viewBox.setYRange(*region)
-              print(*region)
-
-      else: # need to create these since not done automatically (or are they now?)
-        # TBD: assume all strips the same and the strip direction is the Y direction
-        for m, axisCode in enumerate(apiSpectrumDisplay.axisCodes):
-          position, width = _findPpmRegion(spectrum, m, dimensionOrdering[m]-1) # -1 because dimensionOrdering starts at 1
-          for n, strip in enumerate(spectrumView.strips):
-            if m == 1: # Y direction
-              if n == 0:
-                apiSpectrumDisplay.newFrequencyAxis(code=axisCode, position=position, width=width, stripSerial=1)
-            else: # other directions
-              apiSpectrumDisplay.newFrequencyAxis(code=axisCode, position=position, width=width, stripSerial=1) # TBD: non-frequency axis; TBD: should have stripSerial=0 but that not working
-
-            viewBox = strip.viewBox
-            region = (position-0.5*width, position+0.5*width)
-            if m == 0:
-              viewBox.setXRange(*region)
-            elif m == 1:
-              viewBox.setYRange(*region)
-
       for strip in spectrumView.strips:
-        strip.displayASpectrum(spectrumView)
+        strip.displaySpectrum(spectrumView)
       self.dockArea.guiWindow.removeBlankDisplay()
-      return spectrumDisplay
 

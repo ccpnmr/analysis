b46e215461049726438d8b13a300018aef40a2e9
Simon Skinner <ss885@leicester.ac.uk>
2015-03-23 14:00:07 +0000

    changed case of pid filename
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8199 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpncore/util/Pid.py
+++ python/ccpncore/util/Pid.py
@@ -4,7 +4,7 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - $Date$"
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - $Date: 2015-03-16 16:57:10 +0000 (Mon, 16 Mar 2015) $"
 __credits__ = "Wayne Boucher, Rasmus H Fogh, Simon P Skinner, Geerten W Vuister"
 __license__ = ("CCPN license. See www.ccpn.ac.uk/license"
               "or ccpncore.memops.Credits.CcpnLicense for license text")
@@ -14,9 +14,9 @@ __reference__ = ("For publications, please use reference from www.ccpn.ac.uk/lic
 #=========================================================================================
 # Last code modification:
 #=========================================================================================
-__author__ = "$Author$"
-__date__ = "$Date$"
-__version__ = "$Revision$"
+__author__ = "$Author: skinnersp $"
+__date__ = "$Date: 2015-03-16 16:57:10 +0000 (Mon, 16 Mar 2015) $"
+__version__ = "$Revision: 8180 $"
 
 #=========================================================================================
 # Start of code

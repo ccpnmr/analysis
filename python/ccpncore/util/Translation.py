69eaa3a6b0c8da5f66f9de81ae4d76e659f53cf9
Wayne Boucher <wb104@cam.ac.uk>
2015-05-20 16:11:26 +0000

    Rework how translation dictionary updated
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8297 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpncore/util/Translation.py
+++ python/ccpncore/util/Translation.py
@@ -41,25 +41,23 @@ translationDirectory = 'translation'  # assumes that all translations are in sub
 translationLanguage = defaultLanguage
 translationDebug = False
 translationDict = {}  # maps from default language text to translated text
-translationModuleSet = set()  # keeps track of which translation modules have been loaded
 
 def setTranslationLanguage(language=defaultLanguage, debug=False):
   """ Set the translation language """
   
-  global translationLanguage, translationDict, translationDebug, translationModuleSet
+  global translationLanguage, translationDict, translationDebug
 
   translationLanguage = language
   translationDebug = debug
   translationDict = {}
-  translationModuleSet = set()
   
-  updateTranslationDict()
+  ###updateTranslationDict()
 
 ### CARE: Problem is that we want to find the module in which the object is created,
 ### not the module in which the code which creates it lives.
 ### The code below is dangerous and could not work in some Python implementations.
 
-def updateTranslationDict(depth=None):
+def updateTranslationDict(moduleName=None):
   """ Add all the translations for directory in which this object is located.
       The depth is how far up calling stack to go (not including this function itself).
   """
@@ -67,38 +65,16 @@ def updateTranslationDict(depth=None):
   if translationLanguage == defaultLanguage:
     return
   
-  if depth is None:
-    objModuleName = 'ccpncore.util'
-  else:
-    fileName = sys._getframe(depth+1).f_code.co_filename
-    for objModuleName in sys.modules:
-      module = sys.modules[objModuleName]
-      try:
-        if module.__file__ == fileName:
-          break
-      except:
-        pass
-    else:
-      logger = Logging.getLogger()
-      logger.warning('translation module not found for path "%s"' % fileName)
-      return
-    n = objModuleName.rfind('.')
-    if n < 0:
-      logger = Logging.getLogger()
-      logger.warning('translation module missing . for path "%s"' % fileName)
-      return
-    objModuleName = objModuleName[:n]
+  if moduleName is None:
+    moduleName = 'ccpncore.util'
     
-  if objModuleName in translationModuleSet:  # translations already been added for this directory
-    return
+  translationModuleName = '%s.%s.%s' % (moduleName, translationDirectory, translationLanguage)
     
-  moduleName = '%s.%s.%s' % (objModuleName, translationDirectory, translationLanguage)
-  
   try:
-    module = importlib.import_module(moduleName)
+    module = importlib.import_module(translationModuleName)
   except ImportError as e:
     logger = Logging.getLogger()
-    logger.warning('translation module "%s" not available' % moduleName)
+    logger.warning('translation for module "%s" not available for language "%s"' % (moduleName, translationLanguage))
     return
     
   try:
@@ -110,8 +86,6 @@ def updateTranslationDict(depth=None):
   
   translationDict.update(moduleTranslationDict)
   
-  translationModuleSet.add(objModuleName)
-  
 def getTranslation(text):
   """ Translate a specific text into the previously specified language. """
 
@@ -120,7 +94,7 @@ def getTranslation(text):
     
   translatedText = translationDict.get(text)
   
-  if not translatedText:
+  if translatedText is None:
     logger = Logging.getLogger()
     logger.warning('text "%s" not in %s translation dictionary' % (text, translationLanguage))
     translatedText = text
@@ -131,12 +105,5 @@ def getTranslation(text):
   return translatedText
 
 class Translation:
-  def __init__(self, depth=2):
-    """ The depth is how far up calling stack to go (not including this function itself).
-        In general want to go 2 up the stack because that is the module in which the widget lives.
-        (0 is normally Base.py and 1 is normally the ccpncore.gui file, e.g. Action.py or Button.py)
-    """
-    updateTranslationDict(depth+1)
-
   def translate(self, text):
     return getTranslation(text)

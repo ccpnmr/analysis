db47cf9cba0da9f090cde92cd7203eb0b74b9afc
Wayne Boucher <wb104@cam.ac.uk>
2015-06-05 15:46:13 +0000

    add
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8340 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ python/ccpncore/util/testing/Test_UndoPeak.py
@@ -0,0 +1,64 @@
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - $Date: 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__credits__ = "Wayne Boucher, Rasmus H Fogh, Simon P Skinner, Geerten W Vuister"
+__license__ = ("CCPN license. See www.ccpn.ac.uk/license"
+              "or ccpncore.memops.Credits.CcpnLicense for license text")
+__reference__ = ("For publications, please use reference from www.ccpn.ac.uk/license"
+                " or ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification:
+#=========================================================================================
+__author__ = "$Author: rhfogh $"
+__date__ = "$Date: 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__version__ = "$Revision: 7686 $"
+
+#=========================================================================================
+# Start of code
+#=========================================================================================
+from ccpncore.util.Undo import Undo
+from ccpncore.util import Io as ioUtil
+from ccpncore.util.Testing import Testing
+
+class PeakUndoTest(Testing):
+  
+  def __init__(self, *args, **kw):
+    Testing.__init__(self, 'CcpnCourse1b', *args, **kw)
+    
+  def test_new_peak_undo(self):
+    
+    project = self.project
+    nmrProject = project.findFirstNmrProject()
+    experiment = nmrProject.findFirstExperiment(name='HSQC')
+    dataSource = experiment.findFirstDataSource(name='115')
+    peakList = dataSource.findFirstPeakList()
+    
+    project._undo = Undo()
+    project._undo.newWaypoint()
+    print('project undo stack size before newPeak: %d' % len(project._undo))
+    peak = peakList.newPeak()
+    print('project undo stack size after newPeak: %d' % len(project._undo))
+    project._undo.undo()
+    print('project undo stack size after undo: %d' % len(project._undo))
+    
+  def test_new_peaklist_undo(self):
+   
+    project = self.project
+    nmrProject = project.findFirstNmrProject()
+    experiment = nmrProject.findFirstExperiment(name='HSQC')
+    dataSource = experiment.findFirstDataSource(name='115')
+   
+    project._undo = Undo()
+    project._undo.newWaypoint()
+    print('project undo stack size before newPeakList: %d' % len(project._undo))
+    peakList = dataSource.newPeakList()
+    peak = peakList.newPeak()
+    print('project undo stack size after newPeak: %d' % len(project._undo))
+    project._undo.undo()
+    print('project undo stack size after undo: %d' % len(project._undo))
+ 

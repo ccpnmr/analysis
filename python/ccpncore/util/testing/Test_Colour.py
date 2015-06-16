a4022e8d78a68505a9ada69a60268163a923d857
Wayne Boucher <wb104@cam.ac.uk>
2015-06-16 16:47:34 +0000

    Change Color to Colour
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8353 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpncore/util/testing/Test_Colour.py
+++ python/ccpncore/util/testing/Test_Colour.py
@@ -21,31 +21,31 @@ __version__ = "$Revision: 7686 $"
 #=========================================================================================
 # Start of code
 #=========================================================================================
-from ccpncore.util.Color import Color
+from ccpncore.util.Colour import Colour
 
 from nose.tools import raises
 
 @raises(TypeError)
 def test_color_create_blank():
-  c = Color()
+  c = Colour()
 
 def test_color_create_known_name():
-  c = Color('red')
+  c = Colour('red')
 
 @raises(KeyError)
 def test_color_create_unknown_name():
-  c = Color('really-red')
+  c = Colour('really-red')
 
 def test_color_create_hex():
-  c = Color('#ff00ff')
+  c = Colour('#ff00ff')
   print(c.name)
-  c = Color('#FF00FFFF')
+  c = Colour('#FF00FFFF')
   print(c.name)
 
 def test_color_create_rgb():
-  c = Color((255, 0, 255))
+  c = Colour((255, 0, 255))
   print(c.name)
-  c = Color((255, 0, 255, 127))
+  c = Colour((255, 0, 255, 127))
   print(c.name)
 
 

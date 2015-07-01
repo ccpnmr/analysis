b4bb75ffcf89817ee4f7e83c424cd75ef903e049
Simon Skinner <ss885@leicester.ac.uk>
2015-07-01 12:34:11 +0000

    add
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8391 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ python/ccpncore/lib/Io/FastaIo.py
@@ -0,0 +1,30 @@
+
+def isFastaFormat(inputFile):
+
+  if inputFile.split('.')[-1] == 'fasta':
+    return True
+  else:
+    with open(inputFile, 'r') as f:
+      if f.readlines()[0].startswith('>'):
+        return True
+
+
+def parseFastaFile(inputFile):
+
+  sequences = []
+  with open(inputFile, 'r') as f:
+    chains = []
+    lines = [line.strip() for line in f.readlines()][:-1]
+    for line in lines:
+      if line[0] == '>':
+        chains.append(lines.index(line))
+  for chain in chains :
+    name = lines[chain].split()[0]
+    index = chains.index(chain)
+    if not index == len(chains)-1:
+      endIndex = chains[index+1]
+      sequences.append([name, ''.join(lines[chain+1:endIndex])])
+    else:
+      sequences.append([name, ''.join(lines[chain+1:])])
+
+  return sequences

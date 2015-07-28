797a3200acf949e91896d11671c7727273d95546
Wayne Boucher <wb104@cam.ac.uk>
2015-07-28 16:40:36 +0000

    Add region and diagonal exclusions
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8496 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpn/lib/wrapper/PeakList.py
+++ python/ccpn/lib/wrapper/PeakList.py
@@ -32,14 +32,17 @@ from scipy.ndimage import maximum_filter
 
 Sequence = collections.abc.Sequence
 
-def findPeaksNd(peakList:object, positions:Sequence=None, dataDims:Sequence=None, doPos:bool=True, doNeg:bool=True, fitMethod=None):
+def findPeaksNd(peakList:object, positions:Sequence=None, dataDims:Sequence=None,
+                doPos:bool=True, doNeg:bool=True,
+                fitMethod:str=None, excludedRegions:Sequence=None,
+                excludedDiagonalDims:Sequence=None, excludedDiagonalTransform:Sequence=None):
 
   ordering = [dataDim.dim-1 for dataDim in dataDims]
   isoOrdering = [dataDim.getIsotopeCodes() for dataDim in dataDims]
 
   startPoint = []
   endPoint = []
-
+  
   spectrum = peakList.spectrum
 
   for position in positions[1]:
@@ -57,7 +60,9 @@ def findPeaksNd(peakList:object, positions:Sequence=None, dataDims:Sequence=None
   posLevel = spectrum.positiveContourBase*100 if doPos else None
   negLevel = spectrum.negativeContourBase*100 if doNeg else None
 
-  apiPeaks = pickNewPeaks(peakList.apiPeakList, startPoint=startPoints, endPoint=endPoints, posLevel=posLevel, negLevel=negLevel, fitMethod=fitMethod)
+  apiPeaks = pickNewPeaks(peakList.apiPeakList, startPoint=startPoints, endPoint=endPoints,
+                 posLevel=posLevel, negLevel=negLevel, fitMethod=fitMethod, excludedRegions=excludedRegions,
+                 excludedDiagonalDims=excludedDiagonalDims, excludedDiagonalTransform=excludedDiagonalTransform)
 
   data2ObjDict = peakList._project._data2Obj
   

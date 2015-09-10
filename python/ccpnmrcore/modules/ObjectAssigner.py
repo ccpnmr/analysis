e9b97d8ca9929ac2626d4c9092c05bd53ef2445f
Simon Skinner <ss885@leicester.ac.uk>
2015-09-10 10:35:37 +0000

    add
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8625 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ python/ccpnmrcore/modules/ObjectAssigner.py
@@ -0,0 +1,232 @@
+__author__ = 'simon1'
+
+from PyQt4 import QtCore, QtGui
+
+from functools import partial
+
+from ccpncore.gui.Base import Base
+from ccpncore.gui.Label import Label
+from ccpncore.gui.ListWidget import ListWidget
+from ccpncore.gui.PulldownList import PulldownList
+from ccpncore.gui.Table import ObjectTable, Column
+
+from ccpn.lib.Assignment import CCP_CODES, ATOM_NAMES
+
+from ccpnmrcore.gui.assignmentModuleLogic import (getAllNmrAtoms, nmrAtomsForPeaks,
+                                                      peaksAreOnLine, intersectionOfAll,
+                                                      sameAxisCodes,
+                                                      getAxisCodeForPeakDimension,
+                                                      getIsotopeCodeForPeakDimension,
+                                                      getShiftlistForPeak,
+                                                      matchingNmrAtomsForDimensionOfPeaks)
+
+class ObjectAssigner(QtGui.QWidget, Base):
+  def __init__(self, parent, project, dim, objects, opts, **kw):
+
+    QtGui.QWidget.__init__(self, parent)
+    Base.__init__(self, **kw)
+
+
+    self.project = project
+    self.dim = dim
+    self.opts = opts
+    self.objects = objects
+    self.label = self.createEmptyWidgetLabel(dim, objects, grid=(0, 0))
+    self.listWidget = self.createEmptyListWidget(dim, grid=(1, 0))
+    self.objectTable = self.createEmptyNmrAtomsTable(grid=(3, 0))
+    self.assignmentWidget = self.createAssignmentWidget(dim)
+    self.layout().addWidget(self.assignmentWidget, 2, 0)
+    self.updateWidget(objects)
+
+
+
+  def updateWidget(self, objects):
+
+    if objects[0].shortClassName == 'PK':
+      positions = [peak.position[self.dim] for peak in objects]
+      avgPos = round(sum(positions)/len(positions), 3)
+      axisCode = objects[0].peakList.spectrum.axisCodes[self.dim]
+      text = axisCode + ' ' + str(avgPos)
+      self.label.setText(text)
+      print(text)
+      self.label.setStyleSheet("border: 0px solid; color: #f7ffff;")
+
+    else:
+      self.label.setText('')
+
+
+  def createEmptyNmrAtomsTable(self, grid):
+    '''Can be used to add a new table before setting
+       the content.
+
+    '''
+
+    columns = self.opts['objectTableColumns']
+    callback = self.opts['objectTableCallBack']
+
+    objectTable = ObjectTable(self, columns, callback=callback, objects=[], grid=grid)
+
+    # Needed to use this syntax because wanted double click not single.
+    # objectTable.doubleClicked.connect(lambda index: self.assignNmrAtomToDim(dim))
+    objectTable.setFixedHeight(80)
+    return objectTable
+    # self.objectTables.append(objectTable)
+
+  def createEmptyListWidget(self, dim, grid):
+    '''Can be used to add a new listWidget before
+       setting the content.
+
+    '''
+    listWidget = ListWidget(self, callback=self.getNmrAtom,
+                            rightMouseCallback=self.opts['listWidgetCallback'], grid=grid)
+    listWidget.setFixedHeight(80)
+    return listWidget
+    # self.listWidgets.append(listWidget)
+
+  def createEmptyWidgetLabel(self, dim, objects, grid):
+
+    positions = [peak.position[dim] for peak in objects]
+    avgPos = round(sum(positions)/len(positions), 3)
+    axisCode = objects[0].peakList.spectrum.axisCodes[dim]
+    text = axisCode + ' ' + str(avgPos)
+    label = Label(self, text=text, grid=grid)
+    label.setStyleSheet("border: 0px solid; color: #f7ffff;")
+    return label
+    # self.labels.append(label)
+
+  def createAssignmentWidget(self, dim):
+    newAssignmentWidget = QtGui.QWidget()
+    newLayout = QtGui.QGridLayout()
+    chainLabel = Label(self, 'Chain', hAlign='c')
+    seqCodeLabel = Label(self, 'Sequence', hAlign='c')
+    residueTypeLabel = Label(self, 'Type', hAlign='c')
+    atomTypeLabel = Label(self, 'Atom', hAlign='c')
+    chainPulldown = self.createChainPulldown()
+    seqCodePulldown = self.createSeqCodePulldown()
+    residueTypePulldown = self.createResTypePulldown()
+    atomTypePulldown = self.createAtomTypePulldown()
+    newLayout.addWidget(chainLabel, 0, 0)
+    newLayout.addWidget(chainPulldown, 1, 0)
+    newLayout.addWidget(seqCodeLabel, 0, 1)
+    newLayout.addWidget(seqCodePulldown, 1, 1)
+    newLayout.addWidget(residueTypeLabel, 0, 2)
+    newLayout.addWidget(residueTypePulldown, 1, 2)
+    newLayout.addWidget(atomTypeLabel, 0, 3)
+    newLayout.addWidget(atomTypePulldown, 1, 3)
+    newAssignmentWidget.setLayout(newLayout)
+    return newAssignmentWidget
+    # self.assignmentWidgets.append(newAssignmentWidget)
+
+
+  def createChainPulldown(self):
+    pulldownList = PulldownList(self)
+    pulldownList.setEditable(True)
+    # pulldownList.lineEdit().editingFinished.connect(partial(self.addItemToPulldown, pulldownList))
+    # pulldownList.editTextChanged.connect(self.addItemToPulldown)
+    # pulldownList.setData([chain.pid for chain in self.project.nmrChains])
+    # self.chainPulldowns.append(pulldownList)
+    return pulldownList
+
+  def createSeqCodePulldown(self):
+    pulldownList = PulldownList(self)
+    pulldownList.setEditable(True)
+    # pulldownList.editTextChanged.connect(self.addItemToPulldown)
+    # sequenceCodes = [nmrResidue.sequenceCode for nmrResidue in self.project.nmrResidues]
+    # pulldownList.setData(sorted(sequenceCodes, key=self.natural_key))
+    # self.seqCodePulldowns.append(pulldownList)
+    return pulldownList
+
+
+  def createResTypePulldown(self):
+    pulldownList = PulldownList(self)
+    # self.resTypePulldowns.append(pulldownList)
+    pulldownList.setEditable(True)
+    # pulldownList.editTextChanged.connect(self.addItemToPulldown)
+    return pulldownList
+
+  def createAtomTypePulldown(self):
+    pulldownList = PulldownList(self)
+    pulldownList.setEditable(True)
+    # pulldownList.editTextChanged.connect(self.addItemToPulldown)
+    # self.atomTypePulldowns.append(pulldownList)
+    return pulldownList
+
+  def setNmrChain(self, item):
+    self.project._appBase.current.nmrAtom.nmrResidue.nmrChain = self.project.getById(item)
+
+  def setSequenceCode(self, item):
+    self.project._appBase.current.nmrAtom.nmrResidue.sequenceCode = item
+
+  def setResidueType(self, item):
+    self.project._appBase.current.nmrAtom.nmrResidue.residueType = item
+
+  def setAtomType(self, item):
+    self.project._appBase.current.nmrAtom.name = item
+
+  def natural_key(self, string_):
+    import re
+    """See http://www.codinghorror.com/blog/archives/001018.html"""
+    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+
+
+
+  def getNmrAtom(self, dim, item):
+    # print(item.text(), self.project.getById(item.text()))
+    nmrAtom = self.project.getById(item.text())
+    self.project._appBase.current.nmrAtom = nmrAtom
+    chain = nmrAtom.nmrResidue.nmrChain
+    sequenceCode = nmrAtom.nmrResidue.sequenceCode
+    residueType = nmrAtom.nmrResidue.residueType
+    atomType = nmrAtom.name
+    if not nmrAtom.nmrResidue.residue:
+      self.chainPulldowns[dim].setData([chain.id for chain in self.project.nmrChains])
+      # self.chainPulldowns[dim].setIndex(self.chainPulldowns[dim].texts.index(chain.id))
+      self.chainPulldowns[dim].setCallback(partial(self.setNmrChain))
+      sequenceCodes = [nmrResidue.sequenceCode for nmrResidue in self.project.nmrResidues]
+      self.seqCodePulldowns[dim].setData(sorted(sequenceCodes, key=self.natural_key))
+      # self.seqCodePulldowns[dim].setIndex(self.seqCodePulldowns[dim].texts.index(sequenceCode))
+      self.seqCodePulldowns[dim].setCallback(partial(self.setSequenceCode))
+      residueTypes = [code.upper() for code in CCP_CODES] + ['']
+      self.resTypePulldowns[dim].setData(residueTypes)
+      # self.resTypePulldowns[dim].setIndex(self.resTypePulldowns[dim].texts.index(residueType.upper()))
+      self.resTypePulldowns[dim].setCallback(partial(self.setResidueType))
+      atomPrefix = self.peaks[0].peakList.spectrum.isotopeCodes[dim][-1]
+      atomNames = [atomName for atomName in ATOM_NAMES if atomName[0] == atomPrefix] + [nmrAtom.name]
+      self.atomTypePulldowns[dim].setData(atomNames)
+      # self.atomTypePulldowns[dim].setIndex(self.atomTypePulldowns[dim].texts.index(nmrAtom.name))
+      self.atomTypePulldowns[dim].setCallback(partial(self.setAtomType))
+    # print(self.project._appBase.current.nmrResidue, 'currentNmrResidue AssignmentModule')
+    else:
+      self.chainPulldowns[dim].setData([])
+      self.seqCodePulldowns[dim].setData([])
+      self.resTypePulldowns[dim].setData([])
+      self.atomTypePulldowns[dim].setData([])
+
+
+
+
+class New(object):
+  '''Small 'fake' object to get a non-nmrAtom in the objectTable.
+     Maybe this should be solved differently. It works well though.
+
+  '''
+
+  def __init__(self):
+    self.pid = 'New NMR Atom'
+
+
+class NotOnLine(object):
+  '''Small 'fake' object to get a message the user in the assignment
+     Table that a specific dimension can not be assigned in one go
+     since the frequencies of the peaks in this dimension are not on
+     one line (i.e. the C frequencies of the CA and CB in a strip for
+     instance).
+     Maybe this should be solved differently. It works well though.
+
+  '''
+
+  def __init__(self):
+    self.pid = 'Multiple selected peaks not on line.'
+
+NEW = New()
+NOL = NotOnLine()

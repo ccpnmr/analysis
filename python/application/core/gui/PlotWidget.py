90fe5ae3d2352919ab97c37822b09623c32143c3
Luca Mureddu <lgm8@leicester.ac.uk>
2015-11-13 12:31:52 +0000

    Added function to display SampleComponent/substance  reference spectrum
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8841 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/application/core/gui/PlotWidget.py
+++ python/application/core/gui/PlotWidget.py
@@ -77,6 +77,12 @@ class PlotWidget(DropBase, pg.PlotWidget, Base):
                                  'module.displaySpectrum', 'spectrum', [ss, displayPid])
 
   def processSamples(self, pids:Sequence[str], event):
+    """Display sample spectra defined by list of Pid strings"""
     for ss in pids:
       spectrumPids = [spectrum.pid for spectrum in self._appBase.project.getByPid(ss).spectra]
       self.processSpectra(spectrumPids, event)
+
+  def processSampleComponents(self, pids:Sequence[str], event):
+    """Display sampleComponent spectrum defined by its Pid string"""
+    sampleComponent = self._appBase.project.getByPid(pids[0])
+    self.processSpectra([sampleComponent.substance.referenceSpectra[0].pid], event)

44682db96cd0edbc5946181097f1c8f9e261cda0
Luca Mureddu <lgm8@leicester.ac.uk>
2016-01-19 12:38:15 +0000

    Added metabolomics application
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8946 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ python/application/Metabolomics.py
@@ -0,0 +1,19 @@
+__author__ = 'luca'
+
+
+from application.core.AppBase import startProgram
+from application.core.AppBase import AppBase
+from application.core.Version import applicationVersion
+
+applicationName = 'Metabolomics'
+
+class Metabolomics(AppBase):
+  pass
+if __name__ == '__main__':
+  import argparse
+
+  parser = argparse.ArgumentParser(description='Process startup arguments')
+  parser.add_argument('--language', help='Language for menus, etc.')
+  parser.add_argument('projectPath', nargs='?', help='Project path')
+  args = parser.parse_args()
+  startProgram(Metabolomics, applicationName, applicationVersion, args.projectPath, args.language)

50a0f4e4102d02b8333633ccf68d765cf8a2a2d6
Simon Skinner <ss885@leicester.ac.uk>
2016-01-29 14:51:46 +0000

    added new modules
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@8991 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ python/application/metabolomics/Integration.py
@@ -0,0 +1,98 @@
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - : 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__credits__ = "Wayne Boucher, Rasmus H Fogh, Simon P Skinner, Geerten W Vuister"
+__license__ = ("CCPN license. See www.ccpn.ac.uk/license"
+               "or ccpncore.memops.Credits.CcpnLicense for license text")
+__reference__ = ("For publications, please use reference from www.ccpn.ac.uk/license"
+                 " or ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification:
+#=========================================================================================
+__author__ = ": rhfogh $"
+__date__ = ": 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__version__ = ": 7686 $"
+
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - : 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__credits__ = "Wayne Boucher, Rasmus H Fogh, Simon P Skinner, Geerten W Vuister"
+__license__ = ("CCPN license. See www.ccpn.ac.uk/license"
+               "or ccpncore.memops.Credits.CcpnLicense for license text")
+__reference__ = ("For publications, please use reference from www.ccpn.ac.uk/license"
+                 " or ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification:
+#=========================================================================================
+__author__ = ": rhfogh $"
+__date__ = ": 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__version__ = ": 7686 $"
+
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+from PyQt4 import QtCore, QtGui
+
+from ccpncore.gui.Base import Base
+from ccpncore.gui.Button import Button
+from ccpncore.gui.Label import Label
+from ccpncore.gui.LineEdit import LineEdit
+from ccpncore.gui.RadioButton import RadioButton
+
+from application.core.modules.GuiTableGenerator import GuiTableGenerator
+from application.core.modules.PeakTable import PeakListSimple
+
+
+class IntegrationWidget(QtGui.QWidget, Base):
+
+  def __init__(self, parent=None, project=None, **kw):
+    QtGui.QWidget.__init__(self, parent)
+    Base.__init__(self, **kw)
+    self.pickButtonLabel = Label(self, 'Pick', grid=(0, 0))
+    self.pickOnSpectrumButton = Button(self, grid=(0, 1), toggle=True, icon='iconsNew/target3+',hPolicy='fixed')
+    self.currentAreaLabel = Label(self, 'Current Area ID ', grid=(0, 2))
+    self.idLineEdit = LineEdit(self, grid=(0, 3))
+    self.rangeLabel1 = Label(self, 'range ', grid=(0, 4))
+    self.rangeLabel2 = Label(self, 'Insert range here', grid=(0, 5))
+    self.peakInAreaLabel = Label(self, 'Peaks in Area ', grid=(1, 0))
+    columns = [('#', 'serial'), ('Height', lambda pk: self.getPeakHeight(pk)),
+               ('Volume', lambda pk: self.getPeakVolume(pk)),
+               ('Details', 'comment')]
+
+    tipTexts=['Peak serial number', 'Magnitude of spectrum intensity at peak center (interpolated), unless user edited',
+              'Integral of spectrum intensity around peak location, according to chosen volume method',
+              'Textual notes about the peak']
+    self.peakTable = GuiTableGenerator(self, objectLists=project.peakLists, columns=columns,
+                                       selector=None, tipTexts=tipTexts, callback=self.tableCallback)
+    self.layout().addWidget(self.peakTable, 1, 1, 3, 5)
+
+  def tableCallback(self):
+    pass
+
+class IntegrationTable(QtGui.QWidget, Base):
+
+  def __init__(self, parent=None, project=None, **kw):
+    QtGui.QWidget.__init__(self, parent)
+    Base.__init__(self, **kw)
+
+    self.radioButton1 = RadioButton(self, grid=(0, 0), hAlign='r')
+    self.label1 = Label(self, 'By Spectrum', grid=(0, 1), hAlign='l')
+    self.radioButton2 = RadioButton(self, grid=(0, 2), hAlign='r')
+    self.label2 = Label(self, 'By Area', grid=(0, 3), hAlign='l')
+    self.peakList = PeakListSimple(self, project, grid=(1, 0), gridSpan=(1, 4))
+    self.peakList.subtractPeakListsButton.hide()
---
+++ python/application/metabolomics/PeakAssignment.py
@@ -0,0 +1,111 @@
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - : 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__credits__ = "Wayne Boucher, Rasmus H Fogh, Simon P Skinner, Geerten W Vuister"
+__license__ = ("CCPN license. See www.ccpn.ac.uk/license"
+               "or ccpncore.memops.Credits.CcpnLicense for license text")
+__reference__ = ("For publications, please use reference from www.ccpn.ac.uk/license"
+                 " or ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification:
+#=========================================================================================
+__author__ = ": rhfogh $"
+__date__ = ": 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__version__ = ": 7686 $"
+
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - : 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__credits__ = "Wayne Boucher, Rasmus H Fogh, Simon P Skinner, Geerten W Vuister"
+__license__ = ("CCPN license. See www.ccpn.ac.uk/license"
+               "or ccpncore.memops.Credits.CcpnLicense for license text")
+__reference__ = ("For publications, please use reference from www.ccpn.ac.uk/license"
+                 " or ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification:
+#=========================================================================================
+__author__ = ": rhfogh $"
+__date__ = ": 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__version__ = ": 7686 $"
+
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+from PyQt4 import QtCore, QtGui
+
+from ccpncore.gui.Base import Base
+from ccpncore.gui.Button import Button
+from ccpncore.gui.Dock import CcpnDock
+from ccpncore.gui.DoubleSpinbox import DoubleSpinbox
+from ccpncore.gui.Label import Label
+from ccpncore.gui.Table import ObjectTable, Column
+from ccpncore.gui.PulldownList import PulldownList
+from ccpncore.gui.RadioButton import RadioButton
+from ccpncore.gui.Spinbox import Spinbox
+
+from application.core.modules.GuiTableGenerator import GuiTableGenerator
+from application.core.modules.PeakTable import PeakListSimple
+
+import pyqtgraph as pg
+
+class PeakAssignment(QtGui.QWidget, Base):
+
+  def __init__(self, parent=None, project=None, **kw):
+    QtGui.QWidget.__init__(self, parent)
+    Base.__init__(self, **kw)
+
+    self.peaksLabel = Label(self, 'Peaks', grid=(0, 0), gridSpan=(1, 3))
+    self.assignLabel = Label(self, 'Fit', grid=(1, 4))
+    self.assignButton = Button(self, '<--', grid=(2, 4))
+    self.assignAndMoveButton = Button(self, '<-- + A', grid=(3, 4))
+    self.deassignLabel = Label(self, 'Deassign', grid=(1, 5))
+    self.deassignButton = Button(self, 'X', grid=(2, 5))
+    self.deassignAndMoveButton = Button(self, 'X + P', grid=(3, 5))
+    self.suggestionSourceLabel = Label(self, 'Suggestion Source ', grid=(0, 6), gridSpan=(1, 1))
+    self.suggestionSourcePulldown = PulldownList(self, grid=(0, 7), gridSpan=(1, 2))
+    self.peakTable = PeakListSimple(self, project, callback=self.callback,  grid=(1, 0), gridSpan=(4, 3))
+    self.substanceTable = SubstanceTable(self, grid=(1, 6), gridSpan=(4, 3))
+
+  def callback(self):
+    pass
+
+
+class SubstanceTable(QtGui.QWidget, Base):
+  def __init__(self, parent=None, **kw):
+    QtGui.QWidget.__init__(self, parent)
+    Base.__init__(self, **kw)
+
+    substanceTableColumns = [Column('substance', 'substance'), Column('atom', 'atom'), Column('cs', 'cs')]
+
+    tipTexts2 = ['substance Id', 'substance atom', 'substance cs']
+    substanceList = [Substance('1', '2', '3'), Substance('1', '2', '3'), Substance('1', '2', '3')]
+    self.substanceLists = [substanceList]
+
+    self.substanceTable = ObjectTable(self, columns=substanceTableColumns,
+                              callback=self.substanceCallback,
+                              objects=substanceList)
+
+  def substanceCallback(self):
+    pass
+
+class Substance(object):
+  def __init__(self, substance, atom, cs):
+    self.substance = substance
+    self.atom = atom
+    self.cs = cs

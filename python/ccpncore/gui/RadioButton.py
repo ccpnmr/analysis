05a2dd10d4ac148cf1cd59e230ad23132216caac
Simon Skinner <ss885@leicester.ac.uk>
2016-02-08 11:58:47 +0000

    add
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9016 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpncore/gui/RadioButton.py
+++ python/ccpncore/gui/RadioButton.py
@@ -7,7 +7,7 @@ from ccpncore.gui.Base import Base
 
 class RadioButton(QtGui.QRadioButton, Base):
 
-  def __init__(self, parent, text='',textColor=None, textSize=None, **kw):
+  def __init__(self, parent, text='', textColor=None, textSize=None, callback=None, **kw):
 
     QtGui.QRadioButton.__init__(self, text, parent)
     Base.__init__(self,  **kw)
@@ -15,6 +15,7 @@ class RadioButton(QtGui.QRadioButton, Base):
       self.setStyleSheet('QRadioButton {color: %s; font-size: 12pt;}' % textColor)
     # if textSize :
     #   self.setStyleSheet('QRadioButton {font-size: %s;}' % textSize)
+    self.setCallback(callback)
 
   def get(self):
 
@@ -23,3 +24,12 @@ class RadioButton(QtGui.QRadioButton, Base):
   def set(self, text=''):
 
     self.setText(self.translate(text))
+
+  def setCallback(self, callback):
+    #
+    # if self.callback:
+    #   self.disconnect(self, QtCore.SIGNAL('clicked()'), self.callback)
+
+    if callback:
+      self.connect(self, QtCore.SIGNAL('clicked()'), callback)
+      # self.clicked.connect doesn't work with lambda, yet...

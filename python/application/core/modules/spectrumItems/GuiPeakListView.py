ad4114c49f4e1237122b69849098377810c72650
TJ Ragan <tjr22@leicester.ac.uk>
2016-02-29 14:17:19 +0000

    Correct call for 1D annotations
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9082 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/application/core/modules/spectrumItems/GuiPeakListView.py
+++ python/application/core/modules/spectrumItems/GuiPeakListView.py
@@ -821,7 +821,7 @@ def _refreshPeakAnnotation(peak:Peak):
       guiPeakListView = data2Obj[apiStripPeakListView]
       peakItem = guiPeakListView.peakItems.get(peak)
       if peakItem:
-        peakItem.annotation.setupPeakAnnotationItem(peakItem)
+        peakItem.annotation.setupPeakAnnotation(peakItem)
 
 Peak._refreshPeakAnnotation = _refreshPeakAnnotation
 

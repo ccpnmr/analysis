8b050c8a95920fe2dd9c220d90fabcd27c166af6
Luca Mureddu <lgm8@leicester.ac.uk>
2016-03-07 12:23:51 +0000

    Added a spectrumGroups toolbar, and spectrumGroup button
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9117 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/application/metabolomics/SpectrumGroupsWidget.py
+++ python/application/metabolomics/SpectrumGroupsWidget.py
@@ -23,72 +23,96 @@ __version__ = ": 7686 $"
 #=========================================================================================
 
 from PyQt4 import QtCore, QtGui
-
 from ccpncore.gui.Base import Base
 from ccpncore.gui.Button import Button
-from ccpncore.gui.CheckBox import CheckBox
-from ccpncore.gui.DoubleSpinbox import DoubleSpinbox
-from ccpncore.gui.Label import Label
-from ccpncore.gui.Table import ObjectTable, Column
-from ccpncore.gui.PulldownList import PulldownList
-from ccpncore.gui.RadioButton import RadioButton
-from ccpncore.gui.Spinbox import Spinbox
 
-from application.core.modules.GuiTableGenerator import GuiTableGenerator
-from application.core.modules.PeakTable import PeakListSimple
 
-import pyqtgraph as pg
-
-from functools import partial
-
-class SpectrumGroupsWidget(QtGui.QWidget, Base):
-  def __init__(self, parent=None, project=None, strip=None, **kw):
+class SpectrumGroupsToolBar(QtGui.QToolBar, Base):
+  def __init__(self, parent=None, project=None, strip=None, spectrumGroupPid=None, **kw):
     QtGui.QWidget.__init__(self, parent)
     Base.__init__(self, **kw)
 
-    self.button = Button(self, 'Setup SpectrumGroup Display', grid=(0, 0), callback=self.showSpectrumGroupPopup)
-    self.project = project
-    self.strip = strip
+    spectrumGroupButton = SpectrumGroupsWidget(self, project=project, strip=strip, spectrumGroupPid=spectrumGroupPid)
+    self.addWidget(spectrumGroupButton)
 
-  def showSpectrumGroupPopup(self):
-    popup = SpectrumGroupsPopup(project=self.project, strip=self.strip)
-    popup.exec_()
-    popup.raise_()
-
-
-
-class SpectrumGroupsPopup(QtGui.QDialog, Base):
-  def __init__(self, parent=None, project=None, strip=None, **kw):
+class SpectrumGroupsWidget(QtGui.QWidget):
+  def __init__(self, parent=None, project=None, strip=None, spectrumGroupPid=None, **kw):
     QtGui.QWidget.__init__(self, parent)
-    Base.__init__(self, **kw)
-
-    self.strip = strip
-    self.checkBoxes = []
     self.project = project
-    for i, group in enumerate(project.spectrumGroups):
-      newCheckbox = CheckBox(self, grid=(i, 0))
-      newLabel = Label(self, group.pid, grid=(i, 1))
-      newCheckbox.toggled.connect(partial(self.toggleSpectrumGroups, group, i))
-      self.checkBoxes.append(newCheckbox)
-      views = [spectrumView for spectrumView in self.strip.spectrumViews if spectrumView.spectrum in group.spectra]
-      newCheckbox.setChecked(all(view.isVisible() for view in views))
-        # group.isVisible = True
-      # else:
-      #   group.isVisible = False
-      # if group.isVisible:
-      #   (True)
-      # else:
-      #   newCheckbox.setChecked(False)
-      newCheckbox.toggled.connect(partial(self.toggleSpectrumGroups, group, i))
+    self.strip = strip
+    self.setColours()
+    self.spectrumGroup = self.project.getByPid(spectrumGroupPid)
+    self.spectrumGroupButton = Button(self, text=self.spectrumGroup.id,toggle=True)
+    self.spectrumGroupButton.setChecked(True)
+    self.spectrumGroupButton.setMinimumSize(40,33)
+    self.spectrumGroupButton.toggled.connect(self.toggleSpectrumGroups)
 
-  def toggleSpectrumGroups(self, spectrumGroup, index):
+    self.spectrumGroupButton.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+    self.spectrumGroupButton.customContextMenuRequested.connect(self.onContextMenu)
 
-    views = [spectrumView for spectrumView in self.strip.spectrumViews if spectrumView.spectrum in spectrumGroup.spectra]
-    if self.checkBoxes[index].isChecked():
-      for view in views:
-        view.setVisible(True)
-        view.plot.show()
+    self.popMenu = QtGui.QMenu(self)
+    self.popMenu.addAction(QtGui.QAction('Delete',self, triggered=self.deleteSpectrumGroup))
+    self.peakListCheckBox = QtGui.QAction('PeakLists',self, checkable = True, triggered=self.showHidePeakListView)
+    self.peakListCheckBox.setChecked(True)
+    self.popMenu.addAction(self.peakListCheckBox)
+
+    self.spectrumGroupPeakLists = [spectrum.peakLists[0] for spectrum in self.spectrumGroup.spectra]
+    self.peakListViews = [peakListView for peakListView in self.strip.spectrumDisplay.peakListViews ]
+    self.peakListViewDisplayed = [peakListView.peakList for peakListView in self.strip.spectrumDisplay.peakListViews ]
+
+  def onContextMenu(self, point):
+    self.popMenu.exec_(self.spectrumGroupButton.mapToGlobal(point))
+
+  def toggleSpectrumGroups(self):
+    spectrumViews = [spectrumView for spectrumView in self.strip.spectrumViews
+                     if spectrumView.spectrum in self.spectrumGroup.spectra]
+
+    if self.spectrumGroupButton.isChecked():
+      for spectrumView in spectrumViews:
+        spectrumView.setVisible(True)
+        spectrumView.plot.show()
+      self.showPeakList()
+
     else:
-      for view in views:
-        view.setVisible(False)
-        view.plot.hide()
+      for spectrumView in spectrumViews:
+        spectrumView.setVisible(False)
+        spectrumView.plot.hide()
+      self.hidePeakLists()
+
+  def deleteSpectrumGroup(self):
+    self.spectrumGroupButton.deleteLater()
+    for spectrumView in self.strip.spectrumViews:
+      if spectrumView.spectrum in self.spectrumGroup.spectra:
+        spectrumView.delete()
+
+  def showHidePeakListView(self):
+    if self.peakListCheckBox.isChecked():
+      self.showPeakList()
+    else:
+      self.hidePeakLists()
+
+  def hidePeakLists(self):
+    for peakList in self.spectrumGroupPeakLists:
+      for peakListView in self.strip.spectrumDisplay.peakListViews:
+        if peakList == peakListView.peakList:
+          peakListView.setVisible(False)
+
+  def showPeakList(self):
+    for peakList in self.spectrumGroupPeakLists:
+      for peakListView in self.strip.spectrumDisplay.peakListViews:
+        if peakList == peakListView.peakList:
+          peakListView.setVisible(True)
+
+  def setColours(self):
+
+    self.colourScheme = self.project._appBase.preferences.general.colourScheme
+    if self.colourScheme == 'dark':
+      self.setStyleSheet("""
+                      Button::checked {background-color: #020F31;}
+                      Button {background-color: #2a3358; }
+                      """)
+    else:
+      self.setStyleSheet("""
+                      Button::checked {background-color: #bd8413;}
+                      Button {background-color: #fbf4cc; border: 1px solid  #bd8413; color: #122043}
+                      """)

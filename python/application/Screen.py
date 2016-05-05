f6876b64f896f7a08fc5dc1c5d38ed30da1e5ab3
Luca Mureddu <lgm8@leicester.ac.uk>
2016-05-05 15:08:33 +0000

    new start up
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9237 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/application/Screen.py
+++ python/application/Screen.py
@@ -1,18 +1,26 @@
-from application.core.AppBase import startProgram
-from application.core.AppBase import AppBase
+
+
+from application.core.AppBase import defineProgramArguments, AppBase
 from ccpn.lib.Version import applicationVersion
 
 applicationName = 'Screen'
 
-class Screen(AppBase):
-  pass
-if __name__ == '__main__':
-  import argparse
-  components = {'Screen'}
 
-  parser = argparse.ArgumentParser(description='Process startup arguments')
-  parser.add_argument('--language', help='Language for menus, etc.')
-  parser.add_argument('projectPath', nargs='?', help='Project path')
-  args = parser.parse_args()
-  startProgram(Screen, applicationName, applicationVersion, components=components,
-               projectPath=args.projectPath, language=args.language)
+class Screen(AppBase):
+  """Root class for Assign application"""
+
+  def __init__(self, applicationName, applicationVersion, commandLineArguments):
+    AppBase.__init__(self, applicationName, applicationVersion, commandLineArguments)
+    self.components.add('Screen')
+
+
+if __name__ == '__main__':
+
+  # argument parser
+  parser = defineProgramArguments()
+
+  # add any additional commandline argument here
+  commandLineArguments = parser.parse_args()
+
+  program = Screen(applicationName, applicationVersion, commandLineArguments)
+  program.start()

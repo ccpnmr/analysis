39fa3f4f58b345dc7109a3f1220398d903ab8e1b
Luca Mureddu <lgm8@leicester.ac.uk>
2016-05-05 15:28:31 +0000

    Implemented drag and drop functions
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9240 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- python/ccpncore/gui/ListWidget.py
+++ python/ccpncore/gui/ListWidget.py
@@ -7,18 +7,25 @@ from ccpncore.gui.Menu import Menu
 
 class ListWidget(QtGui.QListWidget, Base):
 
-  def __init__(self, parent, callback=None, rightMouseCallback=None, **kw):
+  def __init__(self, parent, callback=None, rightMouseCallback=None, contextMenu=None, **kw):
 
     QtGui.QListWidget.__init__(self, parent)
     Base.__init__(self, **kw)
+
+    self.setDragDropMode(QtGui.QAbstractItemView.DragDrop)
+    self.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
+    self.setAcceptDrops(False)
+    self.contextMenu = contextMenu
     self.callback = callback
     self.rightMouseCallback = rightMouseCallback
     if callback is not None:
       self.itemClicked.connect(callback)
 
+    self.contextMenuItem = 'Delete'
 
-  def contextCallback(self):
-    self.removeItem()
+  def contextCallback(self, remove=False):
+    if remove:
+      self.removeItem()
     self.rightMouseCallback()
 
   def removeItem(self):
@@ -27,7 +34,7 @@ class ListWidget(QtGui.QListWidget, Base):
 
   def mousePressEvent(self, event):
     self._mouse_button = event.button()
-    if event.button() == QtCore.Qt.RightButton:
+    if event.button() == QtCore.Qt.RightButton and self.contextMenu:
       self.raiseContextMenu(event)
     elif event.button() == QtCore.Qt.LeftButton:
       if self.itemAt(event.pos()) is None:
@@ -49,3 +56,30 @@ class ListWidget(QtGui.QListWidget, Base):
     else:
       contextMenu.addItem("Delete", callback=self.contextCallback)
     return contextMenu
+
+  def dragEnterEvent(self, event):
+    if event.mimeData().hasUrls():
+      event.accept()
+    else:
+      super(ListWidget, self).dragEnterEvent(event)
+
+  def dragMoveEvent(self, event):
+    if event.mimeData().hasUrls():
+      event.setDropAction(QtCore.Qt.CopyAction)
+      event.accept()
+    else:
+      super(ListWidget, self).dragMoveEvent(event)
+
+  def dropEvent(self, event):
+    if event.mimeData().hasUrls():
+      event.setDropAction(QtCore.Qt.CopyAction)
+      event.accept()
+      links = []
+      for url in event.mimeData().urls():
+        links.append(str(url.toLocalFile()))
+      self.emit(QtCore.SIGNAL("dropped"), links)
+    else:
+      items = []
+      event.setDropAction(QtCore.Qt.MoveAction)
+      self.emit(QtCore.SIGNAL("dropped"), items)
+      super(ListWidget, self).dropEvent(event)

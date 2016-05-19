91950f4becd3a72d8a2ad8dea3ae313136900ee0
Luca Mureddu <lgm8@leicester.ac.uk>
2016-05-19 13:37:56 +0000

    Added Module file
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9365 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ src/python/ccpn/ui/gui/widgets/ModuleArea.py
@@ -0,0 +1,150 @@
+from PyQt4 import QtGui
+from pyqtgraph.dockarea.DockArea import DockArea
+from pyqtgraph.dockarea.Dock import Dock
+from pyqtgraph.dockarea.Container import Container, TContainer, HContainer, VContainer
+
+ModuleArea = DockArea
+Module = Dock
+
+class CcpnModuleArea(ModuleArea):
+  def __init__(self, **kw):
+    super(CcpnModuleArea, self).__init__(self)
+    self.modules = self.docks
+    self.moveModule = self.moveDock
+    # self.closeModule = self.closeDock
+
+
+  def addModule(self, module=None, position=None, relativeTo=None, **kwds):
+    """With these settings the user can close all the modules from the label 'close module' or pop up and
+     when re-add a new module it makes sure there is a container available.
+    """
+
+
+
+    if module is None:
+      module = Module(**kwds)
+
+    if position is None:
+      position = 'top'
+
+    if position == 'bottom':
+      self.checkPythonConsole()
+
+
+    neededContainer = {
+        'bottom': 'vertical',
+        'top': 'vertical',
+        'left': 'horizontal',
+        'right': 'horizontal',
+        'above': 'tab',
+        'below': 'tab'
+        }[position]
+
+
+
+
+    if relativeTo is None:
+      neighbor = None
+      container = self.addContainer(neededContainer, self.topContainer)
+
+    ## Determine the container to insert this module into.
+    ## If there is no neighbor, then the container is the top.
+    if relativeTo is None or relativeTo is self:
+      if self.topContainer is None:
+        container = self
+        neighbor = None
+      else:
+        container = self.topContainer
+        neighbor = None
+    else:
+      if isinstance(relativeTo, str):
+          relativeTo = self.modules[relativeTo]
+      container = self.getContainer(relativeTo)
+      neighbor = relativeTo
+
+
+    if neededContainer != container.type() and container.type() == 'tab':
+        neighbor = container
+        container = container.container()
+
+    if neededContainer != container.type():
+
+        if neighbor is None:
+          container = self.addContainer(neededContainer, self.topContainer)
+        else:
+          container = self.addContainer(neededContainer, neighbor)
+
+    insertPos = {
+        'bottom': 'after',
+        'top': 'before',
+        'left': 'before',
+        'right': 'after',
+        'above': 'before',
+        'below': 'after'
+    }[position]
+    if container is not None:
+      container.insert(module, insertPos, neighbor)
+    else:
+      container = self.topContainer
+      container.insert(module, insertPos, neighbor)
+    module.area = self
+    self.modules[module.name()] = module
+    # self.movePythonConsole()
+    return module
+
+  # def movePythonConsole(self):
+  #   if 'PYTHON CONSOLE' in self.findAll()[1]:
+  #     pythonConsole = self.findAll()[1]['PYTHON CONSOLE']
+  #     for container in self.findAll()[0]:
+  #       if container and pythonConsole is not None:
+  #         self.moveModule(pythonConsole, 'bottom', container)
+
+  def makeContainer(self, typ):
+    if typ == 'vertical':
+      new = VContainer(self)
+    elif typ == 'horizontal':
+      new = HContainer(self)
+    elif typ == 'tab':
+      new = TabContainer(self)
+    return new
+
+
+  def apoptose(self):
+    if self.temporary and self.topContainer.count() == 0:
+      self.topContainer = None
+      if self.home:
+        self.home.removeTempArea(self)
+
+  def checkPythonConsole(self):
+    if 'PYTHON CONSOLE' in self.findAll()[1]:
+      pythonConsole = self.findAll()[1]['PYTHON CONSOLE']
+      # for c in self.findAll()[0]:
+      #   if c and pythonConsole is not None:
+      #     print(c, 'Testing')
+
+
+class TabContainer(TContainer):
+  ''' This subclass allows to set the orientation of the stacked 'tab' to vertical
+  '''
+  def __init__(self, area):
+    QtGui.QWidget.__init__(self)
+    Container.__init__(self, area)
+    self.layout = QtGui.QGridLayout()
+    self.layout.setSpacing(0)
+    self.layout.setContentsMargins(0, 0, 0, 0)
+    self.setLayout(self.layout)
+
+    self.hTabLayout = self.vTabLayout = QtGui.QVBoxLayout() #this ovveride the original tab orientation in PyGraph
+
+    self.vTabBox = QtGui.QWidget()
+    self.vTabBox.setLayout(self.vTabLayout)
+    self.vTabLayout.setSpacing(2)
+    self.vTabLayout.setContentsMargins(0, 0, 0, 0)
+    self.layout.addWidget(self.vTabBox, 0, 0)
+
+    self.stack = QtGui.QStackedWidget()
+    self.layout.addWidget(self.stack, 0, 1)
+    self.stack.childEvent = self.stackChildEvent
+
+    for n in ['count', 'widget', 'indexOf']:
+      setattr(self, n, getattr(self.stack, n))

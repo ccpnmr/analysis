040d55a21f1312f7ae91935ccccf01eaaa7c0ee6
Simon Skinner <ss885@leicester.ac.uk>
2016-06-17 14:55:37 +0000

    addition of axis flipping macros
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9477 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ src/python/ccpn/macros/FlipXYCurrentStrip.py
@@ -0,0 +1,39 @@
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - : 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__credits__ = "Wayne Boucher, Rasmus H Fogh, Simon P Skinner, Geerten W Vuister"
+__license__ = ("CCPN license. See www.ccpn.ac.uk/license"
+               "or ccpncore.memops.Credits.CcpnLicense for license text")
+__reference__ = ("For publications, please use reference from www.ccpn.ac.uk/license"
+                 " or ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification:
+#=========================================================================================
+__author__ = ": rhfogh $"
+__date__ = ": 2014-06-04 18:13:10 +0100 (Wed, 04 Jun 2014) $"
+__version__ = ": 7686 $"
+
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+nDim = len(current.strip.axisOrder)
+if nDim < 2:
+  print('Too few dimensions for XZ flip')
+
+else:
+
+  axisOrder = [current.strip.axisOrder[1], current.strip.axisOrder[0]]
+
+  if nDim > len(axisOrder):
+    axisOrder = axisOrder + current.strip.axisOrder[2:]
+
+  spectra = current.strip.spectra
+  newDisplay = application.createSpectrumDisplay(spectra[0], axisOrder=axisOrder)
+  for spectrum in spectra:
+    newDisplay.displaySpectrum(spectrum)

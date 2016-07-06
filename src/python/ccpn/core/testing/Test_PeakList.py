b6a9357c65206090da5b77c573e44198160b0617
TJ Ragan <tjr22@leicester.ac.uk>
2016-07-06 09:27:10 +0000

    Fixed peakList creation testing.
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9577 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- src/python/ccpn/core/testing/Test_PeakList.py
+++ src/python/ccpn/core/testing/Test_PeakList.py
@@ -23,15 +23,44 @@ __version__ = "$Revision$"
 #=========================================================================================
 from ccpn.core.testing.WrapperTesting import WrapperTesting
 
-class PeakListTest(WrapperTesting):
+# class PeakListTest(WrapperTesting):
+#
+#   # Path of project to load (None for new project)
+#   projectPath = 'CcpnCourse1b'
+#
+#   def test_newPeakList(self):
+#     spectrum = self.project.getSpectrum('HSQC-115')
+#     peakList = spectrum.newPeakList()
+#     # Undo and redo all operations
+#     self.undo.undo()
+#     self.undo.redo()
 
-  # Path of project to load (None for new project)
-  projectPath = 'CcpnCourse1b'
-    
+
+# Properly done version of above
+class PeakListCreationTest(WrapperTesting):
+  def setUp(self):
+    with self.initialSetup():
+      self.spectrum = self.project.createDummySpectrum('H')
+
+
   def test_newPeakList(self):
-    spectrum = self.project.getSpectrum('HSQC-115')
-    peakList = spectrum.newPeakList()
-    # Undo and redo all operations
+    self.assertEqual(len(self.spectrum.peakLists), 1)
+
+    peakList = self.spectrum.newPeakList()
+
+    self.assertEqual(len(self.spectrum.peakLists), 2)
+    self.assertEqual(peakList.className, 'PeakList')
+    self.assertIs(self.spectrum.peakLists[1], peakList)
+
+
+  def test_newPeakList_UndoRedo(self):
+    peakList = self.spectrum.newPeakList()
+
+    self.assertEqual(len(self.spectrum.peakLists), 2)
     self.undo.undo()
+    self.assertEqual(len(self.spectrum.peakLists), 1)
+
     self.undo.redo()
+    self.assertEqual(len(self.spectrum.peakLists), 2)
+    self.assertIs(self.spectrum.peakLists[1], peakList)
 

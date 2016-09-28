6357171d6bb7a58ebd130d76fd5ca8f81fed9ea7
Wayne Boucher <wb104@cam.ac.uk>
2016-09-28 14:42:52 +0000

    move
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9902 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ src/python/ccpn/ui/gui/popups/ProjectSummaryPopup.py
@@ -0,0 +1,135 @@
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - $Date: 2016-09-07 12:42:52 +0100 (Wed, 07 Sep 2016) $"
+__credits__ = "Wayne Boucher, Rasmus H Fogh, Simon P Skinner, Geerten W Vuister"
+__license__ = ("CCPN license. See www.ccpn.ac.uk/license"
+              "or ccpnmodel.ccpncore.memops.Credits.CcpnLicense for license text")
+__reference__ = ("For publications, please use reference from www.ccpn.ac.uk/license"
+                " or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification:
+#=========================================================================================
+__author__ = "$Author: skinnersp $"
+__date__ = "$Date: 2016-09-07 12:42:52 +0100 (Wed, 07 Sep 2016) $"
+__version__ = "$Revision: 9852 $"
+
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+from PyQt4 import QtGui, QtCore
+
+from ccpn.core.lib import Summary
+
+from ccpn.ui.gui.widgets.Frame import Frame
+from ccpn.ui.gui.widgets.Label import Label
+from ccpn.ui.gui.widgets.Table import ObjectTable, Column
+
+
+class ProjectSummaryPopup(QtGui.QDialog):
+
+  def __init__(self, project, parent=None, title='Project Summary', modal=False):
+
+    self.project = project
+
+    QtGui.QDialog.__init__(self, parent=parent)
+    if modal:  # Set before visible
+      modality = QtCore.Qt.ApplicationModal
+      self.setWindowModality(modality)
+    self.setWindowTitle(title)
+
+    self._setupData()
+
+    frame = Frame(self)
+
+    row = 0
+
+    # SPECTRA
+
+    label = Label(frame, text='Spectra', grid=(row, 0), hAlign='l')
+    row += 1
+
+    columns = [
+      Column('#', lambda spectrum: self.spectrumNumberDict[spectrum], tipText='Number'),
+      Column('Id', lambda spectrum: spectrum.id, tipText='Spectrum id'),
+      Column('Dimension count', lambda spectrum: spectrum.dimensionCount, tipText='Spectrum dimension count'),
+      Column('Chemical shiftList',
+             lambda spectrum: spectrum.chemicalShiftList.id, tipText='Spectrum chemical shiftList'),
+      Column('File path', lambda spectrum: spectrum.filePath, tipText='Spectrum data file path'),
+    ]
+    self.spectrumTable = ObjectTable(frame, columns=columns, objects=self.spectra, grid=(row, 0))
+    row += 1
+
+    # PEAKLISTS
+
+    label = Label(frame, text='PeakLists', grid=(row, 0), hAlign='l')
+    row += 1
+
+    columns = [
+      Column('#', lambda peakList: self.peakListNumberDict[peakList], tipText='Number'),
+      Column('Id', lambda peakList: peakList.id, tipText='PeakList id'),
+      Column('Peak count', lambda peakList: len(peakList.peaks), tipText='Number of peaks in peakList'),
+      Column('Partly assigned count', Summary.partlyAssignedPeakCount,
+             tipText='Number of peaks in peakList at least partially assigned'),
+      Column('Partly assigned %', Summary.partlyAssignedPeakPercentage,
+             tipText='Percentage of peaks in peakList at least partially assigned'),
+      Column('Fully assigned count', Summary.fullyAssignedPeakCount,
+             tipText='Number of peaks in peakList fully assigned'),
+      Column('Fully assigned %', Summary.fullyAssignedPeakPercentage,
+             tipText='Percentage of peaks in peakList fully assigned'),
+    ]
+
+    self.peakListTable = ObjectTable(frame, columns=columns, objects=self.peakLists, grid=(row, 0))
+    row += 1
+
+    # CHAINS
+
+    label = Label(frame, text='Chains', grid=(row, 0), hAlign='l')
+    row += 1
+
+    columns = [
+      Column('#', lambda chain: self.chainNumberDict[chain], tipText='Number'),
+      Column('Id', lambda chain: chain.id, tipText='Chain id'),
+      Column('Residue count', lambda chain: len(chain.residues), tipText='Number of residues in chain'),
+      Column('Assignable atom count', Summary.assignableAtomCount,
+             tipText='Number of atoms in chain which are assignable to'),
+      Column('Assigned atom count', Summary.assignedAtomCount,
+             tipText='Number of atoms in chain which are assigned to'),
+      Column('Assigned atom %', Summary.assignedAtomPercentage,
+             tipText='Percentage of atoms in chain which are assigned to'),
+    ]
+
+    self.chainTable = ObjectTable(frame, columns=columns, objects=self.chains, grid=(row, 0))
+    row += 1
+
+  def _setupData(self):
+
+    # SPECTRA
+
+    self.spectra = self.project.spectra
+    self.spectrumNumberDict = {}
+    for n, spectrum in enumerate(self.spectra):
+      self.spectrumNumberDict[spectrum] = n+1
+
+    # PEAKLISTS
+
+    self.peakLists = []
+    self.peakListNumberDict = {}
+    n = 1
+    for spectrum in self.spectra:
+      self.peakLists.extend(spectrum.peakLists)
+      for peakList in spectrum.peakLists:
+        self.peakListNumberDict[peakList] = n
+        n += 1
+
+    # CHAINS
+
+    self.chains = self.project.chains
+    self.chainNumberDict = {}
+    for n, chain in enumerate(self.chains):
+      self.chainNumberDict[chain] = n+1

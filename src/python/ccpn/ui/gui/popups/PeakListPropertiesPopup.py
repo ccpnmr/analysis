aef74340d6faa960aafdd330097f7226cd956ba5
Wayne Boucher <wb104@cam.ac.uk>
2016-11-01 12:13:08 +0000

    Fix situation where no peakListViews (so spectrum not being displayed)
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9961 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- src/python/ccpn/ui/gui/popups/PeakListPropertiesPopup.py
+++ src/python/ccpn/ui/gui/popups/PeakListPropertiesPopup.py
@@ -10,11 +10,31 @@ from ccpn.ui.gui.widgets.PulldownList import PulldownList
 
 from ccpn.util.Colour import spectrumColours
 
+def _getColour(peakList, peakListViews, attr):
+
+  if peakListViews:
+    colour = getattr(peakListViews[0], attr)
+  else:
+    colour = getattr(peakList, attr)
+
+  if not colour:
+    colour = peakList.spectrum.positiveContourColour
+
+  return colour
+
 class PeakListPropertiesPopup(QtGui.QDialog, Base):
   def __init__(self, parent=None, peakList=None, **kw):
+    # DANGER: above allows peakList to be None but code below assumes it is not None
     super(PeakListPropertiesPopup, self).__init__(parent)
     Base.__init__(self, **kw)
+    self.peakList = peakList
     self.peakListViews = [peakListView for peakListView in peakList.project.peakListViews if peakListView.peakList == peakList]
+
+    symbolColour = _getColour(self.peakList, self.peakListViews, 'symbolColour')
+    textColour = _getColour(self.peakList, self.peakListViews, 'textColour')
+    # NOTE: below is not sorted in any way, but if we change that, we also have to change loop in _fillColourPulldown
+    spectrumColourKeys = list(spectrumColours.keys())
+
     self.peakListLabel = Label(self, "PeakList Name ", grid=(0, 0))
     self.peakListLabel = Label(self, peakList.id, grid=(0, 1))
     self.displayedLabel = Label(self, 'Is displayed', grid=(1, 0))
@@ -25,13 +45,13 @@ class PeakListPropertiesPopup(QtGui.QDialog, Base):
     self.symbolColourLabel = Label(self, 'Peak Symbol Colour', grid=(3, 0))
     self.symbolColourPulldownList = PulldownList(self, grid=(3, 1))
     self._fillColourPulldown(self.symbolColourPulldownList)
-    self.symbolColourPulldownList.setCurrentIndex(list(spectrumColours.keys()).index(self.peakListViews[0].symbolColour))
+    self.symbolColourPulldownList.setCurrentIndex(spectrumColourKeys.index(symbolColour))
     self.symbolColourPulldownList.currentIndexChanged.connect(self._changeSymbolColour)
 
     self.textColourLabel = Label(self, 'Peak Text Colour', grid=(4, 0))
     self.textColourPulldownList = PulldownList(self, grid=(4, 1))
     self._fillColourPulldown(self.textColourPulldownList)
-    self.textColourPulldownList.setCurrentIndex(list(spectrumColours.keys()).index(self.peakListViews[0].textColour))
+    self.textColourPulldownList.setCurrentIndex(spectrumColourKeys.index(textColour))
     self.textColourPulldownList.currentIndexChanged.connect(self._changeTextColour)
 
     self.minimalAnnotationLabel = Label(self, 'Minimal Annotation', grid=(5, 0))
@@ -46,12 +66,14 @@ class PeakListPropertiesPopup(QtGui.QDialog, Base):
 
   def _changeSymbolColour(self, value):
     colour = list(spectrumColours.keys())[value]
+    self.peakList.symbolColour = colour
     for peakListView in self.peakListViews:
       peakListView.symbolColour = colour
 
 
   def _changeTextColour(self, value):
     colour = list(spectrumColours.keys())[value]
+    self.peakList.textColour = colour
     for peakListView in self.peakListViews:
       peakListView.textColour = colour
 

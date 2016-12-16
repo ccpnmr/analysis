d8a453f5ee04a61bf4511295696545a49c2b5986
Rasmus Fogh <rhf22@cam.ac.uk>
2016-12-16 12:45:15 +0000

    Added testing for new stringifier functionality
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@10078 8c04fef6-8de3-4ed0-b809-c157a10b9597

---
+++ src/python/ccpn/core/testing/Test_Formatting.py
@@ -0,0 +1,60 @@
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (www.ccpn.ac.uk) 2014 - $Date$"
+__credits__ = "Wayne Boucher, Rasmus H Fogh, Simon Skinner, Geerten Vuister"
+__license__ = ("CCPN license. See www.ccpn.ac.uk/license"
+               "or ccpnmodel.ccpncore.memops.Credits.CcpnLicense for license text")
+__reference__ = ("For publications, please use reference from www.ccpn.ac.uk/license"
+                 " or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification:
+#=========================================================================================
+__author__ = "$Author$"
+__date__ = "$Date$"
+__version__ = "$Revision$"
+
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+
+from ccpn.core.testing.WrapperTesting import WrapperTesting
+from ccpn.util import Common as commonUtil
+
+
+class StringifierTest(WrapperTesting):
+  # Path of project to load (None for new project
+  projectPath = 'CcpnCourse2c'
+
+  def test_defaultFormatting(self):
+    spectrum = self.project.getSpectrum('T2a-216')
+    peak = spectrum.peaks[0]
+    toString = commonUtil.stringifier('serial', 'peakList', 'height', 'volume', 'className',
+                                      'axisCodes', 'position', 'positionError', 'assignedNmrAtoms',
+                                      'dimensionNmrAtoms')
+    self.assertEquals(toString(peak), "<PK:T2a-216.1.1062| serial=1062, peakList=<PL:T2a-216.1>, "
+                                      "height=4.93752e+06, volume=4.12035e+07, className='Peak', "
+                                      "axisCodes=('Hn', 'Nh', 'delay'), "
+                                      "position=(7.90695, 122.516, 3), "
+                                      "positionError=(None, None, None), "
+                                      "assignedNmrAtoms=((<NA:A.56.ALA.H>, <NA:A.56.ALA.N>, None),),"
+                                      " dimensionNmrAtoms=((<NA:A.56.ALA.H>,), (<NA:A.56.ALA.N>,), ())>")
+
+  def test_userFormatting(self):
+    spectrum = self.project.getSpectrum('HSQC-115')
+    peak = spectrum.peaks[0]
+    toString = commonUtil.stringifier('serial', 'peakList', 'height', 'volume', 'className',
+                                      'axisCodes', 'position', 'positionError', 'assignedNmrAtoms',
+                                      'dimensionNmrAtoms', floatFormat='.3f')
+    self.assertEquals(toString(peak),
+                      "<PK:HSQC-115.1.1| serial=1, peakList=<PL:HSQC-115.1>, height=142260384.000,"
+                      " volume=1314892194.000, className='Peak', axisCodes=('Hn', 'Nh'), "
+                      "position=(8.062, 124.933), positionError=(None, None), "
+                      "assignedNmrAtoms=((<NA:A.93.SER.H>, <NA:A.93.SER.N>),), "
+                      "dimensionNmrAtoms=((<NA:A.93.SER.H>,), (<NA:A.93.SER.N>,))>")
+

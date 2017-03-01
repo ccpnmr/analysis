c0d3b72f7c8c20e176843022b426d5ad2dac2969
Luca Mureddu <lgm8@leicester.ac.uk>
2017-03-01 10:07:12 +0000

    For reference only: Added all commonly imported widgets.
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@10152 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- src/python/ccpn/ui/gui/widgets/__init__.py
+++ src/python/ccpn/ui/gui/widgets/__init__.py
@@ -1,1 +1,50 @@
 pass
+
+'''
+All widgets commonly imported. For reference only.
+
+from ccpn.ui.gui.widgets.Action import Action
+from ccpn.ui.gui.widgets.Arrow import Arrow
+from ccpn.ui.gui.widgets.BarGraph import BarGraph
+from ccpn.ui.gui.widgets.Base import Base
+from ccpn.ui.gui.widgets.BasePopup import BasePopup
+from ccpn.ui.gui.widgets.Button import Button
+from ccpn.ui.gui.widgets.ButtonList import ButtonList
+from ccpn.ui.gui.widgets.CallbackTimer import CallbackTimer
+from ccpn.ui.gui.widgets.CcpnGridItem import CcpnGridItem
+from ccpn.ui.gui.widgets.CcpnWebView import CcpnWebView
+from ccpn.ui.gui.widgets.CheckBox import CheckBox
+from ccpn.ui.gui.widgets.ColourDialog import ColourDialog
+from ccpn.ui.gui.widgets.CompoundView import CompoundView
+from ccpn.ui.gui.widgets.DateTime import DateTime
+from ccpn.ui.gui.widgets.DoubleSpinbox import DoubleSpinbox
+from ccpn.ui.gui.widgets.FileDialog import FileDialog
+from ccpn.ui.gui.widgets.FilteringPulldownList import FilteringPulldownList
+from ccpn.ui.gui.widgets.Font import Font
+from ccpn.ui.gui.widgets.Frame import Frame
+from ccpn.ui.gui.widgets.GroupBox import GroupBox
+from ccpn.ui.gui.widgets.Icon import Icon
+from ccpn.ui.gui.widgets.Label import Label
+from ccpn.ui.gui.widgets.LineEdit import LineEdit
+from ccpn.ui.gui.widgets.ListWidget import ListWidget
+from ccpn.ui.gui.widgets.Menu import Menu
+from ccpn.ui.gui.widgets.Module import CcpnModule
+from ccpn.ui.gui.widgets.ModuleArea import CcpnModuleArea
+from ccpn.ui.gui.widgets.PlotWidget import PlotWidget
+from ccpn.ui.gui.widgets.PulldownList import PulldownList
+from ccpn.ui.gui.widgets.RadioButton import RadioButton
+from ccpn.ui.gui.widgets.RadioButtons import RadioButtons
+from ccpn.ui.gui.widgets.ScrollArea import ScrollArea
+from ccpn.ui.gui.widgets.Slider import Slider
+from ccpn.ui.gui.widgets.Spinbox import Spinbox
+from ccpn.ui.gui.widgets.Splitter import Splitter
+from ccpn.ui.gui.widgets.Table import ObjectTable, Column, ObjectTableItemDelegate
+from ccpn.ui.gui.widgets.TextEditor import TextEditor
+from ccpn.ui.gui.widgets.ToolBar import ToolBar
+from ccpn.ui.gui.widgets.ToolButton import ToolButton
+from ccpn.ui.gui.widgets.VerticalLabel import  VerticalLabel
+from ccpn.ui.gui.widgets.WebBrowser import WebBrowser
+from ccpn.ui.gui.widgets.WebView import WebViewPopup
+from ccpn.ui.gui.widgets.Widget import Widget
+
+'''

60fa08e5cc5f3cb52834f195857f544e6ce83fe3
lgm8 <lgm8@le.ac.uk>
2017-03-14 16:19:37 +0000

    Added TSV for 1d exporter. Bug fix in Popup exporter

--- src/python/ccpn/ui/gui/exporters1D/TextExporter.py
+++ src/python/ccpn/ui/gui/exporters1D/TextExporter.py
@@ -1,8 +1,6 @@
 
 
 
-# the code below is from PyQtGraph
-# Not fully working!!! To be fixed
 
 
 
@@ -12,23 +10,25 @@ from pyqtgraph  import PlotItem
 import pyqtgraph as pg
 import pandas as pd
 from numpy import nan as Nan
-
+from pyqtgraph.widgets.FileDialog import FileDialog
+from PyQt4 import QtGui, QtCore
 
 __all__ = ['CSVExporter']
 
 
 class TextExporter(Exporter):
-  Name = "Csv from 1D plot data"
+  Name = "Text"
   windows = []
 
   def __init__(self, item):
     Exporter.__init__(self, item)
     self.params = Parameter(name='params', type='group', children=[
-      {'name': 'separator', 'type': 'list', 'value': 'comma', 'values': ['comma']},
+      {'name': 'separator', 'type': 'list', 'value': 'separator,', 'values': ['*.csv', '*.tsv']},
 
       # {'name': 'precision', 'type': 'int', 'value': 10, 'limits': [0, None]},
       # {'name': 'columnMode', 'type': 'list', 'values': ['(x,y) per plot', '(x,y,y,y) for all plots']}
     ])
+
 
   def parameters(self):
     return self.params
@@ -45,36 +45,34 @@ class TextExporter(Exporter):
             data.append((x,y))
     return data
 
+  def _createDataframe(self, data):
+    df = []
+    for i in data:
+      x,y = i
+      d = pd.DataFrame([x,y])
+      d = d.transpose()
+      d.columns = ['x', 'y']
+      df.append(d)
+    df = pd.concat(df, keys=[str(i) for i in range(len(df))])
+    return df
+
   def export(self, fileName=None):
 
     if not isinstance(self.item, PlotItem):
       raise Exception("Must have a PlotItem selected for CSV export.")
 
     if fileName is None:
-      self.fileSaveDialog(filter=["*.csv"])#, "*.tsv"])
+      filter = self.params['separator']
+      self.fileSaveDialog(filter=filter) #["*.csv", "*.tsv"])
       return
 
-    self.fileDialog.selectNameFilter('*.tsv')
-
     data = self.getPlottedData()
-    df = []
     if data:
-
-      for i in data:
-        x,y = i
-
-        d = pd.DataFrame([x,y])
-        d = d.transpose()
-        d.columns = ['x', 'y']
-        df.append(d)
-    df = pd.concat(df, keys=[str(i) for i in range(len(df))])
-    df.to_csv(fileName)
-    # print(d)
-
-    # if self.params['separator'] == 'comma':
-    # d.to_csv(fileName)
-    # else:
-    #   d.to_table(fileName)
+      df = self._createDataframe(data)
+    if self.params['separator'] == '*.csv':
+      df.to_csv(fileName)
+    else:
+      df.to_csv(fileName, sep='\t')
 
 
 

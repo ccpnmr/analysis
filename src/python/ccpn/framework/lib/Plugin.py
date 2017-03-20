abe457d7af7538d3588ceed7036aa1a7a6853361
TJ Ragan <tjr22@le.ac.uk>
2017-03-20 16:00:33 +0000

    Start work on auto-generated plugin GUI.

--- src/python/ccpn/framework/lib/Plugin.py
+++ src/python/ccpn/framework/lib/Plugin.py
@@ -5,19 +5,42 @@ from abc import abstractmethod
 import os
 
 
+
+def _autoGenPluginGui(objMethod):
+  from ccpn.ui.gui.lib.GuiGenerator import generateWidget
+  return generateWidget(objMethod,)  # add Container=CcpnModule bit
+
+
+def _issueGuiInstantiatedMessage(objMethod):
+  print('Instantiated', str(objMethod))
+
+
 class Plugin(ABC):
   '''
   Plugin base class.
   '''
 
-  guiModule = None
+
+  guiModule = None  # Specify subclass of CcpnModule here
+  params = None  # Populate this to have an auto-generated gui
+  settings = None  # Break out the settings into another variable so pipelines are portable
+
 
   @property
   @abstractmethod
   def PLUGINNAME(self):
     return str()
 
+
   def __init__(self, application=None):
+    if self.guiModule is None:
+      if self.params is not None:
+        self.guiModule = _autoGenPluginGui
+      else:
+        self.guiModule = self._issueGuiInstantiatedMessage
+
+    self._uiWindow = None
+
     if application is not None:
       self.application = application
       self.current = self.application.current
@@ -31,13 +54,16 @@ class Plugin(ABC):
       except AttributeError:
         pass
 
+
   @property
   def package(self):
     return self.PLUGINNAME.split('...')[0]
 
+
   @property
   def name(self):
     return self.PLUGINNAME.split('...')[-1]
+
 
   @property
   def localInfo(self):
@@ -52,5 +78,13 @@ class Plugin(ABC):
       os.makedirs(pth)
     return pth
 
-  # TODO: cleanup() method
-  # TODO: auto-gen gui
+
+  def cleanup(self):
+    pass
+
+
+  @property
+  def _gui(self):
+    if self._uiWindow is None:
+      self._uiWindow = self.guiModule(self)
+    return self._uiWindow

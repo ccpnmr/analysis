21f0b97ee5fe498ddf6cbecc129879bcd44e5628
TJ Ragan <tjr22@le.ac.uk>
2017-03-22 15:19:37 +0000

    Add documentation for autogeneration of UI.

--- src/python/ccpn/framework/lib/Plugin.py
+++ src/python/ccpn/framework/lib/Plugin.py
@@ -13,6 +13,32 @@ class NoUI:
 class Plugin(ABC):
   '''
   Plugin base class.
+
+  For Autogeneration of Gui:
+  In order to genenerate a (crude) gui, you'll need to populate the params and settings class variables.
+    First, make it an iterable:
+      params = []
+    Now, add variables in the order you want the input boxes to show up.
+    Every variable is declared in a mapping (generally a dictionary) with two required keys:
+      'variable' : The keyward parameter that will be used when the function is called.
+      'value' : the possible values.  See below.
+    In addition to the required keys, several optional keys can be used:
+      label : the label used.  If this is left out, the variable name will be used instead.
+      default : the default value
+      stepsize : the stepsize for spinboxes.  If you include this for non-spinboxes it will be ignored
+
+    The 'value' entry:
+      The type of widget generated is controled by the value of this entry,
+      if the value is an iterable, the type of widget is controlled by the first item in the iterable
+      strings are not considered iterables here.
+        value type                       : type of widget
+        string                           : LineEdit
+        boolean                          : Checkbox
+        Iterable(strings)                : PulldownList
+        Iterable(int, int)               : Spinbox
+        Iterable(float, float)           : DoubleSpinbox
+        Iterable(Iterables(str, object)) : PulldownList where the object is passed instead of the string
+
   '''
 
 

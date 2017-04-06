586302fe08a78880b7817bab9484eebde26ed96e
Wayne Boucher <wb104@cam.ac.uk>
2017-04-06 17:00:44 +0100

    Add registerNotify back into __init__().

--- src/python/ccpn/framework/Current.py
+++ src/python/ccpn/framework/Current.py
@@ -119,6 +119,8 @@ Use print(current) to get a list of attribute, value pairs')
     for field in _fields:
       notifies[field] = []
 
+    self.registerNotify(self._updateSelectedPeaks, 'peaks')
+
   def registerNotify(self, notify, field):
     """Register notifier function 'notify' to be called on field 'field'
 

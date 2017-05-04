ee8aef87242f0f74e4b2068b5202a9ce293efbcb
Ed Brooksbank <ejb66@le.ac.uk>
2017-05-04 12:33:13 +0100

    SpectrumPropertiesPopup working

--- src/python/ccpn/ui/gui/popups/NmrChainPopup.py
+++ src/python/ccpn/ui/gui/popups/NmrChainPopup.py
@@ -1,14 +1,46 @@
+"""
+Module Documentation here
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2017"
+__credits__ = ("Wayne Boucher, Ed Brooksbank, Rasmus H Fogh, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
+__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license",
+               "or ccpnmodel.ccpncore.memops.Credits.CcpnLicense for licence text")
+__reference__ = ("For publications, please use reference from http://www.ccpn.ac.uk/v3-software/downloads/license",
+               "or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2017-04-10 15:35:09 +0100 (Mon, April 10, 2017) $"
+__version__ = "$Revision: 3.0.b1 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: Luca Mureddu $"
+__date__ = "$Date: 2017-03-30 11:28:58 +0100 (Thu, March 30, 2017) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
 from PyQt4 import QtGui
 
 from ccpn.ui.gui.widgets.Base import Base
 from ccpn.ui.gui.widgets.ButtonList import ButtonList
 from ccpn.ui.gui.widgets.Label import Label
 from ccpn.ui.gui.widgets.LineEdit import LineEdit
+from ccpn.ui.gui.popups.Dialog import ccpnDialog      # ejb
 
-class NmrChainPopup(QtGui.QDialog, Base):
-  def __init__(self, parent=None, nmrChain=None, **kw):
-    super(NmrChainPopup, self).__init__(parent)
-    Base.__init__(self, **kw)
+
+# class NmrChainPopup(QtGui.QDialog, Base):
+class NmrChainPopup(ccpnDialog):
+  def __init__(self, parent=None, nmrChain=None
+               , title='Nmr Chain', **kw):
+    ccpnDialog.__init__(self, parent, setLayout=True, windowTitle=title, **kw)
+    # super(NmrChainPopup, self).__init__(parent)
+    # Base.__init__(self, **kw)
 
     self.nmrChain = nmrChain
     self.nmrChainLabel = Label(self, "NmrChain Name ", grid=(0, 0))

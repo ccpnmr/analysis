2219be26c9fe667ef0abfa0249194482ac470d82
Luca Mureddu <lgm8@leicester.ac.uk>
2017-05-30 11:37:07 +0100

    author Header changed from luca to full name

--- src/python/ccpn/ui/gui/modules/PipelineModule.py
+++ src/python/ccpn/ui/gui/modules/PipelineModule.py
@@ -1,3 +1,36 @@
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2017"
+__credits__ = ("Wayne Boucher, Ed Brooksbank, Rasmus H Fogh, Luca Mureddu, Timothy J Ragan"
+               "Simon P Skinner & Geerten W Vuister")
+__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license"
+               "or ccpnmodel.ccpncore.memops.Credits.CcpnLicense for licence text")
+__reference__ = ("For publications, please use reference from http://www.ccpn.ac.uk/v3-software/downloads/license"
+               "or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
+
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2017-04-07 11:41:14 +0100 (Fri, April 07, 2017) $"
+__version__ = "$Revision: 3.0.b1 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: Luca Mureddu $"
+
+__date__ = "$Date: 2017-04-07 10:28:42 +0000 (Fri, April 07, 2017) $"
+#=========================================================================================
+# Start of code
+#====================================
+
+
+
+
 import collections
 import json
 import time
@@ -363,7 +396,6 @@ class GuiPipeline(CcpnModule):
   def _addGuiPipe(self, name, selected):
     for guiPipe in self.guiPipes:
       if guiPipe.pipeName == selected:
-        print(guiPipe.pipe)
         position = self.pipelineSettingsParams['addPosit']
         self.pipelineWidget = guiPipe(parent=self, application=self.application, name=name, project=self.project)
         self.pipelineArea.addDock(self.pipelineWidget, position=position)

2e2ef917acf510ba6ae2ea531a66a491c2443fd3
Luca Mureddu <lgm8@leicester.ac.uk>
2017-06-10 17:40:13 +0100

    Screen Development

---
+++ src/python/ccpn/ui/gui/widgets/FillBetweenRegions.py
@@ -0,0 +1,43 @@
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2017"
+__credits__ = ("Wayne Boucher, Ed Brooksbank, Rasmus H Fogh, Luca Mureddu, Timothy J Ragan"
+               "Simon P Skinner & Geerten W Vuister")
+__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license"
+               "or ccpnmodel.ccpncore.memops.Credits.CcpnLicense for licence text")
+__reference__ = ("For publications, please use reference from http://www.ccpn.ac.uk/v3-software/downloads/license"
+               "or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2017-05-28 10:28:42 +0000 (Sun, May 28, 2017) $"
+__version__ = "$Revision: 3.0.b1 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: Luca Mureddu $"
+__date__ = "$Date: 2017-05-28 10:28:42 +0000 (Sun, May 28, 2017) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+
+import pyqtgraph as pg
+from PyQt4 import QtGui, QtCore
+from pyqtgraph.graphicsItems.FillBetweenItem import FillBetweenItem
+
+
+
+
+class FillBetweenRegions(FillBetweenItem):
+  """
+  GraphicsItem filling the space between two PlotDataItems.
+  """
+
+  def __init__(self, curve1, curve2, brush = None, **kw):
+
+    FillBetweenItem.__init__(self, curve1=curve1, curve2=curve2, brush=brush)
+
+

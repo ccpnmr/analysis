2bc940f83c5961a2cc124e46daf1abe4342667f1
Luca Mureddu <lgm8@leicester.ac.uk>
2017-06-13 11:47:43 +0100

    screen Development

---
+++ src/python/ccpn/pipes/lib/_new1Dspectrum.py
@@ -0,0 +1,40 @@
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2017"
+__credits__ = ("Wayne Boucher, Ed Brooksbank, Rasmus H Fogh, Luca Mureddu, Timothy J Ragan"
+               "Simon P Skinner & Geerten W Vuister")
+__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license"
+               "or ccpnmodel.ccpncore.memops.Credits.CcpnLicense for licence text")
+__reference__ = ("For publications, please use reference from http://www.ccpn.ac.uk/v3-software/downloads/license"
+               "or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2017-05-28 10:28:42 +0000 (Sun, May 28, 2017) $"
+__version__ = "$Revision: 3.0.b1 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: Luca Mureddu $"
+__date__ = "$Date: 2017-05-28 10:28:42 +0000 (Sun, May 28, 2017) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+
+
+def _create1DSpectrum(project, name, intensities, positions, expType):
+  '''
+   CCPN internal. Used in pipes
+  Function to create a user defined CCPN object spectrum. It can be used to create STD spectrum
+  '''
+  axisCodes = ('H',)
+  spectrum = project.createDummySpectrum(axisCodes, name)
+  spectrum._positions = positions
+  spectrum._intensities = intensities
+  spectrum.experimentType = expType
+  spectrum.pointCounts = (len(intensities),)
+
+  return spectrum

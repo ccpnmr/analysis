c62d1a9b4879a48cc503d27c065d23dbd171ef6a
Ed Brooksbank <ejb66@le.ac.uk>
2017-06-23 02:04:42 +0100

    new ExportNef dialog

--- src/python/ccpn/ui/gui/popups/ExportNefPopup.py
+++ src/python/ccpn/ui/gui/popups/ExportNefPopup.py
@@ -27,56 +27,78 @@ from ccpn.ui.gui.widgets.ButtonList import ButtonList
 from ccpn.ui.gui.widgets.Label import Label
 from ccpn.ui.gui.widgets.LineEdit import LineEdit
 from ccpn.ui.gui.popups.Dialog import CcpnDialog
-from ccpn.ui.gui.widgets.FileDialog import NefFileDialog
+from ccpn.ui.gui.widgets.FileDialog import FileDialog, NefFileDialog
 from ccpn.ui.gui.widgets.Widget import Widget
 from ccpn.ui.gui.widgets.Frame import Frame
+from ccpn.ui.gui.widgets.Base import Base
+from ccpn.ui.gui.widgets.LineEdit import LineEdit
+from ccpn.ui.gui.widgets.Icon import Icon
+from ccpn.ui.gui.widgets.Button import Button
+from os.path import expanduser
 
 class ExportNefPopup(CcpnDialog):
   def __init__(self, parent=None, title='Export to Nef File', **kw):
 
     B = {'fileMode':None, 'text':None, 'acceptMode':None, 'preferences':None, 'selectFile':None, 'filter':None}
-    C = {k:v for k, v in kw.items() if k in B}
+    self.saveDict = {k:v for k, v in kw.items() if k in B}
     filterKw = {k:v for k, v in kw.items() if k not in B}
 
     CcpnDialog.__init__(self, parent, setLayout=True, windowTitle=title, **filterKw)
 
-    self.options = Frame(self, setLayout=True)
-    self.buttons = ButtonList(self.options, ['Cancel', 'OK'], [self.reject, self._save], grid=(0, 0))
+    self.options = Frame(self, setLayout=True, grid=(0,0))
+    self.buttons = ButtonList(self.options, ['Cancel', 'OK'], [self._rejectDialog, self._acceptDialog], grid=(0, 0))
+
+    self.openPathIcon = Icon('icons/directory')
+
+    self.saveFrame = Frame(self, setLayout=True, grid=(1,0))
+    self.openPathIcon = Icon('icons/directory')
+    self.saveLabel = Label(self.saveFrame, grid=(0,0), hAlign = 'c')
+    self.pathButton = Button(self.saveFrame, text=''
+                             , icon=self.openPathIcon
+                             , callback=self._openFileDialog
+                             , grid=(0, 1), hAlign='c')
+
+    if 'selectFile' in kw:
+      self.saveLabel.setText(kw['selectFile'])
+    else:
+      self.saveLabel.setText('None')
 
     self.fileWidget = Frame(self, setLayout=True)
-    self.fileDialog = NefFileDialog(self.fileWidget, **C)
+    self.fileSaveDialog = NefFileDialog(self.fileWidget, **self.saveDict)
+    self.layout().addWidget(self.fileSaveDialog, 2,0)
+    self.fileSaveDialog._setParent(self, self._acceptDialog, self._rejectDialog)
 
-    self.layout().addWidget(self.options, 0,0)
-    self.layout().addWidget(self.fileDialog, 1,0)
+  def _acceptDialog(self, exitSaveFileName=None):
+    self.exitFileName = exitSaveFileName
+    self.accept()
+    # return exitSaveFileName
+
+  def _rejectDialog(self):
+    self.exitFileName = None
+    self.reject()
+    # return None
+
+  def show(self):
+    self.raise_()
+    val = self.exec_()
+    self.accept()
+    return self.exitFileName    # and other save options here
+
+  def _openFileDialog(self):
+    self.fileDialog = FileDialog(self, **self.saveDict)
+    selectedFile = self.fileDialog.selectedFile()
+    if selectedFile:
+      self.saveLabel.setText(str(selectedFile))
 
   def _save(self):
     self.accept()
 
-  def selectedFiles(self):
-    return self.fileDialog.selectedFiles()
-    self.close()
-
-  def selectedFile(self):
-    return self.fileDialog.selectedFile()
-    self.close()
-  def reject(self):
-    dialog = self.fileDialog.selectedFile()
-    self.fileDialog.close()
-
-  def _save(self):
-    dialog = self.fileDialog.selectedFile()
-    self.fileDialog.close()
-    self.close()
 
 if __name__ == '__main__':
   from ccpn.ui.gui.widgets.Application import TestApplication
   from ccpn.ui.gui.popups.Dialog import CcpnDialog
   app = TestApplication()
-  # popup = CcpnDialog(windowTitle='Test LineEditButtonDialog')
   dialog = ExportNefPopup()
-  pass
-  # print (popup.layout())
   dialog.show()
-  dialog.raise_()
-  # dialog.exec_()
-  # app.start()
+
+

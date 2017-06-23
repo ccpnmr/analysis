3c6916b70080da76e4dd9e8dcc3dfb206c8957c1
Ed Brooksbank <ejb66@le.ac.uk>
2017-06-23 10:25:04 +0100

    new ExportNef dialog

--- src/python/ccpn/ui/gui/popups/ExportNefPopup.py
+++ src/python/ccpn/ui/gui/popups/ExportNefPopup.py
@@ -34,7 +34,10 @@ from ccpn.ui.gui.widgets.Base import Base
 from ccpn.ui.gui.widgets.LineEdit import LineEdit
 from ccpn.ui.gui.widgets.Icon import Icon
 from ccpn.ui.gui.widgets.Button import Button
+from ccpn.ui.gui.widgets.Spacer import Spacer
+from PyQt4 import QtGui
 from os.path import expanduser
+from ccpn.util.Logging import getLogger
 
 class ExportNefPopup(CcpnDialog):
   def __init__(self, parent=None, title='Export to Nef File', **kw):
@@ -46,6 +49,9 @@ class ExportNefPopup(CcpnDialog):
     CcpnDialog.__init__(self, parent, setLayout=True, windowTitle=title, **filterKw)
 
     self.options = Frame(self, setLayout=True, grid=(0,0))
+
+    # put save options in this section
+
     self.buttons = ButtonList(self.options, ['Cancel', 'OK'], [self._rejectDialog, self._acceptDialog], grid=(0, 0))
 
     self.openPathIcon = Icon('icons/directory')
@@ -57,16 +63,46 @@ class ExportNefPopup(CcpnDialog):
                              , icon=self.openPathIcon
                              , callback=self._openFileDialog
                              , grid=(0, 1), hAlign='c')
+    self.spacer = Spacer(self.saveFrame, 5, 5
+                         , QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Expanding
+                         , grid=(1,1), gridSpan=(1,1))
+
+    # this show/hide button doesn't quite work yet
+    # self.showHide = Frame(self, setLayout=True, grid=(2,0))
+    # self.showHideIcon = Icon('icons/directory')               # need to change this
+    #
+    # self.spacer = Spacer(self.showHide, 5, 5
+    #                      , QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Fixed
+    #                      , grid=(0,0), gridSpan=(1,1))
+    # self.saveLabel2 = Label(self.showHide, text='Show/Hide saveDialog', grid=(0,1), hAlign = 'c')
+    # self.pathButton = Button(self.showHide, text=''
+    #                          , icon=self.showHideIcon
+    #                          , callback=self._toggleSaveDialog
+    #                          , grid=(0, 2), hAlign='t')
+
 
     if 'selectFile' in kw:
       self.saveLabel.setText(kw['selectFile'])
     else:
       self.saveLabel.setText('None')
 
+    self.setSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.MinimumExpanding)
+
+    # from here down is the save dialog
+    # can we add a hide/show button?
+
     self.fileWidget = Frame(self, setLayout=True)
+    self.fileWidget.hide()
     self.fileSaveDialog = NefFileDialog(self.fileWidget, **self.saveDict)
-    self.layout().addWidget(self.fileSaveDialog, 2,0)
+    self.layout().addWidget(self.fileSaveDialog, 3,0)
     self.fileSaveDialog._setParent(self, self._acceptDialog, self._rejectDialog)
+    self._saveState = True
+
+  def _toggleSaveDialog(self):
+    if self._saveState is True:
+      self.fileSaveDialog.hide()
+    else:
+      self.fileSaveDialog.show()
 
   def _acceptDialog(self, exitSaveFileName=None):
     self.exitFileName = exitSaveFileName
@@ -77,6 +113,9 @@ class ExportNefPopup(CcpnDialog):
     self.exitFileName = None
     self.reject()
     # return None
+
+  def closeEvent(self, QCloseEvent):
+    self._rejectDialog()
 
   def show(self):
     self.raise_()

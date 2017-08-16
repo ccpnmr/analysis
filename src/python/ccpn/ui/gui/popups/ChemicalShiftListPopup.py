8fd62f472df9949dd1ca92b0074cbb26798636d2
Luca Mureddu <lgm8@leicester.ac.uk>
2017-08-16 15:22:53 +0100

    Added spectra option for the chemicalshift List popup.

--- src/python/ccpn/ui/gui/popups/ChemicalShiftListPopup.py
+++ src/python/ccpn/ui/gui/popups/ChemicalShiftListPopup.py
@@ -30,31 +30,79 @@ from ccpn.ui.gui.widgets.Label import Label
 from ccpn.ui.gui.widgets.LineEdit import LineEdit
 from ccpn.ui.gui.popups.Dialog import CcpnDialog
 from ccpn.ui.gui.widgets.MessageDialog import showWarning
-
+from ccpn.ui.gui.widgets.ListWidget import ListWidget
 
 class ChemicalShiftListPopup(CcpnDialog):
   def __init__(self, parent=None, mainWindow=None, chemicalShiftList=None, title='Chemical Shift List', **kw):
     """
     Initialise the widget
     """
-    CcpnDialog.__init__(self, parent, setLayout=True, windowTitle=title, **kw)
+    CcpnDialog.__init__(self, parent, setLayout=True, windowTitle=title, size=(500,100), **kw)
 
     self.mainWindow = mainWindow
-    self.application = mainWindow.application
-    self.project = mainWindow.application.project
-    self.current = mainWindow.application.current
+    self.project = None
+    if mainWindow is not None:
+      self.application = mainWindow.application
+      self.project = mainWindow.application.project
+      self.current = mainWindow.application.current
 
     self.chemicalShiftList = chemicalShiftList
-    self.chemicalShiftListLabel = Label(self, "Chemical Shift List Name ", grid=(0, 0))
-    self.chemicalShiftListText = LineEdit(self, chemicalShiftList.name, grid=(0, 1))
-    ButtonList(self, ['Cancel', 'OK'], [self.reject, self._okButton], grid=(1, 1))
+
+    row = 0
+    self.chemicalShiftListLabel = Label(self, "Chemical Shift List Name ", grid=(row, 0))
+    self.chemicalShiftListText = LineEdit(self, grid=(row, 1), gridSpan= (1, 2))
+
+    row += 1
+    tip = 'Drag and drop spectra to the current spectra box to link them to the ChemicalShift List'
+    self.spectraLabel = Label(self, "Current Spectra", grid=(row, 1))
+    self.spectraLabel = Label(self, "Available Spectra", grid=(row, 2), tipText = tip,
+                              )
+
+    row += 1
+    tipDragHere = 'Drag spectra here to link them to the ChemicalShift List.'
+    self.spectraLabel = Label(self, "Spectra", grid=(row, 0))
+    self.spectraList = ListWidget(self, grid=(row, 1), acceptDrops=True, tipText = tipDragHere)
+    self.availableSpectraList = ListWidget(self, grid=(row, 2), tipText = tip)
+
+    row += 1
+    ButtonList(self, ['Cancel', 'OK'], [self.reject, self._okButton], grid=(row, 2))
+
+    self._setData()
+
+  def _setData(self):
+    if self.chemicalShiftList is not None:
+      self.chemicalShiftListText.setText(self.chemicalShiftList.name)
+      self.spectraList.setObjects(self.chemicalShiftList.spectra)
+
+    if self.project is not None:
+      availableSpectra = [sp for sp in self.project.spectra if sp not in self.chemicalShiftList.spectra]
+      self.availableSpectraList.setObjects(availableSpectra)
+
+  def _addSpectraToCSList(self):
+    if self.chemicalShiftList is not None:
+      objs = [self.project.getByPid(text) for text in self.spectraList.getTexts()]
+      self.chemicalShiftList.spectra = objs
+
 
   def _okButton(self):
     newName = self.chemicalShiftListText.text()
     try:
-      if str(newName) != self.chemicalShiftList.name:
-        self.chemicalShiftList.rename(newName)
+      if self.chemicalShiftList is not None:
+        if str(newName) != self.chemicalShiftList.name:
+          self.chemicalShiftList.rename(newName)
+        self._addSpectraToCSList()
       self.accept()
     except Exception as es:
       showWarning(self.windowTitle(), str(es))
 
+if __name__ == '__main__':
+  from ccpn.ui.gui.widgets.Application import TestApplication
+  from ccpn.ui.gui.popups.Dialog import CcpnDialog
+
+  app = TestApplication()
+  popup = ChemicalShiftListPopup()
+
+  popup.show()
+  popup.raise_()
+
+  app.start()

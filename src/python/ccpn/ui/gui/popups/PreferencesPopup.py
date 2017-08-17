65f3a539286a57a2f3eec9c0b20287d6f9ccc9c8
Luca Mureddu <lgm8@leicester.ac.uk>
2017-08-17 12:31:28 +0100

    implementations

--- src/python/ccpn/ui/gui/popups/PreferencesPopup.py
+++ src/python/ccpn/ui/gui/popups/PreferencesPopup.py
@@ -41,12 +41,17 @@ from ccpn.framework.Translation import languages
 from ccpn.ui.gui.popups.Dialog import CcpnDialog
 from ccpn.ui.gui.widgets import MessageDialog
 
+# FIXME separate pure GUI to project/preferences properites
+# The code sets Gui Parameters assuming that  Preference is not None and has a bunch of attributes.
+
+
 PulldownListsMinimumWidth = 200
 LineEditsMinimumWidth = 195
+NotImplementedTipText = 'This option has not been implemented yet'
 
 class PreferencesPopup(CcpnDialog):
   def __init__(self, parent=None, preferences=None, project=None, title='Preferences', **kw):
-    CcpnDialog.__init__(self, parent=parent, setLayout=True, windowTitle=title, size=(300, 300), **kw)
+    CcpnDialog.__init__(self, parent=parent, setLayout=True, windowTitle=title, size=(300, 500), **kw)
 
     if not project:                                         # ejb - should always be loaded
       MessageDialog.showWarning(title, 'No project loaded')
@@ -71,32 +76,32 @@ class PreferencesPopup(CcpnDialog):
     self.tabWidget = QtGui.QTabWidget()
 
     ## 1 Tab
+    self.generalTabFrame = Frame(self, setLayout=True)
+    self._setGeneralTabWidgets(parent=self.generalTabFrame)
+    self.tabWidget.addTab(self.generalTabFrame, 'General')
+
+    ## 2 Tab
     self.appearanceTabFrame = Frame(self, setLayout=True)
     self._setAppearanceTabWidgets(parent=self.appearanceTabFrame)
     self.tabWidget.addTab(self.appearanceTabFrame, 'Appearance')
 
-    ## 2 Tab
-    self.pathsTabFrame = Frame(self, setLayout=True)
-    self._setPathsTabWidgets(parent=self.pathsTabFrame)
-    self.tabWidget.addTab(self.pathsTabFrame, 'Paths')
-
-    ## 3 Tab
-    self.miscellaneousTabFrame = Frame(self, setLayout=True)
-    self._setMiscellaneousTabWidgets(parent=self.miscellaneousTabFrame)
-    self.tabWidget.addTab(self.miscellaneousTabFrame, 'Miscellaneous')
+    ## 3 Tab Disabled. # Keep the code for future additions
+    # self.miscellaneousTabFrame = Frame(self, setLayout=True)
+    # self._setMiscellaneousTabWidgets(parent=self.miscellaneousTabFrame)
+    # self.tabWidget.addTab(self.miscellaneousTabFrame, 'Miscellaneous')
 
 
   def _addMainWidgets(self):
 
     ''' Tabs and Close button are added in the mainLayout. '''
 
-    buttonBox = Button(self.miscellaneousTabFrame, text='Close', callback=self.accept)
+    buttonBox = Button(self, text='Close', callback=self.accept)
     self.mainLayout.addWidget(self.tabWidget, 0, 0, 1, 3)
     self.mainLayout.addWidget(buttonBox, 1, 2)
 
 
-  def _setPathsTabWidgets(self, parent):
-    ''' Insert a widget in here to appear in the Paths Tab  '''
+  def _setGeneralTabWidgets(self, parent):
+    ''' Insert a widget in here to appear in the General Tab  '''
 
     row = 0
     self.dataPathLabel = Label(parent, "Data Path", grid=(row, 0),)
@@ -104,7 +109,7 @@ class PreferencesPopup(CcpnDialog):
     self.dataPathText.setMinimumWidth(LineEditsMinimumWidth)
     self.dataPathText.editingFinished.connect(self._setDataPath)
     self.dataPathText.setText(self.preferences.general.dataPath)
-    self.dataPathButton = Button(self.pathsTabFrame, grid=(row, 2), callback=self._getDataPath, icon='icons/directory', hPolicy='fixed')
+    self.dataPathButton = Button(self.generalTabFrame, grid=(row, 2), callback=self._getDataPath, icon='icons/directory', hPolicy='fixed')
 
     row += 1
     self.auxiliaryFilesLabel = Label(parent, text="Auxiliary Files Path ", grid=(row, 0))
@@ -124,20 +129,37 @@ class PreferencesPopup(CcpnDialog):
 
     row += 1
     self.pluginPathLabel = Label(parent, text="Plugin Path", grid=(row, 0))
-    self.pluginPathData = LineEdit(parent, grid=(row, 1),hAlign='l')
+    self.pluginPathData = LineEdit(parent, grid=(row, 1),hAlign='l', tipText=NotImplementedTipText)
     self.pluginPathData.setMinimumWidth(LineEditsMinimumWidth)
     self.pluginPathData.setText(self.preferences.general.userPluginPath)
     self.pluginPathDataButton = Button(parent, grid=(row, 2), callback=self._getPluginFilesPath, icon='icons/directory', hPolicy='fixed')
     self.pluginPathData.editingFinished.connect(self._setPluginFilesPath)
+    # TODO enable plugin PathData
+    self.pluginPathData.setDisabled(True)
+    self.pluginPathDataButton.setDisabled(True)
 
     row += 1
-    self.extensionPathLabel = Label(parent, text="Extension Path", grid=(row, 0))
-    self.extensionPathData = LineEdit(parent, grid=(row, 1), hAlign='l')
-    self.extensionPathData.setMinimumWidth(LineEditsMinimumWidth)
-    self.extensionPathData.setText(self.preferences.general.userExtensionPath)
-    self.extensionPathDataButton = Button(parent, grid=(row, 2), callback=self._getExtensionFilesPath, icon='icons/directory', hPolicy='fixed')
-    self.extensionPathData.editingFinished.connect(self._setExtensionFilesPath)
+    self.pipesPathLabel = Label(parent, text="Pipes Path", grid=(row, 0),)
+    self.pipesPathData = LineEdit(parent, grid=(row, 1), hAlign='l', tipText=NotImplementedTipText)
+    self.pipesPathData.setMinimumWidth(LineEditsMinimumWidth)
+    self.pipesPathData.setText(self.preferences.general.userExtensionPath)
+    self.pipesPathDataButton = Button(parent, grid=(row, 2), callback=self._getExtensionFilesPath, icon='icons/directory', hPolicy='fixed')
+    self.pipesPathData.editingFinished.connect(self._setPipesFilesPath)
+    # TODO enable pipes PathData
+    self.pipesPathData.setDisabled(True)
+    self.pipesPathDataButton.setDisabled(True)
 
+    row += 1
+    self.autoBackupEnabledLabel = Label(parent, text="Auto Backup On: ", grid=(row, 0))
+    self.autoBackupEnabledBox = CheckBox(parent, grid=(row, 1), checked=self.preferences.general.autoBackupEnabled)
+    self.autoBackupEnabledBox.toggled.connect(partial(self._toggleGeneralOptions, 'autoBackupEnabled'))
+
+    row += 1
+    self.autoBackupFrequencyLabel = Label(parent, text="Auto Backup Freq (mins)", grid=(row, 0))
+    self.autoBackupFrequencyData = LineEdit(parent, grid=(row, 1), hAlign='l')
+    self.autoBackupFrequencyData.setMinimumWidth(LineEditsMinimumWidth)
+    self.autoBackupFrequencyData.setText('%.0f' % self.preferences.general.autoBackupFrequency)
+    self.autoBackupFrequencyData.editingFinished.connect(self._setAutoBackupFrequency)
 
   def _setAppearanceTabWidgets(self, parent):
     ''' Insert a widget in here to appear in the Appearance Tab. Parent = the Frame obj where the widget should live'''
@@ -167,6 +189,13 @@ class PreferencesPopup(CcpnDialog):
     self.regionPaddingData.editingFinished.connect(self._setRegionPadding)
 
     row += 1
+    self.dropFactorLabel = Label(parent, text="Peak Picking Drop (%)", grid=(row, 0))
+    self.dropFactorData = LineEdit(parent, grid=(row, 1), hAlign='l')
+    self.dropFactorData.setMinimumWidth(LineEditsMinimumWidth)
+    self.dropFactorData.setText('%.0f' % (100*self.preferences.general.peakDropFactor))
+    self.dropFactorData.editingFinished.connect(self._setDropFactor)
+
+    row += 1
     self.useNativeLabel = Label(parent, text="Use Native File Dialogs: ", grid=(row, 0))
     self.useNativeBox = CheckBox(parent, grid=(row, 1), checked=self.preferences.general.useNative)
     self.useNativeBox.toggled.connect(partial(self._toggleGeneralOptions, 'useNative'))
@@ -185,28 +214,23 @@ class PreferencesPopup(CcpnDialog):
     self.spectrumBorderBox = CheckBox(parent, grid=(row, 1), checked=self.preferences.general.showSpectrumBorder)
     self.spectrumBorderBox.toggled.connect(partial(self._toggleGeneralOptions, 'showSpectrumBorder'))
 
+    row += 1
+    self.showDoubleCursorLabel = Label(parent, text="Show DoubleCursor: ", grid=(row, 0))
+    self.showDoubleCursorBox = CheckBox(parent, grid=(row, 1), tipText=NotImplementedTipText)#, checked=self.preferences.general.showDoubleCursor)
+    # TODO enable DoubleCursor
+    self.showDoubleCursorBox.setDisabled(True)
+    # self.showDoubleCursorBox.toggled.connect(partial(self._toggleGeneralOptions, 'showDoubleCursor'))
+
 
   def _setMiscellaneousTabWidgets(self, parent):
-    ''' Insert a widget in here to appear in the Miscellaneous Tab  '''
+    ''' 
+    Insert a widget in here to appear in the Miscellaneous Tab 
+    Empty. To be Implemented
+    '''
 
-    row = 0
-    self.autoBackupEnabledLabel = Label(parent, text="Auto Backup On: ", grid=(row, 0))
-    self.autoBackupEnabledBox = CheckBox(parent, grid=(row, 1), checked=self.preferences.general.autoBackupEnabled)
-    self.autoBackupEnabledBox.toggled.connect(partial(self._toggleGeneralOptions, 'autoBackupEnabled'))
-    row += 1
+    pass
 
-    self.autoBackupFrequencyLabel = Label(parent, text="Auto Backup Freq (mins)", grid=(row, 0))
-    self.autoBackupFrequencyData = LineEdit(parent, grid=(row, 1), hAlign='l')
-    self.autoBackupFrequencyData.setMinimumWidth(LineEditsMinimumWidth)
-    self.autoBackupFrequencyData.setText('%.0f' % self.preferences.general.autoBackupFrequency)
-    self.autoBackupFrequencyData.editingFinished.connect(self._setAutoBackupFrequency)
 
-    row += 1
-    self.dropFactorLabel = Label(parent, text="Peak Picking Drop (%)", grid=(row, 0))
-    self.dropFactorData = LineEdit(parent, grid=(row, 1), hAlign='l')
-    self.dropFactorData.setMinimumWidth(LineEditsMinimumWidth)
-    self.dropFactorData.setText('%.0f' % (100*self.preferences.general.peakDropFactor))
-    self.dropFactorData.editingFinished.connect(self._setDropFactor)
 
 
   def _getDataPath(self):
@@ -270,15 +294,15 @@ class PreferencesPopup(CcpnDialog):
       self.preferences.general.pluginMacroPath = directory[0]
 
   def _getExtensionFilesPath(self):
-    if os.path.exists(os.path.expanduser(self.extensionPathData.text())):
-      currentDataPath = os.path.expanduser(self.extensionPathData.text())
+    if os.path.exists(os.path.expanduser(self.pipesPathData.text())):
+      currentDataPath = os.path.expanduser(self.pipesPathData.text())
     else:
       currentDataPath = os.path.expanduser('~')
     dialog = FileDialog(self, text='Select Data File', directory=currentDataPath, fileMode=2, acceptMode=0,
                         preferences=self.preferences.general)
     directory = dialog.selectedFiles()
     if len(directory) > 0:
-      self.extensionPathData.setText(directory[0])
+      self.pipesPathData.setText(directory[0])
       self.preferences.general.userExtensionPath = directory[0]
 
   def _setMacroFilesPath(self):
@@ -289,8 +313,8 @@ class PreferencesPopup(CcpnDialog):
     newPath = self.pluginPathData.text()
     self.preferences.general.userPluginPath = newPath
 
-  def _setExtensionFilesPath(self):
-    newPath = self.extensionPathData.text()
+  def _setPipesFilesPath(self):
+    newPath = self.pipesPathData.text()
     self.preferences.general.userExtensionPath = newPath
 
   def _changeLanguage(self, value):
@@ -311,6 +335,8 @@ class PreferencesPopup(CcpnDialog):
     elif preference == 'autoBackupEnabled':
       application = self.project._appBase
       application.updateAutoBackup()
+
+
 
   def _setAutoBackupFrequency(self):
     try:

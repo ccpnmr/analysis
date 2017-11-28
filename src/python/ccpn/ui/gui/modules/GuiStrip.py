d5f3872e4638d1528ccc51175a65ee6ae799cafd
Ed Brooksbank <ejb66@le.ac.uk>
2017-11-28 17:08:47 +0000

    added simple peak symbol

--- src/python/ccpn/ui/gui/modules/GuiStrip.py
+++ src/python/ccpn/ui/gui/modules/GuiStrip.py
@@ -98,22 +98,22 @@ class GuiStrip(Frame):
     # self.plotWidget.showGrid(x=True, y=True, alpha=None)
 
 
-    # # TODO: ED comment out the block below to return to normal
-    # # self.plotWidget.hide()
-    # from ccpn.util.CcpnOpenGL import CcpnOpenGLWidget, CcpnGLWidget
-    # # self._testCcpnOpenGLWidget = CcpnOpenGLWidget(self)
-    # # self.getLayout().addWidget(self._testCcpnOpenGLWidget, 1, 0)
-    #
-    # self._testCcpnOpenGLWidget = CcpnGLWidget(self)
-    # self.getLayout().addWidget(self._testCcpnOpenGLWidget, 3, 0)
-    # self._testCcpnOpenGLWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-    #
-    # # self.plotWidgetOverlay = pg.PlotWidget(self, useOpenGL=useOpenGL)  #    make a copy
-    # # self.plotWidgetOverlay.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
-    # # self.plotWidgetOverlay.resize(200, 200)
-    # # self.plotWidgetOverlay.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-    # # self.plotWidgetOverlay.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
-    # # self.plotWidgetOverlay.showGrid(x=True, y=True, alpha=None)
+    # TODO: ED comment out the block below to return to normal
+    # self.plotWidget.hide()
+    from ccpn.util.CcpnOpenGL import CcpnOpenGLWidget, CcpnGLWidget
+    # self._testCcpnOpenGLWidget = CcpnOpenGLWidget(self)
+    # self.getLayout().addWidget(self._testCcpnOpenGLWidget, 1, 0)
+
+    self._testCcpnOpenGLWidget = CcpnGLWidget(self)
+    self.getLayout().addWidget(self._testCcpnOpenGLWidget, 3, 0)
+    self._testCcpnOpenGLWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+
+    # self.plotWidgetOverlay = pg.PlotWidget(self, useOpenGL=useOpenGL)  #    make a copy
+    # self.plotWidgetOverlay.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+    # self.plotWidgetOverlay.resize(200, 200)
+    # self.plotWidgetOverlay.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+    # self.plotWidgetOverlay.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
+    # self.plotWidgetOverlay.showGrid(x=True, y=True, alpha=None)
 
 
 

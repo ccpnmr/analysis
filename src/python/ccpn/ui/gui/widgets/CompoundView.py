dd98ecda5639871f3dc32f0cd2530cc94dabd38a
Luca Mureddu <lgm8@leicester.ac.uk>
2018-02-05 16:46:58 +0000

    Bug FIxes for cv

--- src/python/ccpn/ui/gui/widgets/CompoundView.py
+++ src/python/ccpn/ui/gui/widgets/CompoundView.py
@@ -42,8 +42,6 @@ class CompoundView(QGraphicsView, Base):
 
     super(CompoundView, self).__init__()
     self.scene = QGraphicsScene(self)
-    self.tic_tac_toe = TicTacToe()
-    self.scene.addItem(self.tic_tac_toe)
     self.scene.setSceneRect(0, 0, 300, 300)
     self.setScene(self.scene)
     self.setCacheMode(QGraphicsView.CacheBackground)
@@ -103,11 +101,6 @@ class CompoundView(QGraphicsView, Base):
     self.setViewportUpdateMode(QtWidgets.QGraphicsView.FullViewportUpdate)
     self.setInteractive(True)
     self.resetView()
-    # self.tic_tac_toe = TicTacToe()
-    # self.scene = QtWidgets.QGraphicsScene(self)
-    # self.scene.addItem(self.tic_tac_toe)
-    # self.scene.setSceneRect(0, 0, 300, 300)
-    # self.setScene(self.scene)
 
     self.selectionBox = SelectionBox(self.scene, self)
     self.scene.addItem(self.selectionBox)
@@ -151,7 +144,6 @@ class CompoundView(QGraphicsView, Base):
     'set the smiles'
     compound = importSmiles(smiles)
     variant = list(compound.variants)[0]
-    print(smiles, compound.variants)
     self.setVariant(variant)
     variant.snapAtomsToGrid(ignoreHydrogens=False)
     self.smiles = smiles
@@ -2567,54 +2559,6 @@ class AtomItem(QtWidgets.QGraphicsItem):
             drawText(qPoint(x+r-3, y-r+3), text)
 
     self.highlights = set()
-
-class TicTacToe(QtWidgets.QGraphicsItem):
-  def __init__(self):
-    super(TicTacToe, self).__init__()
-    self.board = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
-    self.O = 0
-    self.X = 1
-    self.turn = self.O
-
-  def reset(self):
-    for y in range(3):
-      for x in range(3):
-        self.board[y][x] = -1
-    self.turn = self.O
-    self.update()
-
-  def select(self, x, y):
-    if x < 0 or y < 0 or x >= 3 or y >= 3:
-      return
-    if self.board[y][x] == -1:
-      self.board[y][x] = self.turn
-      self.turn = 1 - self.turn
-
-  def paint(self, painter, option, widget):
-    painter.setPen(Qt.black)
-    painter.drawLine(0, 100, 300, 100)
-    painter.drawLine(0, 200, 300, 200)
-    painter.drawLine(100, 0, 100, 300)
-    painter.drawLine(200, 0, 200, 300)
-
-    for y in range(3):
-      for x in range(3):
-        if self.board[y][x] == self.O:
-          painter.setPen(Qt.red)
-          painter.drawEllipse(QPointF(50 + x * 100, 50 + y * 100), 30, 30)
-        elif self.board[y][x] == self.X:
-          painter.setPen(Qt.blue)
-          painter.drawLine(20 + x * 100, 20 + y * 100, 80 + x * 100, 80 + y * 100)
-          painter.drawLine(20 + x * 100, 80 + y * 100, 80 + x * 100, 20 + y * 100)
-
-  def boundingRect(self):
-    return QRectF(0, 0, 300, 300)
-
-  def mousePressEvent(self, event):
-    pos = event.pos()
-    self.select(int(pos.x() / 100), int(pos.y() / 100))
-    self.update()
-    super(TicTacToe, self).mousePressEvent(event)
 
 class BondItem(QtWidgets.QGraphicsItem):
 

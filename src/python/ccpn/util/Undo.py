24e0624d038b8fcdc0a87d5e0100eab615c4ac2a
Ed Brooksbank <ejb66@le.ac.uk>
2018-02-26 14:53:14 +0000

    enabled the phasingFrame

--- src/python/ccpn/util/Undo.py
+++ src/python/ccpn/util/Undo.py
@@ -333,13 +333,14 @@ class Undo(deque):
 
         undoCall()
       self.nextIndex = undoTo + 1
-    # except Exception as e:
-    #   from ccpn.util.Logging import getLogger
-    #   getLogger().warning ("Error while undoing (%s). Undo stack is cleared." % e)
-    #   if self._debug:
-    #     print ("UNDO DEBUG: error in undo. Last undo function was:", undoCall)
-    #     raise
-    #   self.clear()
+
+    except Exception as e:
+      from ccpn.util.Logging import getLogger
+      getLogger().warning ("Error while undoing (%s). Undo stack is cleared." % e)
+      if self._debug:
+        print ("UNDO DEBUG: error in undo. Last undo function was:", undoCall)
+        raise
+      self.clear()
     finally:
       # Added by Rasmus March 2015. Surely we need to reset self._blocked?
       self._blocked = False
@@ -380,13 +381,14 @@ class Undo(deque):
         #   print ("@~@~ redoing", redoCall)
         redoCall()
       self.nextIndex = redoTo + 1
-    # except Exception as e:
-    #   from ccpn.util.Logging import getLogger
-    #   getLogger().warning("Error while redoing (%s). Undo stack is cleared." % e)
-    #   if self._debug:
-    #     print ("REDO DEBUG: error in redo. Last redo call was:", redoCall)
-    #     raise
-    #   self.clear()
+
+    except Exception as e:
+      from ccpn.util.Logging import getLogger
+      getLogger().warning("Error while redoing (%s). Undo stack is cleared." % e)
+      if self._debug:
+        print ("REDO DEBUG: error in redo. Last redo call was:", redoCall)
+        raise
+      self.clear()
     finally:
       # Added by Rasmus March 2015. Surely we need to reset self._blocked?
       self._blocked = False

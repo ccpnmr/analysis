99dbfff11d2de756a35f2f0d902d7f7c2762eac1
Ed Brooksbank <ejb66@le.ac.uk>
2018-08-27 23:51:06 +0100

    sorting line-selection in sequenceGraph

--- src/python/ccpn/ui/gui/widgets/Menu.py
+++ src/python/ccpn/ui/gui/widgets/Menu.py
@@ -1,5 +1,4 @@
 """Module Documentation here
-
 """
 #=========================================================================================
 # Licence, Reference and Credits
@@ -9,8 +8,7 @@ __credits__ = ("Wayne Boucher, Ed Brooksbank, Rasmus H Fogh, Luca Mureddu, Timot
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license",
                "or ccpnmodel.ccpncore.memops.Credits.CcpnLicense for licence text")
 __reference__ = ("For publications, please use reference from http://www.ccpn.ac.uk/v3-software/downloads/license",
-               "or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
-
+                 "or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
@@ -20,12 +18,12 @@ __version__ = "$Revision: 3.0.b3 $"
 #=========================================================================================
 # Created
 #=========================================================================================
-
 __author__ = "$Author: CCPN $"
 __date__ = "$Date: 2017-04-07 10:28:41 +0000 (Fri, April 07, 2017) $"
 #=========================================================================================
 # Start of code
 #=========================================================================================
+
 from PyQt5 import QtGui, QtWidgets
 
 from ccpn.ui.gui.widgets.Action import Action
@@ -35,38 +33,37 @@ from ccpn.framework.Translation import translator
 
 
 class Menu(QtWidgets.QMenu, Base):
-  def __init__(self, title, parent, isFloatWidget=False, **kw):
-    title = translator.translate(title)
-    QtWidgets.QMenu.__init__(self, title, parent)
-    Base.__init__(self, isFloatWidget=isFloatWidget, **kw)
-    self.isFloatWidget = isFloatWidget
-    self.setFont(menuFont)
-    self.setToolTipsVisible(True)
+    def __init__(self, title, parent, isFloatWidget=False, **kw):
+        title = translator.translate(title)
+        QtWidgets.QMenu.__init__(self, title, parent)
+        Base.__init__(self, isFloatWidget=isFloatWidget, **kw)
+        self.isFloatWidget = isFloatWidget
+        self.setFont(menuFont)
+        self.setToolTipsVisible(True)
 
+    def addItem(self, text, shortcut=None, callback=None, checked=True, checkable=False, icon=None, toolTip=None, **kwargs):
+        action = Action(self.parent(), text, callback=callback, shortcut=shortcut,
+                        checked=checked, checkable=checkable, icon=icon, toolTip=toolTip, isFloatWidget=self.isFloatWidget, **kwargs)
+        self.addAction(action)
+        return action
+        # print(shortcut)
 
-  def addItem(self, text, shortcut=None, callback=None, checked=True, checkable=False,icon=None, toolTip=None,**kwargs):
-    action = Action(self.parent(), text, callback=callback, shortcut=shortcut,
-                         checked=checked, checkable=checkable,icon=icon,toolTip=toolTip, isFloatWidget=self.isFloatWidget, **kwargs)
-    self.addAction(action)
-    return action
-    # print(shortcut)
+    def _addSeparator(self, *args, **kwargs):
+        separator = self.addSeparator()
+        return separator
 
-  def _addSeparator(self,*args, **kwargs):
-    separator = self.addSeparator()
-    return separator
+    def addMenu(self, title, **kwargs):
+        menu = Menu(title, self)
+        QtWidgets.QMenu.addMenu(self, menu)
+        return menu
 
-  def addMenu(self, title, **kwargs):
-    menu = Menu(title, self)
-    QtWidgets.QMenu.addMenu(self, menu)
-    return menu
+    def _addQMenu(self, menu):
+        ''' this adds a normal QMenu '''
+        QtWidgets.QMenu.addMenu(self, menu)
+        return menu
 
-  def _addQMenu(self, menu):
-    ''' this adds a normal QMenu '''
-    QtWidgets.QMenu.addMenu(self, menu)
-    return menu
 
 class MenuBar(QtWidgets.QMenuBar):
-  def __init__(self, parent):
-
-    QtWidgets.QMenuBar.__init__(self, parent)
-    self.setFont(menuFont)
+    def __init__(self, parent):
+        QtWidgets.QMenuBar.__init__(self, parent)
+        self.setFont(menuFont)

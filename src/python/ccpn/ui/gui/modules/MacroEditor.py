7904344f4b6ab5e3792444e499784a6bb83da1a2
Luca Mureddu <lgm8@leicester.ac.uk>
2018-08-28 18:42:28 +0100

    bug fixes on macro editor

--- src/python/ccpn/ui/gui/modules/MacroEditor.py
+++ src/python/ccpn/ui/gui/modules/MacroEditor.py
@@ -38,52 +38,47 @@ class MacroEditor(CcpnModule):
   includeSettingsWidget = False
   className = 'MacroEditor'
 
-  def __init__(self, mainWindow=None, name='Macro Editor', showRecordButtons=False):
+  def __init__(self, mainWindow=None, name='Macro Editor', filePath=None):
     CcpnModule.__init__(self, mainWindow=mainWindow, name=name)
 
-    self.mainWindow = mainWindow
-    self.application = mainWindow.application
-    self.project = mainWindow.application.project
-    self.current = mainWindow.application.current
-    self.preferences = self.application.preferences
+    if mainWindow:
+      self.mainWindow = mainWindow
+      self.application = mainWindow.application
+      self.project = mainWindow.application.project
+      self.current = mainWindow.application.current
+      self.preferences = self.application.preferences
 
-    # macro selection
-    self.label1 = Label(self, 'Macro Name')
-    self.lineEdit1 = LineEdit(self)
-    self.button = Button(self, text='Select...', callback=self._openMacroFile, vPolicy='fixed')
+    self.macroPath = None #self.preferences.general.userMacroPath
 
+    self.mainWidget.layout().setSpacing(5)
+    self.mainWidget.layout().setContentsMargins(10,10,10,10)
+
+    hGrid = 0
+    self.namelabel = Label(self.mainWidget, 'Macro Name', grid=(hGrid,0))
+    self.nameLineEdit = LineEdit(self.mainWidget,  grid=(0,1))
+    # self.nameLineEdit.editingFinished().connect(self._changedName)
+
+    hGrid +=1
     # macro editing area
-    self.textBox = TextEditor()
+    self.textBox = TextEditor(self.mainWidget, grid=(hGrid,0), gridSpan=(1,2))
 
-    # layouts
-    #widget = QtWidgets.QWidget()
-    widget = self.mainWidget
-    # widgetLayout = QtWidgets.QGridLayout()
-    # widget.setLayout(widgetLayout)
+    hGrid +=1
+    self.buttonBox = ButtonList(self, texts=['Open', 'Save', 'Run'],
+                                callbacks=[self._openMacroFile, self._saveMacro, None], grid = (hGrid,1))
 
-    widget.layout().setSpacing(5)
-    widget.layout().setContentsMargins(10,10,10,10)
-    widget.layout().addWidget(self.label1,    0, 0, 1, 1)
-    widget.layout().addWidget(self.lineEdit1, 0, 1, 1, 3)
-    widget.layout().addWidget(self.button,    0, 4, 1, 1)
-    widget.layout().addWidget(self.textBox,   1, 0, 1, 5)
+   # if  a path is specified then open it
+    self.filePath = filePath
 
-    self.buttonBox = ButtonList(self, texts=['Start', 'Stop', 'Close', 'Save Macro'],
-                                callbacks=[self._startMacroRecord, self._stopMacroRecord, self._reject, self._saveMacro])
-#    buttonLayout = QtWidgets.QGridLayout()
-#    self.buttonBox.setLayout(buttonLayout)
-#    self.buttonBox.layout().setSpacing(0)
+    if self.filePath:
 
-    self.buttonBox.buttons[1].setDisabled(True)
-    self.buttonBox.buttons[0].setEnabled(True)
-    widget.layout().addWidget(self.buttonBox, 2, 3, 1, 2)
+      self._openPath(filePath)
+      self._setFileName(filePath)
 
-#    self.layout().setSpacing(1)
-#    self.layout.addWidget(widget)
 
-    if showRecordButtons is False:
-      self.buttonBox.buttons[0].hide()
-      self.buttonBox.buttons[1].hide()
+  # def _changedName(self):
+  #   print()
+
+
 
 
   def _saveMacro(self):
@@ -91,43 +86,52 @@ class MacroEditor(CcpnModule):
     Saves the text inside the textbox to a file, if a file path is not specified, a save file dialog
     appears for specification of the file path.
     """
-    if self.lineEdit1.text() == '':
-      self._saveMacroAs()
-    else:
-      with open(self.lineEdit1.text(), 'w') as f:
+    if self.nameLineEdit.get() == self._getFileNameFromPath(self.filePath):
+      with open(self.filePath, 'w') as f:
         f.write(self.textBox.toPlainText())
         f.close()
+    else:
+      self._saveMacroAs()
 
-    # newText = self.textBox.toPlainText()
-    # self.macroFile.write(newText)
+
 
   def _saveMacroAs(self):
     """
     Opens a save file dialog and saves the text inside the textbox to a file specified in the dialog.
     """
-    macroPath = self.preferences.general.userMacroPath
+
+    # macroPath = self.preferences.general.userMacroPath or None
     # colourScheme = self.preferences.general.colourScheme
     newText = self.textBox.toPlainText()
     dialog = FileDialog(self, fileMode=FileDialog.AnyFile, text='Save Macro As...',
-                        acceptMode=FileDialog.AcceptSave, preferences=self.preferences.general,
-                        directory=macroPath, filter='*.py')
+                        acceptMode=FileDialog.AcceptSave,selectFile=self.nameLineEdit.text(),
+                        directory=None, filter='*.py')
     filePath = dialog.selectedFile()
+
     if filePath:
+      if not filePath.endswith('.py'):
+        filePath+='.py'
       with open(filePath, 'w') as f:
         f.write(newText)
         f.close()
+    self.filePath = filePath
 
   def _openMacroFile(self):
     """
     Opens a file dialog box at the macro path specified in the application preferences and loads the
     contents of the macro file into the textbox.
     """
-    macroPath = self.preferences.general.userMacroPath
+
     dialog = FileDialog(self, text='Open Macro', fileMode=FileDialog.ExistingFile,
-                        acceptMode=FileDialog.AcceptOpen, directory=macroPath,
-                        preferences=self.preferences.general, filter='*.py')
+                        acceptMode=FileDialog.AcceptOpen, directory=self.macroPath,
+                        filter='*.py')
 
     filePath = dialog.selectedFile()
+    self._openPath(filePath)
+    self._setFileName(filePath)
+
+  def _openPath(self, filePath):
+
     if filePath:
       with open(filePath, 'r') as f:
         self.textBox.clear()
@@ -135,27 +139,68 @@ class MacroEditor(CcpnModule):
           self.textBox.insertPlainText(line)
         self.macroFile = f
 
-      self.lineEdit1.setText(filePath)
+  def _setFileName(self, filePath):
 
-  def _startMacroRecord(self):
-    """
-    Starts recording of a macro from commands performed in the software and output to the console.
-    """
-    self.mainWindow.recordingMacro = True
-    self.buttonBox.buttons[1].setEnabled(True)
-    self.buttonBox.buttons[0].setDisabled(True)
-    self.mainWindow.editor = self
+    fileName = self._getFileNameFromPath(filePath)
+    self.nameLineEdit.set(str(fileName))
+
+  def _getFileNameFromPath(self, filePath):
+    if filePath.endswith('.py'):
+      path = filePath.split('/')
+      fileName = path[-1].split('.')[0]
+      return fileName
 
 
-  def _stopMacroRecord(self):
-    """
-    Stops macro recording.
-    """
-    self.mainWindow.recordingMacro = False
-    self.buttonBox.buttons[1].setDisabled(True)
-    self.buttonBox.buttons[0].setEnabled(True)
-    del self.mainWindow.editor
 
-  def _reject(self):
-    self.close()
+
+
+
+  # def _startMacroRecord(self):
+  #   """
+  #   Starts recording of a macro from commands performed in the software and output to the console.
+  #   """
+  #   self.mainWindow.recordingMacro = True
+  #   self.buttonBox.buttons[1].setEnabled(True)
+  #   self.buttonBox.buttons[0].setDisabled(True)
+  #   self.mainWindow.editor = self
+  #
+  #
+  # def _stopMacroRecord(self):
+  #   """
+  #   Stops macro recording.
+  #   """
+  #   self.mainWindow.recordingMacro = False
+  #   self.buttonBox.buttons[1].setDisabled(True)
+  #   self.buttonBox.buttons[0].setEnabled(True)
+  #   del self.mainWindow.editor
+
+
+
+if __name__ == '__main__':
+  from PyQt5 import QtGui, QtWidgets
+  from ccpn.ui.gui.widgets.Application import TestApplication
+  from ccpn.ui.gui.widgets.CcpnModuleArea import CcpnModuleArea
+  from ccpn.ui.gui.widgets.CheckBox import EditableCheckBox, CheckBox
+
+
+
+
+  app = TestApplication()
+  win = QtWidgets.QMainWindow()
+
+  moduleArea = CcpnModuleArea(mainWindow=None)
+
+  module = MacroEditor(mainWindow=None, filePath='/Users/luca/Desktop/tqqt.py')
+
+
+  moduleArea.addModule(module)
+
+  win.setCentralWidget(moduleArea)
+  win.resize(1000, 500)
+  win.setWindowTitle('Testing %s' % module.moduleName)
+  win.show()
+
+
+  app.start()
+  win.close()
 

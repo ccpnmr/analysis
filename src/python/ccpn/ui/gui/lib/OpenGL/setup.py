60ae09b4a8995087e4209c88fcf8c5f13a5d082e
Ed Brooksbank <ejb66@le.ac.uk>
2018-09-10 12:18:28 +0100

    cython testing

---
+++ src/python/ccpn/ui/gui/lib/OpenGL/setup.py
@@ -0,0 +1,54 @@
+"""Module Documentation here
+
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2017"
+__credits__ = ("Wayne Boucher, Ed Brooksbank, Rasmus H Fogh, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
+__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license",
+               "or ccpnmodel.ccpncore.memops.Credits.CcpnLicense for licence text")
+__reference__ = ("For publications, please use reference from http://www.ccpn.ac.uk/v3-software/downloads/license",
+               "or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: CCPN $"
+__dateModified__ = "$dateModified: 2017-07-07 16:32:44 +0100 (Fri, July 07, 2017) $"
+__version__ = "$Revision: 3.0.b3 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: CCPN $"
+__date__ = "$Date: 2017-04-07 10:28:41 +0000 (Fri, April 07, 2017) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+from distutils.core import setup
+from distutils.extension import Extension
+import sys
+
+if '--use-cython' in sys.argv:
+    USE_CYTHON = True
+    sys.argv.remove('--use-cython')
+else:
+    USE_CYTHON = False
+ext = '.pyx' if USE_CYTHON else '.c'
+
+# make a list of Extensions in here, or use '*', '*'+ext to catch all,
+# but puts .so files into a subdirectory /python
+# ignore 'does not match fully qualified name' warnings
+extensions = [Extension('CcpnOpenGLLabelling',
+                        ['CcpnOpenGLLabelling'+ext],
+                        language='c',
+                        include_dirs=['c/']),]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions,
+                        annotate=True)
+
+setup(
+    ext_modules = extensions
+)

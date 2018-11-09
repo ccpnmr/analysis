7f0f7ef95abfa3846b2f5a8804bf1f6cd3c602de
Luca Mureddu <lgm8@leicester.ac.uk>
2018-11-09 16:26:44 +0000

    temporary disabled 1d picker

--- src/python/ccpn/pipes/PeakPicker1DPipe.py
+++ src/python/ccpn/pipes/PeakPicker1DPipe.py
@@ -166,6 +166,6 @@ class PeakPicker1DPipe(SpectraPipe):
 
 
 
-PeakPicker1DPipe.register() # Registers the pipe in the pipeline
+# PeakPicker1DPipe.register() # Registers the pipe in the pipeline
 
 

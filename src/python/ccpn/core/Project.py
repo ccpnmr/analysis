42545b1ccb54cd131e0ca21210801405465a0a04
Ed Brooksbank <ejb66@le.ac.uk>
2018-11-17 01:50:49 +0000

    small bug in ListWidget; setObjects doesn't like UserRole more improvements to table notifiers

--- src/python/ccpn/core/Project.py
+++ src/python/ccpn/core/Project.py
@@ -758,7 +758,7 @@ class Project(AbstractWrapperObject):
         """ call object-has-changed notifiers
         """
         obj = self._data2Obj[wrappedData]
-        print('>>>_modifiedApiObject')
+        print('>>>_modifiedApiObject', obj)
         obj._finaliseAction('change')
 
     def _finaliseApiDelete(self, wrappedData):

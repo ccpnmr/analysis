53caaaf752a8f485fe750d2e6be33ebf1fbc38cc
Ed Brooksbank <ejb66@le.ac.uk>
2019-02-01 20:20:58 +0000

    1d peak picker just need to fix so that only picks in intensity range

--- src/python/ccpn/core/PeakList.py
+++ src/python/ccpn/core/PeakList.py
@@ -887,8 +887,6 @@ class PeakList(AbstractWrapperObject):
                 posLevel = posLevel or 0.0
                 negLevel = negLevel or 0.0
 
-                print(">>>dataArray", dataArray)
-
                 # Note: requires an exclusionBuffer of 1 in all axis directions
                 peakPoints = CPeak.findPeaks(dataArray, doNeg, doPos,
                                              negLevel, posLevel, exclusionBuffer,

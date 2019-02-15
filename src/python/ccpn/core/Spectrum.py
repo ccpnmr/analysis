3f14fbb90606745c3350ddc46f1ed968cf29e078
Geerten Vuister <gv29@leicester.ac.uk>
2019-02-15 08:42:47 +0000

    _clearCache method

--- src/python/ccpn/core/Spectrum.py
+++ src/python/ccpn/core/Spectrum.py
@@ -142,9 +142,9 @@ class Spectrum(AbstractWrapperObject):
 
     MAXDIM = 4  # Maximum dimensionality
 
-    PLANEDATACACHE = '_planeDataCache'  # Attribute name for the planeData cache
-    SLICEDATACACHE = '_sliceDataCache'  # Attribute name for the slicedata cache
-    SLICE1DDATACACHE = '_slice1DDataCache'  # Attribute name for the 1D slicedata cache
+    _PLANEDATACACHE = '_planeDataCache'  # Attribute name for the planeData cache
+    _SLICEDATACACHE = '_sliceDataCache'  # Attribute name for the slicedata cache
+    _SLICE1DDATACACHE = '_slice1DDataCache'  # Attribute name for the 1D slicedata cache
 
     def __init__(self, project: Project, wrappedData: Nmr.ShiftList):
 
@@ -1209,13 +1209,13 @@ class Spectrum(AbstractWrapperObject):
     def getPositionValue(self, position):
         return self._apiDataSource.getPositionValue(position)
 
-    @cached(SLICE1DDATACACHE, maxItems=1, debug=False)
+    @cached(_SLICE1DDATACACHE, maxItems=1, debug=False)
     def _get1DSliceData(self, position, sliceDim: int):
         """Internal routine to get 1D sliceData;
         """
         return self._apiDataSource.getSliceData(position=position, sliceDim=sliceDim)
 
-    @cached(SLICEDATACACHE, maxItems=1024, debug=False)
+    @cached(_SLICEDATACACHE, maxItems=1024, debug=False)
     def _getSliceDataFromPlane(self, position, xDim: int, yDim: int, sliceDim: int):
         """Internal routine to get sliceData; optimised to use (buffered) getPlaneData
         CCPNINTERNAL: used in CcpnOpenGL
@@ -1273,7 +1273,7 @@ class Spectrum(AbstractWrapperObject):
 
         return result
 
-    @cached(PLANEDATACACHE, maxItems=64, debug=False)
+    @cached(_PLANEDATACACHE, maxItems=64, debug=False)
     def _getPlaneData(self, position, xDim: int, yDim: int):
         "Internal routine to improve caching: Calling routine set the positions of xDim, yDim to 1 "
         return self._apiDataSource.getPlaneData(position=position, xDim=xDim, yDim=yDim)
@@ -1767,12 +1767,20 @@ class Spectrum(AbstractWrapperObject):
         """
         self._wrappedData.delete()
 
-    @cached.clear(PLANEDATACACHE)  # Check if there was a planedata cache, and if so, clear it
-    @cached.clear(SLICEDATACACHE)  # Check if there was a slicedata cache, and if so, clear it
+    @cached.clear(_PLANEDATACACHE)  # Check if there was a planedata cache, and if so, clear it
+    @cached.clear(_SLICEDATACACHE)  # Check if there was a slicedata cache, and if so, clear it
+    @cached.clear(_SLICE1DDATACACHE)  # Check if there was a slice1ddata cache, and if so, clear it
+    def _clearCache(self):
+        """Convenience to clear the cache; all action done by the decorators
+        """
+        pass
+
     def delete(self):
         """Delete Spectrum"""
         with logCommandBlock(get='self') as log:
             log('delete')
+
+            self._clearCache()
 
             # handle spectrumView ordering - this should be moved to spectrumView or spectrumDisplay via notifier?
             specDisplays = []

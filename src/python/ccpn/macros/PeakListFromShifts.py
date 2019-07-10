0fc9f5899cd89e664fbec283f63d4fa82b2c5d5d
Vicky <victoria.higman@bristol.ac.uk>
2019-07-10 11:50:26 +0100

    first bit of pseudocode...

--- src/python/ccpn/macros/PeakListFromShifts.py
+++ src/python/ccpn/macros/PeakListFromShifts.py
@@ -1,19 +1,30 @@
 #Macro to create a (synthetic) peak list from a chemical shift list
 #
-# What this macro needs to do:
-# User specifies a chemical shift list (CSL)
+# What this macro ultimately needs to do:
+# User specifies a chemical shift list
 # User specifies an existing spectrum
-#   macro identifies axis codes and asks for atom types for each axis
-#   (at some stage use Experiment types?)
-#   (ideally as drop-down based on atom types in CSL?)
-#   (ideally include ability to have multiple atom types per axis (HNCACB!))
-#   create a new peak list with peaks for all residues where the specified atom types exist in the CSL
-#   (Let user know if no peaks could be created)
-# (Alternatively user specifies a new (type of) spectrum ...)
+#   - macro identifies axis codes and asks for atom types for each axis
+#     (ideally as drop-down based on atom types in CSL?)
+#     (ideally include ability to have multiple atom types per axis (HNCACB!))
+#     (at some stage use Experiment types? (i.e. if there is an experiment type, automatically set the atom types,
+#     rather than asking the user))
+#   - create a new peak list with peaks for all residues where the specified atom types exist in the CSL
+#   - (Let user know if no peaks could be created)
+# (Alternatively user specifies a new (type of) spectrum in which case create a dummy spectrum before
+# creating and populating the peak list)
 
+
+# Try first:
+# get the default csl
+# specify atom types as HN
+# create a new HSQC peak list
+# populate the peak list with peaks
+
+# specify chemical shift list
 csl = project.getByPid('CL:default')
 
-# set axis codes for new peak list (HN)
+# set Atom types wanted in peak list
+atomTypes = [H, N]
 
 # create new HSQC peaklist (based on axis codes)
 get('SP:hsqc').newPeakList(title=None, comment=None, isSimulated=False, symbolStyle=None, symbolColour=None, textColour=None)
@@ -21,6 +32,9 @@ get('SP:hsqc').newPeakList(title=None, comment=None, isSimulated=False, symbolSt
 # Loop through each residue in the chemical shift list
 for residue in csl
     # check that both H and N atoms exist for that residue
-    if have H and N atoms
-       #  create an NH peak in the peak list using the CS in the csl
-       create peak in peaklist
+    if cslNmrAtoms of residue include all atomTypes
+       #  create an HN peak in the peak list using the ChemShifts and NmrAtoms in the csl
+       peak = peakList.newPeak(cslHAtomShift, cslNAtomShift)
+       peak.assignDimension(cslHNmrAtom, cslNNmrAtom)
+
+# if no peaks created, give an error message

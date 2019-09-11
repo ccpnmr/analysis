b41281cff13cf7ea90d8fa439aae476957af7fb2
Ed Brooksbank <ejb66@le.ac.uk>
2019-09-11 18:40:07 +0100

    updated residue information widget - added width selection and sequence module small bug in sequence graph fixed for empty nmrChain

--- src/python/ccpn/ui/gui/modules/ResidueInformation.py
+++ src/python/ccpn/ui/gui/modules/ResidueInformation.py
@@ -31,6 +31,9 @@ from ccpn.ui.gui.widgets.Spacer import Spacer
 from ccpn.ui.gui.widgets.Widget import Widget
 from ccpn.ui.gui.widgets.PulldownList import PulldownList
 from ccpn.ui.gui.widgets.ScrollArea import ScrollArea
+from ccpn.ui.gui.widgets.Splitter import Splitter
+from ccpn.ui.gui.widgets.Frame import Frame
+from ccpn.ui.gui.modules.SequenceModule import SequenceModule
 from ccpn.ui.gui.guiSettings import getColours
 from ccpn.ui.gui.guiSettings import LABEL_SELECTEDBACKGROUND, LABEL_SELECTEDFOREGROUND, LABEL_HIGHLIGHT
 
@@ -41,47 +44,92 @@ class ResidueInformation(CcpnModule):
     settingsPosition = 'top'
     className = 'ResidueInformation'
 
+    _residueWidth = '3'
+
     def __init__(self, mainWindow, name='Residue Information', **kwds):
         CcpnModule.__init__(self, mainWindow=mainWindow, name=name)
 
+        # Derive application, project, and current from mainWindow
         self.mainWindow = mainWindow
-        self.application = mainWindow.application
-        self.project = mainWindow.application.project
-        self.current = mainWindow.application.current
+        if self.mainWindow:
+            self.application = mainWindow.application
+            self.project = mainWindow.application.project
+            self.current = mainWindow.application.current
+        else:
+            self.application = None
+            self.project = None
+            self.current = None
 
+        # add a splitter to contain the residue table and the sequence module
+        self.splitter = Splitter(self.mainWidget, horizontal=False)
+        self._sequenceModuleFrame = Frame(None, setLayout=True)
+        # self._SequenceGraphFrame = Frame(self.splitter, setLayout=True)
+        self.mainWidget.getLayout().addWidget(self.splitter, 1, 0)
+
+        # initialise the sequence module
+        self.thisSequenceModule = SequenceModule(moduleParent=self,
+                                                 parent=self._sequenceModuleFrame,
+                                                 mainWindow=mainWindow)
+
+        # add a scroll area to contain the residue table
         self._widgetScrollArea = ScrollArea(parent=self.mainWidget, grid=(0, 0), scrollBarPolicies=('asNeeded', 'asNeeded'), **kwds)
         self._widgetScrollArea.setWidgetResizable(True)
         self._widget = Widget(parent=self._widgetScrollArea, setLayout=True)
         self._widgetScrollArea.setWidget(self._widget)
         self._widget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
 
-        self.chainLabel = Label(self._widget, text='Chain', grid=(0, 0))
+        # insert into the mainWidget
+        self.splitter.addWidget(self._widgetScrollArea)
+        self.splitter.addWidget(self._sequenceModuleFrame)
+        self.splitter.setStretchFactor(0, 5)
+        self.splitter.setChildrenCollapsible(False)
+
+        # make a frame to contain the pulldown widgets
+        self._pulldownFrame = Frame(self._widget, setLayout=True, showBorder=False, grid=(0, 0))
+
+        # insert into pulldownFrame
+        self.chainLabel = Label(self._pulldownFrame, text='Chain', grid=(0, 0))
         # self.layout.addWidget(chainLabel, 0, 0)
-        chainPulldown = PulldownList(self._widget, callback=self._setChain, grid=(0, 1))
+        chainPulldown = PulldownList(self._pulldownFrame, callback=self._setChain, grid=(0, 1))
         chainPulldownData = [chain.pid for chain in self.project.chains]
         chainPulldownData.append('<All>')
         chainPulldown.setData(chainPulldownData)
         self.selectedChain = self.project.getByPid(chainPulldown.currentText())
-        self.residueLabel = Label(self._widget, text='Residue ', grid=(0, 3))
+        self.residueLabel = Label(self._pulldownFrame, text='Residue ', grid=(0, 3))
 
         self.colourScheme = self.application.colourScheme
-        self.residuePulldown = PulldownList(self._widget, callback=self._setCurrentResidue,
+        self.residuePulldown = PulldownList(self._pulldownFrame, callback=self._setCurrentResidue,
                                             grid=(0, 4))
+
+        self._residueWidthLabel = Label(self._pulldownFrame, text='Residue window width', grid=(0, 5))
+        self._residueWidthData = PulldownList(self._pulldownFrame, #callback=self._setResidueWidth,
+                                            grid=(0, 6))
+        self._residueWidthData.setData(texts=['1', '3', '5', '7'])
+        self._residueWidthData.set(self._residueWidth)
+
         self.residuePulldown.setData(CCP_CODES)
         self.selectedResidueType = self.residuePulldown.currentText()
+
+        # set the callback after populating
+        self._residueWidthData.setCallback(self._setResidueWidth)
+        # add under the pulldownFrame
         self.residueWidget = Widget(self._widget, setLayout=True,
-                                    grid=(1, 0), gridSpan=(1, 5))
+                                    grid=(1, 0), gridSpan=(1, 2))
 
         self.spacer = Spacer(self._widget, 5, 5,
                              QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding,
-                             grid=(2, 5), gridSpan=(1, 1))
+                             grid=(2, 3), gridSpan=(1, 1))
 
-        # self._setWidgetHeight(35)
+        self._widget.setContentsMargins(5, 5, 5, 5)
 
         # self.residueWidget = QtWidgets.QWidget(self)
         # self.residueWidget.setLayout(QtWidgets.QGridLayout())
         # self.project = project
         # self.layout.addWidget(self.residueWidget, 1, 0, 1, 5)
+        self._getResidues()
+
+    def _setResidueWidth(self, *args):
+        self._residueWidth = self._residueWidthData.get()
         self._getResidues()
 
     def _setChain(self, value: str):
@@ -108,6 +156,23 @@ class ResidueInformation(CcpnModule):
         palette.setColor(QtGui.QPalette.Foreground, QtGui.QColor(LABEL_SELECTEDFOREGROUND))
         palette.setColor(QtGui.QPalette.Background, QtGui.QColor(LABEL_SELECTEDBACKGROUND))
         widget.setPalette(palette)
+
+    def _removeWidget(self, widget, removeTopWidget=False):
+        """Destroy a widget and all it's contents
+        """
+        def deleteItems(layout):
+            if layout is not None:
+                while layout.count():
+                    item = layout.takeAt(0)
+                    widget = item.widget()
+                    if widget is not None:
+                        widget.setVisible(False)
+                        widget.setParent(None)
+                        del widget
+
+        deleteItems(widget.getLayout())
+        if removeTopWidget:
+            del widget
 
     def _getResidues(self):
         """
@@ -146,47 +211,43 @@ class ResidueInformation(CcpnModule):
                 return
 
         if residues:
-            for residue in residues:
+            width = int(self._residueWidthData.get()) // 2
+
+            for resInd, residue in enumerate(residues):
                 if residue.residueType == self.selectedResidueType.upper():
-                    foundResidues.append([residue.previousResidue, residue, residue.nextResidue])
-            layout = self.residueWidget.layout()
-            for r in range(layout.rowCount()):
-                for n in range(3):
-                    item = layout.itemAtPosition(r, n)
-                    if item is not None:
-                        item.widget().deleteLater()
+                    # add the previous and next residue chains to the visible list for this residue
+                    resList = [residue]
+                    leftRes = residue
+                    rightRes = residue
+                    for count in range(width):
+                        if leftRes:
+                            resList.insert(0, leftRes.previousResidue)
+                            leftRes = leftRes.previousResidue
+                        else:
+                            resList.insert(0, None)
+                    for count in range(width):
+                        if rightRes:
+                            resList.append(rightRes.nextResidue)
+                            rightRes = rightRes.nextResidue
+                        else:
+                            resList.append(None)
+                    foundResidues.append(resList)
 
-            j = 0  # why was this introduced (it's not altered below)?
-            for i in range(len(foundResidues)):
+            self._removeWidget(self.residueWidget, removeTopWidget=False)
 
-                checkResidues = foundResidues[j + i]
+            for i, checkResidues in enumerate(foundResidues):
+                for rr in range(int(self._residueWidthData.get())):
+                    if rr >= 0 and rr < len(checkResidues):
+                        if checkResidues[rr] is not None:
+                            label = Label(self, text=checkResidues[rr].id,
+                                           hAlign='c')
+                            label.setMaximumHeight(30)
+                            if checkResidues[rr].nmrResidue is not None:
+                                label.setStyleSheet(stylesheet)
+                                # self._setWidgetColour(label1)
 
-                if checkResidues[0] is not None:
-                    label1 = Label(self, text=checkResidues[0].id,
-                                   hAlign='c')
-                    label1.setMaximumHeight(30)
-                    if checkResidues[0].nmrResidue is not None:
-                        label1.setStyleSheet(stylesheet)
-                        # self._setWidgetColour(label1)
+                            self.residueWidget.layout().addWidget(label, i, rr)
 
-                    self.residueWidget.layout().addWidget(label1, j + i, 0)
-                    
-                if len(checkResidues) > 1 and checkResidues[1] is not None:
-                    label2 = Label(self, text=checkResidues[1].id,
-                                   hAlign='c')
-                    if checkResidues[1].nmrResidue is not None:
-                        label2.setStyleSheet(stylesheet)
-                        # self._setWidgetColour(label2)
-
-                    label2.setMaximumHeight(30)
-                    self.residueWidget.layout().addWidget(label2, j + i, 1)
-                    
-                if len(checkResidues) > 2 and checkResidues[2] is not None:
-                    label3 = Label(self, text=checkResidues[2].id,
-                                   hAlign='c')
-                    if checkResidues[2].nmrResidue is not None:
-                        label3.setStyleSheet(stylesheet)
-                        # self._setWidgetColour(label3)
-
-                    self.residueWidget.layout().addWidget(label3, j + i, 2)
-                    label3.setMaximumHeight(30)
+            self.spacer = Spacer(self.residueWidget, 5, 5,
+                                 QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding,
+                                 grid=(i+1, rr+1), gridSpan=(1, 1))

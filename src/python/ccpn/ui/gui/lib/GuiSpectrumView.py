ccpnmr/analysis

commit cee7be94a1594315edf2b0f5a21e069534754bfe

modify spectrum toolbar buttons to deal with devicePixelRatio
Oh boy what a hack!

src/python/ccpn/ui/gui/lib/GuiSpectrumView.py
@@ -43,6 +43,7 @@ SpectrumViewParams = collections.namedtuple('SpectrumViewParams', ('valuePerPoin
                                                                    'maxAliasedFrequency',
                                                                    'dataDim'))
 
+WIDGET = 'widget'
 
 class GuiSpectrumView(QtWidgets.QGraphicsObject):
 
@@ -198,6 +199,7 @@ def _spectrumViewHasChanged(data):
     NB SpectrumView change notifiers are triggered when either DataSource or ApiSpectrumView change.
     """
     self = data[Notifier.OBJECT]
+    widget = data[WIDGET]
 
     if self.isDeleted:
         return
@@ -208,7 +210,7 @@ def _spectrumViewHasChanged(data):
     # Update action icol colour
     action = spectrumDisplay.spectrumActionDict.get(apiDataSource)
     if action:
-        pix = QtGui.QPixmap(QtCore.QSize(60, 10))
+        pix = QtGui.QPixmap(QtCore.QSize(60/widget.devicePixelRatio(), 10/widget.devicePixelRatio()))
         if self._showContours:
             if spectrumDisplay.is1D:
                 pix.fill(QtGui.QColor(self.sliceColour))

src/python/ccpn/ui/gui/widgets/SpectrumToolBar.py
@@ -39,7 +39,7 @@ from ccpn.ui._implementation.MultipletListView import MultipletListView
 from ccpn.core.PeakList import PeakList
 from ccpn.core.IntegralList import IntegralList
 from ccpn.core.MultipletList import MultipletList
-from ccpn.ui.gui.lib.GuiSpectrumView import _spectrumViewHasChanged
+from ccpn.ui.gui.lib.GuiSpectrumView import _spectrumViewHasChanged,WIDGET
 from ccpn.ui.gui.popups.SpectrumPropertiesPopup import SpectrumPropertiesPopup
 from ccpn.core.lib import Pid
 from ccpn.ui.gui.lib.GuiStripContextMenus import _SCMitem, ItemTypes, ITEM, _addMenuItems, _createMenu, _separator
@@ -211,7 +211,7 @@ class SpectrumToolBar(ToolBar):
         from ccpn.ui.gui.lib.OpenGL.CcpnOpenGL import GLNotifier
 
         specView._showContours = not specView._showContours
-        _spectrumViewHasChanged({Notifier.OBJECT: specView})
+        _spectrumViewHasChanged({Notifier.OBJECT: specView, WIDGET : self})
 
         GLSignals = GLNotifier(parent=self)
         GLSignals.emitPaintEvent()
@@ -477,7 +477,7 @@ class SpectrumToolBar(ToolBar):
 
                 spectrumDisplay.spectrumActionDict[apiDataSource] = action
                 # The following call sets the icon colours:
-                _spectrumViewHasChanged({Notifier.OBJECT: spectrumView})
+                _spectrumViewHasChanged({Notifier.OBJECT: spectrumView, WIDGET : self})
 
         # if spectrumDisplay.is1D:
         #     action.toggled.connect(spectrumView.plot.setVisible)

commit 32535637f5b136607c8692429c1c8ef4f4d90baa

remove regression caused by cee7be94a159
use a simple impimentation to get relevant widget

src/python/ccpn/ui/gui/lib/GuiSpectrumView.py
@@ -43,8 +43,6 @@ SpectrumViewParams = collections.namedtuple('SpectrumViewParams', ('valuePerPoin
                                                                    'maxAliasedFrequency',
                                                                    'dataDim'))
 
-WIDGET = 'widget'
-
 class GuiSpectrumView(QtWidgets.QGraphicsObject):
 
     #def __init__(self, guiSpectrumDisplay, apiSpectrumView, dimMapping=None):
@@ -199,7 +197,6 @@ def _spectrumViewHasChanged(data):
     NB SpectrumView change notifiers are triggered when either DataSource or ApiSpectrumView change.
     """
     self = data[Notifier.OBJECT]
-    widget = data[WIDGET]
 
     if self.isDeleted:
         return
@@ -210,7 +207,7 @@ def _spectrumViewHasChanged(data):
     # Update action icol colour
     action = spectrumDisplay.spectrumActionDict.get(apiDataSource)
     if action:
-        pix = QtGui.QPixmap(QtCore.QSize(60/widget.devicePixelRatio(), 10/widget.devicePixelRatio()))
+        pix = QtGui.QPixmap(QtCore.QSize(60/spectrumDisplay.devicePixelRatio(), 10/spectrumDisplay.devicePixelRatio()))
         if self._showContours:
             if spectrumDisplay.is1D:
                 pix.fill(QtGui.QColor(self.sliceColour))

src/python/ccpn/ui/gui/widgets/SpectrumToolBar.py
@@ -39,7 +39,7 @@ from ccpn.ui._implementation.MultipletListView import MultipletListView
 from ccpn.core.PeakList import PeakList
 from ccpn.core.IntegralList import IntegralList
 from ccpn.core.MultipletList import MultipletList
-from ccpn.ui.gui.lib.GuiSpectrumView import _spectrumViewHasChanged,WIDGET
+from ccpn.ui.gui.lib.GuiSpectrumView import _spectrumViewHasChanged
 from ccpn.ui.gui.popups.SpectrumPropertiesPopup import SpectrumPropertiesPopup
 from ccpn.core.lib import Pid
 from ccpn.ui.gui.lib.GuiStripContextMenus import _SCMitem, ItemTypes, ITEM, _addMenuItems, _createMenu, _separator
@@ -211,7 +211,7 @@ class SpectrumToolBar(ToolBar):
         from ccpn.ui.gui.lib.OpenGL.CcpnOpenGL import GLNotifier
 
         specView._showContours = not specView._showContours
-        _spectrumViewHasChanged({Notifier.OBJECT: specView, WIDGET : self})
+        _spectrumViewHasChanged({Notifier.OBJECT: specView})
 
         GLSignals = GLNotifier(parent=self)
         GLSignals.emitPaintEvent()
@@ -477,7 +477,7 @@ class SpectrumToolBar(ToolBar):
 
                 spectrumDisplay.spectrumActionDict[apiDataSource] = action
                 # The following call sets the icon colours:
-                _spectrumViewHasChanged({Notifier.OBJECT: spectrumView, WIDGET : self})
+                _spectrumViewHasChanged({Notifier.OBJECT: spectrumView})
 
         # if spectrumDisplay.is1D:
         #     action.toggled.connect(spectrumView.plot.setVisible)
f264a1b6133cd56ce760f189124f543b09a22492
Vicky <victoria.higman@bristol.ac.uk>
2020-03-16 10:03:07 +0000

    new macro

--- src/python/ccpn/macros/ideas/CCPeakListFromShifts.py
+++ src/python/ccpn/macros/ideas/CCPeakListFromShifts.py
@@ -1,38 +1,40 @@
-#Macro to create a CC peak list from an imported BRMB chemical shift list
+# Macro to create a CC peak list from a chemical shift list
 
-# specify chemical shift list
-csl = project.getByPid('CL:chemicalshiftlist_1')
-
-# specify PDSD spectrum
-targetSpectrum = project.getByPid('SP:PDSD')
-
-# create new peaklist for specified PDSD spectrum
-peakList = targetSpectrum.newPeakList(title=None, comment=None, isSimulated=True, symbolStyle=None, symbolColour=None, textColour=None)
+from ccpn.core.lib.ContextManagers import undoBlock
 
 # specify NmrChain associated with the imported chemical shifts
-nmrCh = project.getByPid('NC:@2')
+nmrChain = project.getByPid('NC:@2')
 
-# Loop through each NmrResidue in the NmrChain
-for nmrRes in nmrCh:
-    # Loop through each NmrAtom
-    atomlist = []
-    for nmrAt in nmrRes:
-        # Check that the NmrAtom is a carbon atom
-        if nmrAt[0] == 'C':
-            atomlist.append(nmrAt)
-    # create all combinations of C-C peaks and assign them
-    while len(atomlist) > 1:
-        v = atomlist.pop()
-        for w in atomlist:
-            #get chemShifts of v and w NmrAtoms from csl
+# specify CC spectrum
+targetSpectrum = project.getByPid('SP:sh3_uni_pdsd100')
+# find carbon dimensions
+carbonAxes = [axis for axis in targetSpectrum.axisCodes if axis.startswith('C')]
 
-            #create peak with atoms v and w
-            peak = peakList.newPeak((vChemShift,wChemShift))
-            peak.assignDimension(axisCode='C', v)
-            peak.assignDimension(axisCode='C1', w)
-            #create second peak with shifts/atoms the other way round
-            peak = peakList.newPeak((wChemShift, vChemshift))
-            peak.assignDimension(axisCode='C', w)
-            peak.assignDimension(axisCode='C1', v)
 
-# Is there an issue if peaks are created outside the spectrum bounds??
+def makePairs(theList):
+    "make pairs from the elements of theList (excluding same-item pairs); return a list of tuples"
+    result = []
+    for idx, item1 in enumerate(theList):
+        for item2 in theList[idx + 1:]:
+            result.append((item1, item2))
+            result.append((item2, item1))
+    return result
+
+
+with undoBlock():
+    # create new peaklist for specified  spectrum
+    peakList = targetSpectrum.newPeakList(isSimulated=True)
+    print('created:', peakList)
+
+    # Loop through each NmrResidue in the NmrChain
+    for nmrRes in nmrChain.nmrResidues:
+        # Loop through each NmrAtom, selecting each carbon
+        atomList = [nmrAtom for nmrAtom in nmrRes.nmrAtoms if nmrAtom.name.startswith('C')]
+
+        # create C-C peaks according to makePairs routine
+        pairs = makePairs(atomList)
+        for atm1, atm2 in pairs:
+            peak = peakList.newPeak(ppmPositions=(atm1.chemicalShifts[0].value, atm2.chemicalShifts[0].value))
+            peak.assignDimension(carbonAxes[0], atm1)
+            peak.assignDimension(carbonAxes[1], atm2)
+

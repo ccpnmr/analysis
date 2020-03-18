24e58e2037f1d4ae7befb27e12ffef472090dabd
Ed Brooksbank <ejb66@le.ac.uk>
2020-03-18 18:28:54 +0000

    edited paths/bin/bat for macOS/windows edited imports (nefimporter) to match python 2.7 check specification - new mmcif_nef_v1_1.dic does not have parent_category_id! Why not? texteditor patch for new fonts

--- bat/ccpnNefIo.bat
+++ bat/ccpnNefIo.bat
@@ -1,9 +1,7 @@
 @echo off
 setlocal
-pushd..
-set CCPNMR_TOP_DIR=%CD%
-popd
-call "paths"
+set CCPNMR_TOP_DIR=%~dp0\..
+call "%~dp0\paths"
 
 set ENTRYMODULE="%CCPNMR_TOP_DIR%"\src\python\ccpn\core\lib\CcpnNefIo.py
 "%ANACONDA3%"\python -i -O -W ignore "%ENTRYMODULE%" %*

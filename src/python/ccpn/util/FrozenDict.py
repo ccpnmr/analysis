59f3791e7c7f336ae2d0923b48937a9d827f5179
varioustoxins <garyt.and.sarahb@gmail.com>
2020-04-05 13:30:34 +0100

    Add a simple frozen dictionary

---
+++ src/python/ccpn/util/FrozenDict.py
@@ -0,0 +1,22 @@
+
+# from https://stackoverflow.com/questions/2703599/what-would-a-frozen-dict-be
+class FrozenDict(dict):
+    def __init__(self, *args, **kwargs):
+        self._hash = None
+        super(FrozenDict, self).__init__(*args, **kwargs)
+
+    def __hash__(self):
+        if self._hash is None:
+            self._hash = hash(tuple(sorted(self.items())))  # iteritems() on py2
+        return self._hash
+
+    def _immutable(self, *args, **kws):
+        raise TypeError('cannot change object - object is immutable')
+
+    __setitem__ = _immutable
+    __delitem__ = _immutable
+    pop = _immutable
+    popitem = _immutable
+    clear = _immutable
+    update = _immutable
+    setdefault = _immutable

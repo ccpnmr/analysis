47ac9e99d6503c7988f640661f8caa9ab4261a6a
Ed Brooksbank <ejb66@le.ac.uk>
2020-06-23 18:47:10 +0100

    added Gary's name to FrozenDict.py

--- src/python/ccpn/util/FrozenDict.py
+++ src/python/ccpn/util/FrozenDict.py
@@ -1,3 +1,29 @@
+"""
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
+__credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
+__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
+                 "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
+                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2020-06-23 18:47:11 +0100 (Tue, June 23, 2020) $"
+__version__ = "$Revision: 3.0.1 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: Gary Thompson $"
+__date__ = "$Date: 2017-04-07 10:28:41 +0000 (Fri, April 07, 2017) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
 
 # from https://stackoverflow.com/questions/2703599/what-would-a-frozen-dict-be
 class FrozenDict(dict):

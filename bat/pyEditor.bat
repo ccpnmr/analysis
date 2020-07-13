6c09bd5e0213da0011a419957580059fa5c2ac5b
Ed Brooksbank <ejb66@le.ac.uk>
2020-07-13 12:31:26 +0100

    added new windows script for pyEditor.bat

---
+++ bat/pyEditor.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+set CCPNMR_TOP_DIR=%~dp0\..
+call "%~dp0\paths"
+
+set ENTRYMODULE=%CCPNMR_TOP_DIR%\src\python\ccpn\ui\gui\modules\MacroEditor.py
+"%ANACONDA3%"\python -i -O -W ignore "%ENTRYMODULE%" %*
+endlocal

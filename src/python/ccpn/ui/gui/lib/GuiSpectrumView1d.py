added new colour gradients to marks colour and spectrum contours  - gradients are ignored by autocolour fix  - marks now cycle through colours of gradient  - minor speedup to print Nd but still a little slow

src/python/ccpn/ui/gui/lib/GuiSpectrumView1d.py
@@ -4,7 +4,7 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2019"
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
 __credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
@@ -13,9 +13,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: CCPN $"
-__dateModified__ = "$dateModified: 2017-07-07 16:32:44 +0100 (Fri, July 07, 2017) $"
-__version__ = "$Revision: 3.0.0 $"
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2020-07-23 17:10:53 +0100 (Thu, July 23, 2020) $"
+__version__ = "$Revision: 3.0.1 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -33,7 +33,7 @@ import pyqtgraph as pg
 
 from ccpn.ui.gui.lib.GuiSpectrumView import GuiSpectrumView
 
-from ccpn.util.Colour import spectrumColours
+from ccpn.util.Colour import spectrumColours, colorSchemeTable
 from ccpn.util import Phasing
 
 
@@ -168,6 +168,10 @@ class GuiSpectrumView1d(GuiSpectrumView):
         # glList.indices = np.arange(numVertices, dtype=np.uint32)
 
         colour = self._getColour('sliceColour', '#AAAAAA')
+        if not colour.startswith('#'):
+            # get the colour from the gradient table or a single red
+            colour = colorSchemeTable[colour][0] if colour in colorSchemeTable else '#FF0000'
+
         colR = int(colour.strip('# ')[0:2], 16) / 255.0
         colG = int(colour.strip('# ')[2:4], 16) / 255.0
         colB = int(colour.strip('# ')[4:6], 16) / 255.0
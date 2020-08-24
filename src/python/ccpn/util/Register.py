3e7dafdbd63a13880ae35a746a3d5a53c4a96d56
Ed Brooksbank <ejb66@le.ac.uk>
2020-08-24 18:19:29 +0100

    minor change to registration to write fewer logs

--- src/python/ccpn/util/Register.py
+++ src/python/ccpn/util/Register.py
@@ -4,7 +4,7 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2019"
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
 __credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
@@ -13,9 +13,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: CCPN $"
-__dateModified__ = "$dateModified: 2017-07-07 16:32:59 +0100 (Fri, July 07, 2017) $"
-__version__ = "$Revision: 3.0.0 $"
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2020-08-24 18:19:29 +0100 (Mon, August 24, 2020) $"
+__version__ = "$Revision: 3.0.1 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -144,7 +144,9 @@ def updateServer(registrationDict, version='3'):
 
     values['version'] = str(version)
     values['OSversion'] = platform.platform()
-    values['systemInfo'] = ';'.join(platform.uname())
+    # values['systemInfo'] = ';'.join(platform.uname())
+    _uname = platform.uname()
+    values['systemInfo'] = ';'.join((_uname.system, _uname.machine, _uname.processor))
     values['ID'] = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
     values['compileVersion'] = _getCompileVersion()
     var0, var1, var2, _ = _getBuildID()
@@ -210,7 +212,9 @@ def checkServer(registrationDict, version='3'):
 
     values['version'] = str(version)
     values['OSversion'] = platform.platform()
-    values['systemInfo'] = ';'.join(platform.uname())
+    # values['systemInfo'] = ';'.join(platform.uname())
+    _uname = platform.uname()
+    values['systemInfo'] = ';'.join((_uname.system, _uname.machine, _uname.processor))
     values['ID'] = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
     values['compileVersion'] = _getCompileVersion()
     var0, var1, var2, _ = _getBuildID()

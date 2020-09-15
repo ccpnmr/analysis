98d154aefa211724d19daf08d6628c4e52f4c0fe
Ed Brooksbank <ejb66@le.ac.uk>
2020-09-15 18:36:11 +0100

    minor change to wording of DeleteItems.py popup

--- src/python/ccpn/ui/gui/popups/DeleteItems.py
+++ src/python/ccpn/ui/gui/popups/DeleteItems.py
@@ -4,7 +4,7 @@ Module Documentation here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2019"
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
 __credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
@@ -13,9 +13,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: CCPN $"
-__dateModified__ = "$dateModified: 2017-07-07 16:32:48 +0100 (Fri, July 07, 2017) $"
-__version__ = "$Revision: 3.0.0 $"
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2020-09-15 18:36:11 +0100 (Tue, September 15, 2020) $"
+__version__ = "$Revision: 3.0.1 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -63,7 +63,8 @@ class DeleteItemsPopup(CcpnDialogMainWidget):
             newCheckBox = CheckBoxCompoundWidget(self.mainWidget,
                                                  grid=(row, 0), vAlign='top', stretch=(0, 0), hAlign='left',
                                                  orientation='right',
-                                                 labelText='%i %s' % (len(values), itemName),
+                                                 # assume that the name is plural
+                                                 labelText='{} {}{}'.format(len(values), itemName.rstrip('s'), 's' if len(values) > 1 else ''),
                                                  checked=True if itemName in ['peaks', 'Peaks'] else False
                                                  )
             newCheckBox.setToolTip('\n'.join([str(obj.pid) for obj in values]))

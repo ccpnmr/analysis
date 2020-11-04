73e8e276c2c93f73a0fc318f30bd04bf60c40fe2
Luca Mureddu <lgm8@leicester.ac.uk>
2020-11-04 13:07:14 +0000

    Bmrb CSL importer bug fix for multiple chains. Partially refactored.

--- src/python/ccpn/core/lib/CcpnStarIo.py
+++ src/python/ccpn/core/lib/CcpnStarIo.py
@@ -10,6 +10,7 @@ from ccpn.util.Common import getAxisCodeMatchIndices
 from ccpn.framework.PathsAndUrls import macroPath as mp
 from ccpn.core.lib.ContextManagers import undoBlock
 from ccpn.util.Logging import getLogger
+from ccpn.core.lib.ContextManagers import notificationEchoBlocking
 
 NMRSTARVersion = '3.2.1.32'
 NMRSTARV3GROUPS = od([
@@ -140,21 +141,36 @@ NMRSTARV3GROUPS = od([
 
 
 
-_Atom_chem_shift = "_Atom_chem_shift." # used to find the loop of interest in the BMRB and shorten the column header name on the DataFrame
-
-# only the entries of interest to map to CcpNmr V3 objects. Can be increased as needed
-ID = "ID"
-Seq_ID = "Seq_ID"
-Comp_ID = "Comp_ID"
-Atom_ID = "Atom_ID"
-Atom_type = "Atom_type"
-Val = "Val"
-Val_err = "Val_err"
-Details = "Details"
+# Variable definitions from NmrStar 3.1
+ID = 'ID'
+Assembly_atom_ID = 'Assembly_atom_ID'
+Entity_assembly_ID = 'Entity_assembly_ID'
+Entity_ID = 'Entity_ID'
+Comp_index_ID = 'Comp_index_ID'
+Seq_ID = 'Seq_ID'
+Comp_ID =  'Comp_ID'
+Atom_ID = 'Atom_ID'
+Atom_type = 'Atom_type'
+Atom_isotope_number = 'Atom_isotope_number'
+Val = 'Val'
+Val_err = 'Val_err'
+Assign_fig_of_merit = 'Assign_fig_of_merit'
+Ambiguity_code = 'Ambiguity_code'
+Occupancy = 'Occupancy'
 Resonance_ID = 'Resonance_ID'
+Auth_entity_assembly_ID = 'Auth_entity_assembly_ID'
+Auth_asym_ID = 'Auth_asym_ID'
 Auth_seq_ID = 'Auth_seq_ID'
+Auth_comp_ID = 'Auth_comp_ID'
+Auth_atom_ID = 'Auth_atom_ID'
+Details = 'Details'
+Entry_ID = 'Entry_ID'
+Assigned_chem_shift_list_ID = 'Assigned_chem_shift_list_ID'
+
 STOP = "stop_"
+_Atom_chem_shift = "_Atom_chem_shift." # used to find the loop of interest in the BMRB and shorten the column header name on the DataFrame
 Atom_chem_shift = _Atom_chem_shift+ID
+
 
 def _getTitles(filePath):
     lines = []
@@ -165,7 +181,7 @@ def _getTitles(filePath):
                 break
     return lines
 
-def _openBmrb(filePath):
+def _getCSListLinesFromBmrbFile(filePath):
     # HardCoded bit to find just the Chemical Shifts, Replace with NEF Style
     lines = []
     with open (filePath, "r") as bmrbFileText:
@@ -179,7 +195,7 @@ def _openBmrb(filePath):
             lines.append(line)
     return lines
 
-def makeDataFrame(bmrbLines):
+def makeCSLDataFrame(bmrbLines):
     ## create dataframe of interest using the same names as appear in the BMRB.
     # NB Columns are not always the same in number (E.G. fileA=10columns, fileB=12columns).
     table = []
@@ -200,31 +216,81 @@ def _isfloat(value):
   except ValueError:
     return False
 
-def makeCSLfromDF(project, df, chemicalShiftListName=None):
-    """
+def _isValidValue(row, header):
+    if row[header] != '.':
+        return True
+    else:
+        return False
 
-    :param df: Pandas dataFrame
-    :param chemicalShiftListName: str, default if None
-    :return: Chemical Shift List Object
+def makeCSLfromDF(project, df):
     """
+    :param df: Pandas dataFrame from parsing an NmrStar 3 File.
+    :return: ChemicalShiftList Object
 
-    chemicalShiftList = project.newChemicalShiftList(name=chemicalShiftListName)
-    nmrChain = project.fetchNmrChain()
-    for index, row in df.iterrows():
-        nmrResidue = nmrChain.fetchNmrResidue(residueType=row[Comp_ID], sequenceCode=row[Auth_seq_ID])
-        nmrAtom = nmrResidue.fetchNmrAtom(row[Atom_ID])
-        if chemicalShiftList:
+    Creates a V3 ChemicalShiftList and associated V3 objects from a dataFrame created parsing an NmrStar 3 File.
+    The reader assumes the dataframe contains tags as specified in the bmrb dictionary. See reference.
+    TAGs reference: https://bmrb.io/dictionary/tag.php?tagcat=Atom_chem_shift
+
+    +------------------------------|-----------------------------------------+
+    + CCPN V3 Object/property      |  BMRB *.TAG (*prefix: _Atom_chem_shift) +
+    +------------------------------|-----------------------------------------+
+    chemicalShiftList:
+                    name           => Entry_ID
+    nmrChain(s):
+                    name           => Entity_assembly_ID
+    NmrResidue(s):
+                    sequenceCode   => seq_ID  or Auth_seq_ID (if present)
+                    residueType    => Comp_ID
+    NmrAtom(s):
+                    name           => Atom_ID
+                    isotopeCode    => Atom_isotope_number + Atom_type
+    ChemicalShift(s):
+                    value          => Val
+                    valueError     => Val_er
+                    comment        => Details
+
+    Others not stored. Maybe save in ChemicalShift internal?
+                                   Assembly_atom_ID
+                                   Assign_fig_of_merit
+                                   Assigned_chem_shift_list_ID
+                                   Ambiguity_code
+                                   Ambiguity_set_ID
+                                   Auth_asym_ID
+                                   Occupancy_ID
+                                   Resonance_ID
+    # TODO: NmrAtom Name => use Auth_atom_ID and Ambiguity_code to create an NEF-compliant nomenclature
+    """
+    ## check if the required columns are present.
+    requiredColumns = [Entry_ID, Entity_assembly_ID, Seq_ID, Auth_seq_ID, Comp_ID, Val, Val_err, Details]
+    if not set(requiredColumns).issubset(df.columns):
+        missingColumns = [c for c in requiredColumns if not c in df.columns]
+        raise ValueError('Creating ChemicalShiftList failed. Missing required Tags (%s).' %', '.join(missingColumns))
+
+    ## check if the Auth_seq_ID is present (not as '.'). Use the author value as preferred option.
+    useAuthSeqID = True if not df[Auth_seq_ID].eq('.').all() else False
+    with notificationEchoBlocking():
+        chemicalShiftListName = df[Entry_ID].astype(str, errors='ignore').unique()[-1] #Mandatory tag. Values always present
+        chemicalShiftList = project.newChemicalShiftList(name=chemicalShiftListName)
+        for index, row in df.iterrows():
+            nmrChain = project.fetchNmrChain(row[Entity_assembly_ID])
+            sequenceCode = row[Auth_seq_ID] if useAuthSeqID else row[Seq_ID]
+            nmrResidue = nmrChain.fetchNmrResidue(residueType=row[Comp_ID], sequenceCode=sequenceCode)
+            nmrAtom = nmrResidue.fetchNmrAtom(row[Atom_ID])
+            isotopeCode = row[Atom_isotope_number] + row[Atom_type]
             try:
-                csValue = row[Val]
-                if _isfloat(csValue):
-                    cs = chemicalShiftList.newChemicalShift(value=float(csValue), nmrAtom=nmrAtom)
+                nmrAtom._setIsotopeCode(isotopeCode)
+            except Exception as icError:
+                getLogger().warn('Error setting the IsotopeCode: %s for the nmrAtom %s. %s.' %(isotopeCode, nmrAtom.pid, icError))
+            try:
+                if _isfloat(row[Val]):
+                    cs = chemicalShiftList.newChemicalShift(value=float(row[Val]), nmrAtom=nmrAtom)
+                    cs.comment = cs.comment+ ' Imported: ' +row[Details]
                     valueErr = row[Val_err]
                     if _isfloat(valueErr):
                         cs.valueError = float(valueErr)
             except Exception as e:
-                getLogger().warn('Error in creating new shift: %s' %e)
+                getLogger().warn('Error creating a new ChemicalShift: %s' %e)
     return chemicalShiftList
-
 
 def _simulatedSpectrumFromCSL(project, csl, axesCodesMap):
     """
@@ -237,57 +303,65 @@ def _simulatedSpectrumFromCSL(project, csl, axesCodesMap):
     :return:
     """
     if csl is None:
-        print("Provide a Chemical Shift List")
+        getLogger().warn("Provide a Chemical Shift List")
         return
     if axesCodesMap is None:
-        print("Select NmrAtom to use and axis codes")
+        getLogger().warn("Select NmrAtom to use and axis codes")
         return
+    with notificationEchoBlocking():
+        nmrAtomNames = list(axesCodesMap.keys())
+        targetSpectrumAxCodes = list(axesCodesMap.values())
+        targetSpectrum = project.createDummySpectrum(axisCodes=targetSpectrumAxCodes, name=None)#, chemicalShiftList=csl)
+        peakList = targetSpectrum.peakLists[-1]
 
-    nmrAtomNames = list(axesCodesMap.keys())
-    targetSpectrumAxCodes = list(axesCodesMap.values())
-    targetSpectrum = project.createDummySpectrum(axisCodes=targetSpectrumAxCodes, name=None)#, chemicalShiftList=csl)
-    peakList = targetSpectrum.peakLists[-1]
+        # filter by NmrAtom of interest
+        nmrResiduesOD = od()
+        for chemicalShift in csl.chemicalShifts:
+            na = chemicalShift.nmrAtom
+            if na.name in nmrAtomNames:
+                try:
+                    nmrResiduesOD[na.nmrResidue].append([na, chemicalShift.value])
+                except KeyError:
+                    nmrResiduesOD[na.nmrResidue] = [(na, chemicalShift.value)]
 
-    # filter by NmrAtom of interest
-    nmrResiduesOD = od()
-    for chemicalShift in csl.chemicalShifts:
-        na = chemicalShift.nmrAtom
-        if na.name in nmrAtomNames:
-            try:
-                nmrResiduesOD[na.nmrResidue].append([na, chemicalShift.value])
-            except KeyError:
-                nmrResiduesOD[na.nmrResidue] = [(na, chemicalShift.value)]
+        incompleteNmrResiduesAssignments = []
+        for nmrResidue in nmrResiduesOD:
+            shifts = []
+            atoms = []
+            for v in nmrResiduesOD[nmrResidue]:
+                atoms.append(v[0])
+                shifts.append(v[1])
 
-    for nmrResidue in nmrResiduesOD:
-        shifts = []
-        atoms = []
-        for v in nmrResiduesOD[nmrResidue]:
-            atoms.append(v[0])
-            shifts.append(v[1])
+            targetSpectrumAxCodes = targetSpectrum.axisCodes
+            axisCodes = [n.name for n in atoms]
+            i = getAxisCodeMatchIndices(axisCodes,targetSpectrumAxCodes) # get the correct order.
+            if len(i) != len(nmrAtomNames): #if not all the NmrAtoms are found for a specific CSL cannot make the peak, incomplete assignment/shifts
+                incompleteNmrResiduesAssignments.append(nmrResidue.pid)
+                continue
 
-        targetSpectrumAxCodes = targetSpectrum.axisCodes
-        axisCodes = [n.name for n in atoms]
-        i = getAxisCodeMatchIndices(axisCodes,targetSpectrumAxCodes) # get the correct order.
-        if len(i) != len(nmrAtomNames): #if not all the NmrAtoms are found for a specific CSL cannot make the peak, incomplete assignment/shifts
-            print('Skipping, not enough information to create and assign peak for NmrResidue: %s' %nmrResidue)
-            continue
-
-        i = np.array(i)
-        shifts = np.array(shifts)
-        atoms = np.array(atoms)
-        try: # trap unknown issues
-            peak = peakList.newPeak(list(shifts[i]))
-            for na, sa in zip(atoms[i],targetSpectrumAxCodes) :
-                peak.assignDimension(sa, [na])
-        except Exception as e:
-            print('Error assigning NmrResidue %s , NmrAtoms: %s, Spectrum Axes: %s, Shifts: %s . Error: %s'
-                  %(nmrResidue,atoms,targetSpectrumAxCodes,shifts, e))
+            i = np.array(i)
+            shifts = np.array(shifts)
+            atoms = np.array(atoms)
+            try: # trap unknown issues
+                peak = peakList.newPeak(list(shifts[i]))
+                for na, sa in zip(atoms[i],targetSpectrumAxCodes) :
+                    peak.assignDimension(sa, [na])
+            except Exception as e:
+                getLogger().warn('Error assigning NmrResidue %s , NmrAtoms: %s, Spectrum Axes: %s, Shifts: %s . Error: %s'
+                      %(nmrResidue,atoms,targetSpectrumAxCodes,shifts, e))
+        if len(incompleteNmrResiduesAssignments)>0:
+            message = 'Could not fully assign peaks for NmrResidue(s): %s. ' \
+                      'Not enough nmrAtom information.' %', '.join(incompleteNmrResiduesAssignments)
+            getLogger().warn(message)
 
 def _importAndCreateV3Objs(project, file, acMap, simulateSpectra=False):
-    lines = _openBmrb(file)
-    df = makeDataFrame(lines)
+    getLogger().info('NmrStar file import started...')
+    lines = _getCSListLinesFromBmrbFile(file)
+    df = makeCSLDataFrame(lines)
     with undoBlock():
         csl = makeCSLfromDF(project, df)
-        if simulateSpectra:
-            _simulatedSpectrumFromCSL(project, csl, axesCodesMap=acMap)
+        if csl is not None:
+            if simulateSpectra:
+                _simulatedSpectrumFromCSL(project, csl, axesCodesMap=acMap)
+    getLogger().info('NmrStar file import completed.')
 

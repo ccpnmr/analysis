32a5ed65a102d061ba7e0041b8a604154b61c7df
VickyAH <vad5@leicester.ac.uk>
2021-01-13 10:27:56 +0000

    Corrections to Tests
    Removes produceNmrAtom and instead tests with fetchNmrAtom and newNmrAtom

--- src/python/ccpn/core/testing/Test_NmrAtom.py
+++ src/python/ccpn/core/testing/Test_NmrAtom.py
@@ -14,7 +14,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: VickyAH $"
-__dateModified__ = "$dateModified: 2021-01-08 11:49:57 +0000 (Fri, January 08, 2021) $"
+__dateModified__ = "$dateModified: 2021-01-13 10:27:56 +0000 (Wed, January 13, 2021) $"
 __version__ = "$Revision: 3.0.1 $"
 #=========================================================================================
 # Created
@@ -32,18 +32,18 @@ class NmrAtomTest(WrapperTesting):
     # Path of project to load (None for new project
     projectPath = 'CcpnCourse2c.ccpn'
 
-    def test_reassign1(self):
+    def test_fetchNmrAtomReassign(self):
         nchain = self.project.getByPid('NC:A')
 
         arg11 = self.project.getByPid('NR:A.11.ARG')
         atomN = arg11.fetchNmrAtom('N')
         self.assertEqual(atomN.pid, 'NA:A.11.ARG.N')
 
-        atomNE = self.project.produceNmrAtom('NA:A.11.ARG.NE')
-        atomNE2 = self.project.produceNmrAtom(chainCode='A', sequenceCode='11', name='NE')
+        atomNE = arg11.fetchNmrAtom('NE')
+        atomNE2 = arg11.fetchNmrAtom('NE')
         self.assertIs(atomNE, atomNE2)
 
-        atomCX = self.project.produceNmrAtom('NA:A.11.ARG.CX')
+        atomCX = arg11.fetchNmrAtom('CX')
         atomNX = arg11.newNmrAtom(name='NX')
         with self.assertRaises(ValueError):
             atomCX.rename('NX')
@@ -66,8 +66,6 @@ class NmrAtomTest(WrapperTesting):
 
         self.project._wrappedData.root.checkAllValid(complete=True)
 
-        with self.assertRaises(ValueError):
-            atomCX = self.project.produceNmrAtom('NA:A.11.VAL.CX')
 
         self.assertEqual(atomCX.pid, 'NA:A.888.ARG.C@198')
         # Undo and redo all operations
@@ -76,18 +74,25 @@ class NmrAtomTest(WrapperTesting):
         self.undo.redo()
         self.assertEqual(atomCX.pid, 'NA:A.888.ARG.C@198')
 
-    def test_produce_reassign(self):
-        at0 = self.project.produceNmrAtom(name='HX')
-        self.assertEqual(at0.id, '@-.@89..HX')
-        at1 = self.project.produceNmrAtom('X.101.VAL.N')
-        self.assertEqual(at1.id, 'X.101.VAL.N')
+    def test_newNmrAtomReassign(self):
+        nc = self.project.newNmrChain(shortName='X')
+        self.assertEqual(nc.pid, 'NC:X')
+
+        nr = nc.newNmrResidue(sequenceCode='101', residueType='VAL')
+        self.assertEqual(nr.pid, 'NR:X.101.VAL')
+
+        at1 = nr.newNmrAtom(name='N')
+        self.assertEqual(at1.pid, 'NA:X.101.VAL.N')
         at1 = at1.assignTo(name='NE')
-        self.assertEqual(at1.id, 'X.101.VAL.NE')
-        at0 = at0.assignTo(sequenceCode=101)
-        self.assertEqual(at0.id, '@-.101..HX')
-        # Undo and redo all operations
+        self.assertEqual(at1.pid, 'NA:X.101.VAL.NE')
+        at1 = at1.assignTo(sequenceCode='777')
+        self.assertEqual(at1.pid, 'NA:X.777.VAL.NE')
         self.undo.undo()
-        self.assertEqual(at0.id, '@-.@89..HX')
+        self.assertEqual(at1.pid, 'NA:X.101.VAL.NE')
+        self.undo.undo()
+        self.assertEqual(at1.pid, 'NA:X.101.VAL.N')
         self.undo.redo()
-        self.assertEqual(at0.id, '@-.101..HX')
-        self.assertEqual(at1.id, 'X.101.VAL.NE')
+        self.assertEqual(at1.pid, 'NA:X.101.VAL.NE')
+        self.undo.redo()
+        self.assertEqual(at1.pid, 'NA:X.777.VAL.NE')
+

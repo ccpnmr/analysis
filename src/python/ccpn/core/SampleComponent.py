97843dff48d535d2285d8bc25f1d70b29119fe12
Luca Mureddu <lgm8@leicester.ac.uk>
2021-01-15 19:00:27 +0000

    Added copyTo method to SampleComponent

--- src/python/ccpn/core/SampleComponent.py
+++ src/python/ccpn/core/SampleComponent.py
@@ -3,7 +3,7 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
 __credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
@@ -12,9 +12,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2020-11-02 17:47:51 +0000 (Mon, November 02, 2020) $"
-__version__ = "$Revision: 3.0.1 $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2021-01-15 19:00:27 +0000 (Fri, January 15, 2021) $"
+__version__ = "$Revision: 3.0.3 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -213,6 +213,18 @@ class SampleComponent(AbstractWrapperObject):
     #=========================================================================================
     # CCPN functions
     #=========================================================================================
+
+    def copyTo(self, targetSample:Sample):
+
+        kwds = {
+            'concentration': self.concentration,
+            'concentrationError': self.concentrationError,
+            'comment': self.comment,
+            'purity': self.purity,
+            'role': self.role,
+            }
+        newSC = _newSampleComponent(targetSample, name=self.name, labelling=self.labelling, **kwds)
+        return newSC
 
     #===========================================================================================
     # new'Object' and other methods

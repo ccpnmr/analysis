396b45208219a993e85f06600fb759d0fbf43cf6
Ed Brooksbank <ejb66@le.ac.uk>
2021-01-21 15:08:38 +0000

    added setLabelText

--- src/python/ccpn/ui/gui/widgets/CompoundWidgets.py
+++ src/python/ccpn/ui/gui/widgets/CompoundWidgets.py
@@ -1,7 +1,7 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
 __credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
@@ -11,8 +11,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2020-11-02 17:47:54 +0000 (Mon, November 02, 2020) $"
-__version__ = "$Revision: 3.0.1 $"
+__dateModified__ = "$dateModified: 2021-01-21 15:08:39 +0000 (Thu, January 21, 2021) $"
+__version__ = "$Revision: 3.0.3 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -557,6 +557,11 @@ class PulldownListCompoundWidget(CompoundBaseWidget):
         """Set the callback for the doubleSpinBox
         """
         self.pulldownList.setCallback(callback)
+
+    def setLabelText(self, value):
+        """Set the text for the label
+        """
+        self.label.setText(value)
 
 
 class CheckBoxCompoundWidget(CompoundBaseWidget):

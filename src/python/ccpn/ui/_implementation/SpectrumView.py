037dfa316ece833340f432fa71cd2bb4fe5ca4e5
Ed Brooksbank <ejb66@le.ac.uk>
2021-02-04 16:52:40 +0000

    minor tweak to dimensionOrdering

--- src/python/ccpn/ui/_implementation/SpectrumView.py
+++ src/python/ccpn/ui/_implementation/SpectrumView.py
@@ -14,7 +14,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-02-03 18:11:37 +0000 (Wed, February 03, 2021) $"
+__dateModified__ = "$dateModified: 2021-02-04 16:52:40 +0000 (Thu, February 04, 2021) $"
 __version__ = "$Revision: 3.0.3 $"
 #=========================================================================================
 # Created
@@ -366,7 +366,7 @@ class SpectrumView(AbstractWrapperObject):
 
         # Convert to zero-origin (for indices) and return
         ll = tuple(dimensionOrdering[axisCodes.index(x)] for x in axisOrder)
-        return tuple(None if not x else x - 1 for x in ll)
+        return tuple((x or None) and x - 1 for x in ll)
 
     #=========================================================================================
     # Implementation functions

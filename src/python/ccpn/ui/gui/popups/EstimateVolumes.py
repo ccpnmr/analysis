1fb339c76b9366e9865760b8d551da6b97b7182c
Ed Brooksbank <ejb66@le.ac.uk>
2021-02-19 16:51:06 +0000

    minor bug fix - now checks current correctly

--- src/python/ccpn/ui/gui/popups/EstimateVolumes.py
+++ src/python/ccpn/ui/gui/popups/EstimateVolumes.py
@@ -4,7 +4,7 @@ Module Documentation here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
 __credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
@@ -14,8 +14,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2020-03-17 01:02:52 +0000 (Tue, March 17, 2020) $"
-__version__ = "$Revision: 3.0.1 $"
+__dateModified__ = "$dateModified: 2021-02-19 16:51:06 +0000 (Fri, February 19, 2021) $"
+__version__ = "$Revision: 3.0.3 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -179,7 +179,7 @@ class EstimateVolumes(CcpnDialogMainWidget):
         from ccpn.core.lib.peakUtils import estimateVolumes
 
         # return if both the lists are empty
-        if not self.current.peaks:
+        if not (self.current and self.current.peaks):
             return
 
         with undoBlockWithoutSideBar():

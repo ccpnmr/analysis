d9e636afdbf813e518a73ffd6367fa229850c549
Luca Mureddu <lgm8@leicester.ac.uk>
2021-03-02 13:58:04 +0000

    comment out not needed line

--- src/python/ccpn/core/Spectrum.py
+++ src/python/ccpn/core/Spectrum.py
@@ -52,7 +52,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2021-02-26 14:59:54 +0000 (Fri, February 26, 2021) $"
+__dateModified__ = "$dateModified: 2021-03-02 13:58:05 +0000 (Tue, March 02, 2021) $"
 __version__ = "$Revision: 3.0.3 $"
 #=========================================================================================
 # Created
@@ -588,7 +588,7 @@ assignmentTolerances
             for refExperiment in nmrExpPrototype.sortedRefExperiments():
                 if value == refExperiment.name:
                     # refExperiment matches name string - set it
-                    self._wrappedData.experiment.refExperiment = refExperiment
+                    # self._wrappedData.experiment.refExperiment = refExperiment
                     # set API RefExperiment and ExpTransfer
                     _setApiRefExperiment(self._wrappedData.experiment, refExperiment)
                     _setApiExpTransfers(self._wrappedData.experiment)

94b022bc29d37d391a99420777a04aec1852fc29
Ed Brooksbank <ejb66@le.ac.uk>
2021-05-07 13:06:49 +0100

    updated restraint to handle empty peaks

--- src/python/ccpn/core/Restraint.py
+++ src/python/ccpn/core/Restraint.py
@@ -14,7 +14,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-05-07 12:57:52 +0100 (Fri, May 07, 2021) $"
+__dateModified__ = "$dateModified: 2021-05-07 13:06:49 +0100 (Fri, May 07, 2021) $"
 __version__ = "$Revision: 3.0.4 $"
 #=========================================================================================
 # Created
@@ -327,12 +327,14 @@ def _newRestraint(self: RestraintList, figureOfMerit: float = None, comment: str
     :param serial: optional serial number.
     :return: a new Restraint instance.
     """
+    dd = {'figureOfMerit': figureOfMerit, 'vectorLength': vectorLength, 'details': comment,}
+
     if peaks:
         getByPid = self._project.getByPid
         peaks = [(getByPid(x) if isinstance(x, str) else x) for x in peaks]
-
-    dd = {'figureOfMerit': figureOfMerit, 'vectorLength': vectorLength, 'details': comment,
-          'peaks': [pk._wrappedData for pk in peaks if pk is not None]}
+        apiPeaks = [pk._wrappedData for pk in peaks if isinstance(pk, Peak)]
+        if apiPeaks:
+            dd['peaks'] = apiPeaks
 
     apiRestraint = self._wrappedData.newGenericConstraint(**dd)
     result = self._project._data2Obj.get(apiRestraint)
@@ -390,7 +392,8 @@ def _createSimpleRestraint(self: RestraintList, comment: str = None, figureOfMer
     if peaks:
         getByPid = self._project.getByPid
         peaks = [(getByPid(x) if isinstance(x, str) else x) for x in peaks]
-        values['peaks'] = tuple(x.pid for x in peaks)
+        # this doesn't appear to be used
+        values['peaks'] = tuple(pk.pid for pk in peaks if isinstance(pk, Peak))
 
     restraint = self.newRestraint(comment=comment, peaks=peaks, figureOfMerit=figureOfMerit,
                                   vectorLength=vectorLength, )

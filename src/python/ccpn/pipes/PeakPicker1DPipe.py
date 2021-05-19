429f08c18fca7a613b23bd92f089e6804aab9a58
Luca Mureddu <lgm8@leicester.ac.uk>
2021-05-19 18:03:47 +0100

    Bug fix for pipes. Added new annotate peaks from pipe

--- src/python/ccpn/pipes/PeakPicker1DPipe.py
+++ src/python/ccpn/pipes/PeakPicker1DPipe.py
@@ -17,24 +17,20 @@ __version__ = "$Revision: 3.0.0 $"
 # Created
 #=========================================================================================
 __author__ = "$Author: Luca Mureddu $"
-__date__ = "$Date: 2017-05-28 10:28:42 +0000 (Sun, May 28, 2017) $"
+__date__ = "$Date: 2017-05-20 10:28:42 +0000 (Sun, May 28, 2017) $"
 #=========================================================================================
 # Start of code
 #=========================================================================================
 
 
 #### GUI IMPORTS
-from ccpn.ui.gui.widgets.CheckBox import CheckBox
 from ccpn.ui.gui.widgets.PipelineWidgets import GuiPipe
-from ccpn.ui.gui.widgets.Spinbox import Spinbox
-from ccpn.ui.gui.widgets.PulldownList import PulldownList
-from ccpn.ui.gui.widgets.Label import Label
-from ccpn.ui.gui.widgets.DoubleSpinbox import DoubleSpinbox
 
 #### NON GUI IMPORTS
 from ccpn.framework.lib.pipeline.PipeBase import SpectraPipe, PIPE_ANALYSIS
-from ccpn.pipes.lib._getNoiseLevel import _getNoiseLevelForPipe
 from ccpn.util.Logging import getLogger
+from tqdm import tqdm
+
 
 ########################################################################################################################
 ###   Attributes:
@@ -42,18 +38,8 @@ from ccpn.util.Logging import getLogger
 ########################################################################################################################
 
 PipeName = 'Peak Picker 1D'
-
 ExcludeRegions = 'Exclude_Regions'
 NoiseThreshold = 'Noise_Threshold'
-NegativePeaks = 'Negative_Peaks'
-NoiseLevelFactor = 'Noise_Level_Factor'
-MaximumFilterSize = 'Maximum_Filter_Size'
-MaximumFilterMode = 'Maximum_Filter_Mode'
-MinimalLineWidth = 'Minimal_LineWidth'
-EstimateNoiseThreshold = 'Estimate_Noise_Threshold'
-Modes = ['wrap', 'reflect', 'constant', 'nearest', 'mirror']
-
-DefaultNoiseThresholdFactor = 10
 DefaultNoiseThreshold = [0.0, 0.0]
 DefaultExcludeRegions = [[0.0, 0.0], [0.0, 0.0]]
 DefaultPeakListIndex = -1
@@ -63,36 +49,25 @@ DefaultPeakListIndex = -1
 ##########################################      ALGORITHM       ########################################################
 ########################################################################################################################
 
+# see in ccpn.core.PeakList.py function peakFinder1D
+# This algorithm uses noise threshold and excluded regions in ppm. Set these using other pipes
 
 ########################################################################################################################
 ##########################################     GUI PIPE    #############################################################
 ########################################################################################################################
 
 
-class PeakPicker1DGuiPipe(GuiPipe):
+class PeakDetector1DGuiPipe(GuiPipe):
     preferredPipe = True
     pipeName = PipeName
 
     def __init__(self, name=pipeName, parent=None, project=None, **kwds):
-        super(PeakPicker1DGuiPipe, self)
+        super(PeakDetector1DGuiPipe, self)
         GuiPipe.__init__(self, parent=parent, name=name, project=project, **kwds)
         self._parent = parent
 
-        row = 0
-        self.pickNegativeLabel = Label(self.pipeFrame, text=NegativePeaks, grid=(row, 0))
-        setattr(self, NegativePeaks, CheckBox(self.pipeFrame, text='', checked=True, grid=(row, 1)))
 
-        row += 1
-        self.noiseLevelFactorLabel = Label(self.pipeFrame, text=NoiseLevelFactor, grid=(row, 0))
-        setattr(self, NoiseLevelFactor, DoubleSpinbox(self.pipeFrame, value=10.0, min=0.01, step=0.1, grid=(row, 1)))
 
-        row += 1
-        self.maximumFilterSize = Label(self.pipeFrame, text=MaximumFilterSize, grid=(row, 0))
-        setattr(self, MaximumFilterSize, Spinbox(self.pipeFrame, value=10, max=1000, grid=(row, 1)))
-        row += 1
-
-        self.maximumFilterMode = Label(self.pipeFrame, text=MaximumFilterMode, grid=(row, 0))
-        setattr(self, MaximumFilterMode, PulldownList(self.pipeFrame, texts=Modes, grid=(row, 1)))
 
 
 ########################################################################################################################
@@ -101,64 +76,44 @@ class PeakPicker1DGuiPipe(GuiPipe):
 
 
 class PeakPicker1DPipe(SpectraPipe):
-    guiPipe = PeakPicker1DGuiPipe
+    guiPipe = PeakDetector1DGuiPipe
     pipeName = PipeName
     pipeCategory = PIPE_ANALYSIS
 
     _kwargs = {
-        ExcludeRegions        : DefaultExcludeRegions,
-        NoiseThreshold        : DefaultNoiseThreshold,
-        EstimateNoiseThreshold: True,
-        NoiseLevelFactor      : DefaultNoiseThresholdFactor,
-        MaximumFilterSize     : 5,
-        MaximumFilterMode     : Modes[0],
-        NegativePeaks         : True,
-
-        }
+               ExcludeRegions  : DefaultExcludeRegions,
+              }
 
     def runPipe(self, spectra):
         '''
         :param data:
         :return:
         '''
-        from tqdm import tqdm
-
         getLogger().info(self._startedInfo)
-        if NoiseThreshold not in self._kwargs:
-            self._kwargs.update({NoiseThreshold: DefaultNoiseThreshold})
-
-        maximumFilterSize = self._kwargs[MaximumFilterSize]
-        maximumFilterMode = self._kwargs[MaximumFilterMode]
-        negativePeaks = self._kwargs[NegativePeaks]
-        positiveNoiseThreshold = max(self._kwargs[NoiseThreshold])
-        negativeNoiseThreshold = min(self._kwargs[NoiseThreshold])
-        noiseLevelFactor = self._kwargs[NoiseLevelFactor]
-
         if ExcludeRegions in self.pipeline._kwargs:
             excludeRegions = self.pipeline._kwargs[ExcludeRegions]
         else:
             self._kwargs.update({ExcludeRegions: DefaultExcludeRegions})
             excludeRegions = self._kwargs[ExcludeRegions]
+        # simple1DPeakPicker([1], [1], 1)# just compile
+        from ccpn.core.lib.ContextManagers import  undoBlockWithoutSideBar, notificationEchoBlocking
+        with undoBlockWithoutSideBar():
+            with notificationEchoBlocking():
+                for spectrum in tqdm(self.inputData):
+                    if len(spectrum.peakLists) > 0:
+                        if NoiseThreshold in self.pipeline._kwargs:
+                            noiseThresholds = self.pipeline._kwargs[NoiseThreshold]
+                            spectrum.noiseLevel = max(noiseThresholds) or None
+                            spectrum.negativeNoiseLevel = min(noiseThresholds) or None
 
-        for spectrum in tqdm(self.inputData):
-            noiseThreshold = _getNoiseLevelForPipe(cls=self, spectrum=spectrum, estimateNoiseThreshold_var=EstimateNoiseThreshold,
-                                                   noiseThreshold_var=NoiseThreshold)
-            if noiseThreshold:
-                negativeNoiseThreshold = noiseThreshold[0]
-                positiveNoiseThreshold = noiseThreshold[1]
-
-            # print('Peak Picker @@@ noiseThreshold', noiseThreshold)
-
-            if len(spectrum.peakLists) > 0:
-                spectrum.peakLists[DefaultPeakListIndex].pickPeaks1dFiltered(size=maximumFilterSize, mode=maximumFilterMode,
-                                                                             positiveNoiseThreshold=positiveNoiseThreshold,
-                                                                             negativeNoiseThreshold=negativeNoiseThreshold,
-                                                                             excludeRegions=excludeRegions,
-                                                                             negativePeaks=negativePeaks,
-                                                                             factor=noiseLevelFactor)
-            else:
-                getLogger().warning('Error: PeakList not found. Add a new PeakList first')
+                        spectrum.peakLists[DefaultPeakListIndex].peakFinder1D(maxNoiseLevel=spectrum.noiseLevel,
+                                                                              minNoiseLevel=spectrum.negativeNoiseLevel,
+                                                                              ignoredRegions=excludeRegions, negativePeaks=False,
+                                                                              )
+                    else:
+                        getLogger().warning('Error: PeakList not found for Spectrum: %s. Add a new PeakList first' % spectrum.pid)
         getLogger().info(self._finishedInfo)
         return spectra
 
-# PeakPicker1DPipe.register() # Registers the pipe in the pipeline
+
+PeakPicker1DPipe.register()  # Registers the pipe in the pipeline

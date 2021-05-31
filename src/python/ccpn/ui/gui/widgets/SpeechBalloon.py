f4e81636144ce0229e55e490096ebfc1d9d4b53d
varioustoxins <garyt.and.sarahb@gmail.com>
2021-05-31 15:51:48 +0100

    remove trailing ,\

--- src/python/ccpn/ui/gui/widgets/SpeechBalloon.py
+++ src/python/ccpn/ui/gui/widgets/SpeechBalloon.py
@@ -7,7 +7,7 @@ from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import QRectF, Qt, QRect, QPoint, pyqtProperty, QTimer
 from PyQt5.QtGui import QPainterPath, QPainter, QPen, QColor, QBrush, QPolygon, QPolygonF, QPixmap, QPalette, QCursor, \
      QFontMetrics
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLayout, QFrame, \
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLayout, QFrame
 
 
 

ce5d7a27d22a67c5b9212ae5499bf4ffca37cede
Ed Brooksbank <ejb66@le.ac.uk>
2021-06-29 13:31:40 +0100

    added missing headers

--- src/python/ccpn/ui/gui/widgets/PythonEditor.py
+++ src/python/ccpn/ui/gui/widgets/PythonEditor.py
@@ -1,3 +1,31 @@
+"""
+Module Documentation here
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
+                 "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
+                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2021-06-29 13:31:41 +0100 (Tue, June 29, 2021) $"
+__version__ = "$Revision: 3.0.4 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: Ed Brooksbank $"
+__date__ = "$Date: 2018-08-30 13:20:58 +0100 (Tue, August 30, 2018) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
 # from https://wiki.python.org/moin/PyQt/Python%20syntax%20highlighting. Example was based on existing work by Carson Farmer and Christophe Kibleur, and an example on the SciPres wiki.
 
 

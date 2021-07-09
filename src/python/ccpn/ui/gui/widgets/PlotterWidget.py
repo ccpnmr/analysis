eb4e4998e05923fbddb805eb441517eaa6c70a94
Luca Mureddu <lgm8@leicester.ac.uk>
2021-07-09 10:18:29 +0100

    progressing with Plotter. Added more plots

--- src/python/ccpn/ui/gui/widgets/PlotterWidget.py
+++ src/python/ccpn/ui/gui/widgets/PlotterWidget.py
@@ -22,28 +22,27 @@ __date__ = "$Date: 2017-05-28 10:28:42 +0000 (Sun, May 28, 2017) $"
 # Start of code
 #=========================================================================================
 
-from contextlib import contextmanager
-from collections import OrderedDict as od
-import matplotlib
 import os
-import ccpn.ui.gui.guiSettings as GS
-from ccpn.ui.gui.widgets.Widget import Widget
+import time
+import matplotlib
+import matplotlib.backends.qt_editor.figureoptions as figureoptions
+import matplotlib.backend_bases as backends
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.backend_tools import ToolBase, ToolToggleBase, ZoomPanBase, cursors, _views_positions
-import matplotlib.backend_bases as backends
-import matplotlib.backends.qt_editor.figureoptions as figureoptions
 from matplotlib.backends.qt_editor.formsubplottool import UiSubplotTool
 from matplotlib.backend_managers import ToolManager
 import matplotlib.pyplot as plt
+from PyQt5 import QtWidgets, QtCore
+from contextlib import contextmanager
+from collections import OrderedDict as od
+import ccpn.ui.gui.guiSettings as GS
+from ccpn.ui.gui.widgets.Widget import Widget
 from ccpn.ui.gui.widgets.Icon import Icon
 from ccpn.ui.gui.widgets.Action import Action
 from ccpn.ui.gui.widgets.ToolBar import ToolBar
-from PyQt5 import QtWidgets, QtCore
-import time
 from ccpn.ui.gui.widgets import MessageDialog as md
-
-
+from ccpn.ui.gui.popups.Dialog import CcpnDialog
 
 # TODO # Set appearance as Main CCPN plots #
 
@@ -307,10 +306,8 @@ class PyPlotWidget(Widget):
     See examples below for usage.
 
     common:
-            'plot',
             'pie',
             'pcolormesh',
-            'hist',
             'streamplot',
             'bar',
             'scatter',
@@ -346,6 +343,17 @@ class PyPlotWidget(Widget):
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
         setPlotStyle(plt)
+
+    @property
+    def currentPlot(self):
+        """
+        get the current AxesSubplot instance
+        """
+        return self.gca()
+
+    @property
+    def currentFigure(self):
+        return self.gcf()
 
     ############### Plotting options ###############
 
@@ -399,6 +407,22 @@ class PyPlotWidget(Widget):
         """
         self.pyplot.plot(*args, **kwargs)
 
+    def plotLineWithErrors(self, x, y, yErr=None, xErr=None, fmt='', errColour=None, eLinewidth=None, capSize=None,
+                           barsAbove=False, lowLims=False, upperLims=False, xLowLims=False, xUpLims=False, errorEvery=1,
+                           capThick=None, *args, **kwargs):
+        """
+        Plot data as line and x/y errors.
+        -------------
+        Parameters:
+
+        For full documentation see:
+            https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.errorbar.html#matplotlib.axes.Axes.errorbar
+        """
+        self.pyplot.errorbar(x, y, yerr=yErr, xerr=xErr, fmt=fmt, ecolor=errColour, elinewidth=eLinewidth,
+                             capsize=capSize, barsabove=barsAbove, lolims=lowLims, uplims=upperLims, xlolims=xLowLims,
+                             xuplims=xUpLims, errorevery=errorEvery, capthick=capThick, *args, **kwargs)
+
+
     def plotHist(self, x, bins=None, range=None, density=False, weights=None,
                 cumulative=False, bottom=None, histtype='bar', align='mid',
                 orientation='vertical', rwidth=None, log=False, colour=None,
@@ -428,6 +452,25 @@ class PyPlotWidget(Widget):
                 orientation=orientation, rwidth=rwidth, log=log, color=colour,
                 label=label, stacked=stacked, normed=normed, data=data,**kwargs)
 
+    def plotBar(self, values, heights, unitLabels=None, errors=None, thickness=0.8, colour=None, orientation='v', **kwargs):
+        """
+        Plot data as horizonatal bars using the native MatplotLib Module.
+        For full documentation see:
+         https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.bar.html#matplotlib.pyplot.bar
+        -------------
+
+        """
+
+        if orientation in ['v', 'vertical', 'V', 'Vertical']:
+            self.pyplot.bar(x=values, height=heights, width=thickness, color=colour, yerr=errors, **kwargs)
+            self.currentPlot.set_xticklabels(unitLabels)
+            self.currentPlot.set_xticks(values)
+
+        if orientation in ['h', 'H', 'Horizontal', 'horizontal']:
+            self.pyplot.barh(y=values, width=heights, height=thickness, color=colour, xerr=errors, **kwargs)
+            self.currentPlot.set_yticklabels(unitLabels)
+            self.currentPlot.set_yticks(values)
+
     def plotContour(self, x, y, heights, levels=None, colours=None, **kwargs):
         """
         Plot contours.
@@ -445,6 +488,48 @@ class PyPlotWidget(Widget):
 
         """
         self.pyplot.contour(x, y, heights, levels=levels, colors=colours, **kwargs)
+
+    def plotScatter(self, x, y, shape=None, colours=None, marker=None, cmap=None, norm=None, vMin=None, vMax=None,
+                    alpha=None, lineWidths=None, edgeColours=None, plotNonFinite=False, **kwargs):
+        """
+        Plot scatter plots.
+        For full documentation see:
+        https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.scatter.html#matplotlib.pyplot.scatter
+
+        -------------
+        Parameters:
+            x, y:   float or array-like, shape (n, ). The data positions.
+            shape:  float or array-like, shape (n, ), optional
+            colours:   array-like or list of colors or color, optional
+
+        """
+        self.pyplot.scatter(x, y, s=shape, c=colours, marker=marker, cmap=cmap, norm=norm, vmin=vMin, vmax=vMax,
+                            alpha=alpha, linewidths=lineWidths, edgecolors=edgeColours, plotnonfinite=plotNonFinite,
+                            **kwargs)
+
+    def plotPie(self, x, explode=None, labels=None, colours=None, autopct=None, pctDistance=0.6, shadow=False,
+                labelDistance=1.1, startAngle=0, radius=1, counterClock=True, wedgeProps=None,
+                textProps=None, centre=(0,0), frame=False, rotateLabels=False):
+        """
+        Plot a pie plot.
+        For full documentation see:
+        https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.pie.html#matplotlib.pyplot.pie
+
+        -------------
+        Parameters:
+            x:   array-like. The wedge sizes.
+            explode:  array-like, optional. A len(x) array which specifies the fraction of the radius with which to offset each wedge.
+            labels:   list, optional. A sequence of strings providing the labels for each wedge
+            colours:   array-like or list of colors or color, optional
+
+        """
+
+        self.pyplot.pie(x, explode=explode, labels=labels, colors=colours, autopct=autopct, pctdistance=pctDistance,
+                            shadow=shadow, labeldistance=labelDistance, startangle=startAngle, radius=radius,
+                            counterclock=counterClock, wedgeprops=wedgeProps, textprops=textProps, center=centre,
+                            frame=frame, rotatelabels=rotateLabels)
+
+
 
     def plotDataFrame(self, dataFrame, kind='line', *args, **kwds):
         """
@@ -583,15 +668,25 @@ class PyPlotWidget(Widget):
                 self.axis('off')
             return img
 
-    def show(self, windowTitle='', size=(700, 700), *args, **kwds):
+    def show(self, windowTitle='CcpNmr Plot', size=(700, 700),
+             xAxisTitle=None, yAxisTitle=None, plotTitle=None, showLegend=True, minimalMargins=False, dialogKwds=None):
         """
         :param args: as CcpnDialog
         :param kwds: CcpnDialog
         :return: opens a CcpnDialog with the plot
         """
-        from ccpn.ui.gui.popups.Dialog import CcpnDialog
+        dialogKwds = dialogKwds or {}
 
-        popup = CcpnDialog(setLayout=True, windowTitle=windowTitle, size=size, *args, **kwds)
+        if showLegend:
+            self.legend()
+        if minimalMargins:
+            self.tight_layout()
+        self.xlabel(xAxisTitle)
+        self.ylabel(yAxisTitle)
+        self.title(plotTitle)
+
+        # wrap all in a ccpn dialog popup
+        popup = CcpnDialog(setLayout=True, windowTitle=windowTitle, size=size, **dialogKwds)
         popup.getLayout().addWidget(self)
         popup.show()
         popup.raise_()
@@ -599,11 +694,13 @@ class PyPlotWidget(Widget):
     def clear(self):
         self._figure.clear()
 
-    def setColourScheme(self):
-        self.plot
+    def setBackGroundColour(self, colour):
+        self.pyplot.gca().set_facecolor(colour)
+
+
 
 @contextmanager
-def plotter(xLabel=None, yLabel=None, plotTitle=None, showLegend=True,
+def plotter(xAxisTitle=None, yAxisTitle=None, plotTitle=None, showLegend=True,
             windowTitle=None, size=(700, 700), *args, **kwargs):
     """Wrap a pyplot and show the window at the end
 
@@ -613,7 +710,7 @@ def plotter(xLabel=None, yLabel=None, plotTitle=None, showLegend=True,
         with plotter() as plot:
             x = [1,2,3,]
             y = [10,20,30]
-            plot.plot(x,y, label='myCurve')
+            plot.plotLine(x,y, label='myCurve')
     ===============
     """
     plot = PyPlotWidget()
@@ -621,15 +718,10 @@ def plotter(xLabel=None, yLabel=None, plotTitle=None, showLegend=True,
     try:
         yield plot
     finally:
-        # plot.tight_layout()
-        if showLegend:
-            plot.legend()
-        plot.xlabel(xLabel)
-        plot.ylabel(yLabel)
-        plot.title(plotTitle)
-        plot.show(size=size, windowTitle=windowTitle, *args, **kwargs)
+        plot.show(size=size, windowTitle=windowTitle,
+                  xAxisTitle=xAxisTitle, yAxisTitle=yAxisTitle, plotTitle=plotTitle,
+                  showLegend=showLegend, *args, **kwargs)
 
-        # plot.deleteLater()
 
 
 
@@ -651,17 +743,16 @@ if __name__ == '__main__':
 
     def _simplePlotExample():
         plt = PyPlotWidget()
-        x = [1,2,3,4,5]
-        y = [10,20,30,14,5]
-        x1 = [1, 2, 3, 4, 5]
-        y1 = [100, 200, 300, 140, 50]
-        m = np.mean([y])
-        m1 = np.mean([y1])
-        ys = y/m
-        y1s = y1/m1
+        x = np.arange(10)
+        y = np.random.rand(len(x))
+        plt.plotLine(x,y,)
+        plt.show(windowTitle='Simple plot', size=(500, 500))
 
-        plt.plot(x,ys, x1,y1s,)
-        plt.ylabel('some numbers')
+    def _plotLineWithErrorsExample():
+        plt = PyPlotWidget()
+        x = np.arange(100)
+        y = np.random.rand(len(x))
+        plt.plotLineWithErrors(x,y, xErr=x*0.01, yErr=y*0.06)
         plt.show(windowTitle='Simple plot', size=(500, 500))
 
     def _pandasPlotExample():
@@ -678,7 +769,6 @@ if __name__ == '__main__':
         path = os.getcwd()
         imgPath = (os.path.join(path, 'AboutCcpNmr.png'))
         plt.displayImage(imgPath)
-        plt.tight_layout()
         plt.show(windowTitle='Image', size=(500, 500))
 
     def _multiPlotExample():
@@ -768,8 +858,58 @@ if __name__ == '__main__':
         plt.plotContour(X, Y, Z, [1,4,7], ['r','g','b'])
         plt.show()
 
-    _contourExample()
+    def _barExample(orientation='v'):
+        import numpy as np
+
+        # Example data
+        people = ('Tom', 'Alex', 'Harry', 'Slim', 'Jim')
+        x = np.arange(len(people))
+        performance = 3 + 10 * np.random.rand(len(people))
+        error = np.random.rand(len(people))
+        #  plot
+        plt = PyPlotWidget()
+        plt.plotBar(x, heights=performance, unitLabels=people, errors=error, orientation=orientation, label='myData1')
+        plt.plotBar(x, heights=performance*0.6, unitLabels=people, errors=error, orientation=orientation, label='myData2')
+
+        plt.show(plotTitle='Example', xAxisTitle='AX', yAxisTitle='ddd', )
+
+    def _scatterExample():
+
+        N = 50
+        x = np.random.rand(N)
+        y = np.random.rand(N)
+        colors = np.random.rand(N)
+        area = (30 * np.random.rand(N)) ** 2  # 0 to 15 point radii
+        plt = PyPlotWidget()
+        plt.plotScatter(x, y, shape=area, colours=colors, alpha=0.7)
+        plt.show(plotTitle='Scatter Example', xAxisTitle='size', yAxisTitle='size Y',)
+
+
+    def _pieExample():
+
+        # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+        labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+        sizes = np.array([15, 30, 45, 10])
+        explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        plt = PyPlotWidget()
+        plt.plotPie(sizes, explode, labels=labels, autopct='%1.1f%%', shadow=True, startAngle=90)
+        plt.show(plotTitle='Pie Example', )
+
+
+
+    # _contourExample()
+    # _pieExample()
+    # _scatterExample()
+    # _barExample('h')
+    # _barExample('v')
+    # _3DExample()
+    # _3DsurfaceExample()
     # _imageExample()
     # _multiPlotExample()
     # _simplePlotExample()
+    _plotLineWithErrorsExample()
+
+
+
     app.start()

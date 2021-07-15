845b8a554143a2c313303c380226a9f540ecb7a1
Geerten Vuister <gv29@leicester.ac.uk>
2021-07-15 15:53:06 +0100

    Cleanup: CPPNNMR_NAMESPACE constant

--- src/python/ccpn/core/_implementation/AbstractWrapperObject.py
+++ src/python/ccpn/core/_implementation/AbstractWrapperObject.py
@@ -432,19 +432,19 @@ class AbstractWrapperObject(NotifierBase):
         pidFields = [cls.shortClassName] + [str(x) for x in args]
         return Pid.new(*pidFields)
 
-    CCPNMR_NAMESPACE = '_ccpNmrV3internal'
+    _CCPNMR_NAMESPACE = '_ccpNmrV3internal'
 
     def _setInternalParameter(self, parameterName: str, value):
         """Sets parameterName for CCPNINTERNAL namespace to value; value must be json seriliasable"""
-        self.setParameter(self.CCPNMR_NAMESPACE, parameterName, value)
+        self.setParameter(self._CCPNMR_NAMESPACE, parameterName, value)
 
     def _getInternalParameter(self, parameterName: str):
         """Gets parameterName for CCPNINTERNAL namespace"""
-        return self.getParameter(self.CCPNMR_NAMESPACE, parameterName)
+        return self.getParameter(self._CCPNMR_NAMESPACE, parameterName)
 
     def _hasInternalParameter(self, parameterName: str):
         """Returns true if parameterName for CCPNINTERNAl namespace exists"""
-        return self.hasParameter(self.CCPNMR_NAMESPACE, parameterName)
+        return self.hasParameter(self._CCPNMR_NAMESPACE, parameterName)
 
     def setParameter(self, namespace: str, parameterName: str, value):
         """Sets parameterName for namespace to value; value must be json serialisable"""

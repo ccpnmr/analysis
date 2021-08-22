c994ca9110b49aaf1b395e56b71f0b53dcc87931
Ed Brooksbank <ejb66@le.ac.uk>
2021-08-22 11:23:27 +0100

    added error note to test case - error from empty dataMatrices: getBFactors, getOccupancies and getCoordinates

--- src/python/ccpn/core/testing/Test_Undo.py
+++ src/python/ccpn/core/testing/Test_Undo.py
@@ -5,7 +5,8 @@
 # Licence, Reference and Credits
 #=========================================================================================
 __copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
-__credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
@@ -13,9 +14,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: VickyAH $"
-__dateModified__ = "$dateModified: 2021-02-03 11:50:33 +0000 (Wed, February 03, 2021) $"
-__version__ = "$Revision: 3.0.3 $"
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2021-08-22 11:23:28 +0100 (Sun, August 22, 2021) $"
+__version__ = "$Revision: 3.0.4 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -94,6 +95,9 @@ class ComplexUndoTest(WrapperTesting):
         apiProject.checkAllValid()
 
     def test_copy_chain_undo_redo(self):
+        # NOTE:ED - this will fail until
+        #   getBFactors, getOccupancies and getCoordinates are removed from api (or return [])
+        #   not required for current structureEnsembles
         apiProject = self.project._wrappedData.root
         apiProject._undo = Undo()
         self.project.newUndoPoint()

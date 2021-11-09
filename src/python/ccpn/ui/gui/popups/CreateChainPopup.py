34b3d9f697824b7db7f5ee33e90094b77562208c
Ed Brooksbank <ejb66@le.ac.uk>
2021-11-09 13:59:40 +0000

    minor spelling

--- src/python/ccpn/ui/gui/popups/CreateChainPopup.py
+++ src/python/ccpn/ui/gui/popups/CreateChainPopup.py
@@ -5,7 +5,8 @@ Module Documentation here
 # Licence, Reference and Credits
 #=========================================================================================
 __copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
-__credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
@@ -13,9 +14,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2021-04-09 17:22:55 +0100 (Fri, April 09, 2021) $"
-__version__ = "$Revision: 3.0.3 $"
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2021-11-09 13:59:40 +0000 (Tue, November 09, 2021) $"
+__version__ = "$Revision: 3.0.4 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -157,7 +158,7 @@ class CreateChainPopup(AttributeEditorPopupABC):
         self.obj.addPseudoAtoms = DefaultAddPseudoAtoms
         self.obj.addNonstereoAtoms = DefaultAddNonstereoAtoms
 
-        # attach the calbacks for the widgets
+        # attach the callbacks for the widgets
         self.moleculeEdit.textChanged.connect(self._queueSetMoleculeName)
         self.commentName.textChanged.connect(self._queueSetComment)
         self.lineEdit1a.valueChanged.connect(self._queueSetSequenceStart)

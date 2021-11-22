79878d65a9c3b8701fd582a8590bca9b8b2a627a
Ed Brooksbank <ejb66@le.ac.uk>
2021-11-22 16:55:30 +0000

    added convenience peak.chemicalShiftList

--- src/python/ccpn/core/Peak.py
+++ src/python/ccpn/core/Peak.py
@@ -14,7 +14,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-11-04 13:25:03 +0000 (Thu, November 04, 2021) $"
+__dateModified__ = "$dateModified: 2021-11-22 16:55:30 +0000 (Mon, November 22, 2021) $"
 __version__ = "$Revision: 3.0.4 $"
 #=========================================================================================
 # Created
@@ -103,6 +103,11 @@ class Peak(AbstractWrapperObject):
     def spectrum(self):
         """Convenience property to get the spectrum, equivalent to peak.peakList.spectrum"""
         return self.peakList.spectrum
+
+    @property
+    def chemicalShiftList(self):
+        """Convenience property to get the spectrum, equivalent to peak.peakList.chemicalShiftList"""
+        return self.peakList.chemicalShiftList
 
     @property
     def height(self) -> Optional[float]:

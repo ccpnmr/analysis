9ada0ce075852b9e59ce55441b8e5ef3b860e733
Luca Mureddu <lgm8@leicester.ac.uk>
2022-01-13 10:42:38 +0000

    fixing the drop of pids from sidebar to tables

--- src/python/ccpn/ui/gui/widgets/GuiTable.py
+++ src/python/ccpn/ui/gui/widgets/GuiTable.py
@@ -15,7 +15,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2022-01-11 19:20:27 +0000 (Tue, January 11, 2022) $"
+__dateModified__ = "$dateModified: 2022-01-13 10:42:38 +0000 (Thu, January 13, 2022) $"
 __version__ = "$Revision: 3.0.4 $"
 #=========================================================================================
 # Created
@@ -447,7 +447,10 @@ class GuiTable(TableWidget, Base):
         from ccpn.ui.gui.widgets.ScrollBarVisibilityWatcher import ScrollBarVisibilityWatcher
 
         # add a dropped notifier to all tables
-        # self.moduleParent.mainWidget._dropEventCallback = self._processDroppedItems
+        if self.moduleParent is not None:
+            # set the dropEvent to the mainWidget of the module, otherwise the event gets stolen by Frames
+            self.moduleParent.mainWidget._dropEventCallback = self._processDroppedItems
+
         self.droppedNotifier = GuiNotifier(self,
                                            [GuiNotifier.DROPEVENT], [DropBase.PIDS],
                                            self._processDroppedItems)

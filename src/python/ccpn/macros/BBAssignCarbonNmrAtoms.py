44ea5eae1c60487e9bdbc066017fb93166beda5a
Ed Brooksbank <ejb66@le.ac.uk>
2022-02-01 17:05:07 +0000

    update macro from .alpha3

--- src/python/ccpn/macros/BBAssignCarbonNmrAtoms.py
+++ src/python/ccpn/macros/BBAssignCarbonNmrAtoms.py
@@ -28,8 +28,8 @@ before you move on to the next residue or go on to the backbone assignment stage
 ##############################     Settings      #################################
 
 assignAxCde = 'C'   # Carbon AxisCode to be assigned, may need changing for HNCO/HNcoCA expts
-assignDim = 2       # Spectrum dimension to be assigned (0,1,2 for 3Ds)
-rootDim = 0         # One of the root dimensions which has already been assigned (0,1,2 for 3Ds)
+assignIsotope = '13C'       # Isotope of dimension to be assigned
+rootIsotope = '1H'         # Isotope of one of the root dimensions which has already been assigned
 casPosCbsNeg = True # True if Ca peaks are positive and Cb peaks are negative in HNCACB,
                     # False if Cas are negative and Cb peaks are positive
 glyHasCaSign = True  # True if Glycine Ca peaks in the HNCACB spectrum have the same sign as the other Ca peaks,
@@ -50,7 +50,7 @@ from statistics import mean
 def assignNmrAtom(seqCode, atomName, offset):
     if offset == -1:
         newsc = ''.join((seqCode, '-1'))
-        newnr = nc.fetchNmrResidue(sequenceCode=newsc, residueType=None)
+        newnr = peakNmrChain.fetchNmrResidue(sequenceCode=newsc, residueType=None)
         newna = newnr.fetchNmrAtom(name=atomName)
         peak.assignDimension(axisCode=assignAxCde, value=newna)
     elif offset == 0:
@@ -123,14 +123,6 @@ if len(current.peaks) == 0:
                                      'assigned root (NH) resonances.')
     sys.exit()
 
-if current.peak.assignmentsByDimensions[rootDim]:
-    nc = current.peak.assignmentsByDimensions[rootDim][0].nmrResidue.nmrChain
-else:
-    showWarning('Missing Root Assignment', 'Please make sure all your peaks have '
-                                           'their root NH NmrAtoms assigned')
-    sys.exit()
-
-
 with undoBlock():
     pkDict = defaultdict(list)
     GlyCheck = False
@@ -143,15 +135,19 @@ with undoBlock():
                     'CB-1': {'shifts': [], 'peaks': []}}
 
     for peak in current.peaks:
-        peakExptType = peak.peakList.spectrum.experimentType
+        assignDim = [ind for ind, value in enumerate(peak.peakList.spectrum.isotopeCodes) if value == assignIsotope][0]
+        assignAxCde = peak.peakList.spectrum.axisCodes[assignDim]
+        rootDim = [ind for ind, value in enumerate(peak.peakList.spectrum.isotopeCodes) if value == rootIsotope][0]
         # Check peak root dim is assigned
         if peak.assignmentsByDimensions[rootDim]:
             peakNmrRes = peak.assignmentsByDimensions[rootDim][0].nmrResidue
+            peakNmrChain = peak.assignmentsByDimensions[rootDim][0].nmrResidue.nmrChain
         else:
             showWarning('Missing Root Assignment', 'Please make sure all your peaks have '
                                                    'their root NH NmrAtoms assigned')
             sys.exit()
         # Put peaks into pkDict
+        peakExptType = peak.peakList.spectrum.experimentType
         if peakExptType is not None:
             if peakExptType not in pkDict:
                 pkDict[peakExptType] = [peak]
@@ -163,7 +159,6 @@ with undoBlock():
                                                    'associated with them (use shortcut ET '
                                                    'to set these)')
             sys.exit()
-
 
     for expt in pkDict:
         if expt in iSpectra:

5540b790edd91ee6077dd41b4bc53478914f31c7
Ed Brooksbank <ejb66@le.ac.uk>
2022-02-08 12:55:32 +0000

    wrong import

--- src/python/ccpn/core/lib/peakUtils.py
+++ src/python/ccpn/core/lib/peakUtils.py
@@ -1,19 +1,19 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2022"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2022-01-20 15:21:06 +0000 (Thu, January 20, 2022) $"
-__version__ = "$Revision: 3.0.4 $"
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2022-02-08 12:55:32 +0000 (Tue, February 08, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -692,7 +692,7 @@ def peakParabolicInterpolation(peak: 'Peak', update=False):
     import numpy
     from ccpn.core.Peak import Peak
     from ccpn.core.lib.Pid import Pid
-    from ccpn.framework.Framework import getApplication
+    from ccpn.framework.Application import getApplication
     from ccpn.util.Parabole import Parabole
 
     if isinstance(peak, Peak):

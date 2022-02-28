7b164ac64be7d5bc76105d17662881328388b985
Ed Brooksbank <ejb66@le.ac.uk>
2022-02-28 18:56:19 +0000

    minor formatting

--- src/python/ccpn/ui/gui/lib/_SimplePandasTable.py
+++ src/python/ccpn/ui/gui/lib/_SimplePandasTable.py
@@ -15,7 +15,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-02-28 18:28:42 +0000 (Mon, February 28, 2022) $"
+__dateModified__ = "$dateModified: 2022-02-28 18:56:19 +0000 (Mon, February 28, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -233,6 +233,8 @@ class _SimplePandasTableModel(QtCore.QAbstractTableModel):
         """
         if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
             return self._data.columns[col] if not self._data.empty else None
+        if orientation == QtCore.Qt.Vertical and role == QtCore.Qt.DisplayRole:
+            return self._data.index[col] if not self._data.empty else None
         return None
 
     def setForeground(self, row, column, colour):

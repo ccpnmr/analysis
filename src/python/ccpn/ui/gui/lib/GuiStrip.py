510816d92e69d3c297cfbf1bca03607f883cc6e4
Ed Brooksbank <ejb66@le.ac.uk>
2022-02-28 13:45:52 +0000

    now uses correct flag for peak-picking

--- src/python/ccpn/ui/gui/lib/GuiStrip.py
+++ src/python/ccpn/ui/gui/lib/GuiStrip.py
@@ -4,10 +4,10 @@ Module Documentation here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2022"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
@@ -15,8 +15,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-01-13 17:30:50 +0000 (Thu, January 13, 2022) $"
-__version__ = "$Revision: 3.0.4 $"
+__dateModified__ = "$dateModified: 2022-02-28 13:45:53 +0000 (Mon, February 28, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -2500,8 +2500,8 @@ class GuiStrip(Frame):
 
                 # get parameters to apply to peak picker
                 _sliceTuples = _displayedSpectrum.getSliceTuples(regions)
-                positiveThreshold = spectrum.positiveContourBase if spectrumView.displayPositiveContours else None
-                negativeThreshold = spectrum.negativeContourBase if spectrumView.displayNegativeContours else None
+                positiveThreshold = spectrum.positiveContourBase if spectrum.includePositiveContours else None
+                negativeThreshold = spectrum.negativeContourBase if spectrum.includeNegativeContours else None
 
                 for thisPeakListView in validPeakListViews:
                     peakList = thisPeakListView.peakList

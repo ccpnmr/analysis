7256e4ac62ca4bbe663958529b4d774335bdc4c3
Ed Brooksbank <ejb66@le.ac.uk>
2022-03-01 18:20:26 +0000

    ChemicalShiftTable.py now only shows peaks associated wth that table

--- src/python/ccpn/ui/gui/modules/ChemicalShiftTable.py
+++ src/python/ccpn/ui/gui/modules/ChemicalShiftTable.py
@@ -17,7 +17,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-02-23 13:03:56 +0000 (Wed, February 23, 2022) $"
+__dateModified__ = "$dateModified: 2022-03-01 18:20:26 +0000 (Tue, March 01, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -741,7 +741,7 @@ class ChemicalShiftTable(GuiTable):
         """Get a tuple of derived values from obj
         Not very generic yet - column class now seems redundant
         """
-        _peaks = obj.allAssignedPeaks or []
+        _peaks = obj.assignedPeaks or []
         allPeaks = str([pp.pid for pp in _peaks])
         try:
             shiftPeakCount = len([pp for pp in _peaks if pp.spectrum.chemicalShiftList == obj.chemicalShiftList])

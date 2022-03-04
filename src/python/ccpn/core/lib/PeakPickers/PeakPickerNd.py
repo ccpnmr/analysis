17eb61171fe50a3dfd9e02d4c6bf2f27941a4ab2
Ed Brooksbank <ejb66@le.ac.uk>
2022-03-04 15:43:14 +0000

    added error-trap to individual peak picking - messages are concatenated

--- src/python/ccpn/core/lib/PeakPickers/PeakPickerNd.py
+++ src/python/ccpn/core/lib/PeakPickers/PeakPickerNd.py
@@ -4,10 +4,10 @@ Module Documentation here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2022"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
@@ -15,8 +15,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-01-21 11:22:07 +0000 (Fri, January 21, 2022) $"
-__version__ = "$Revision: 3.0.4 $"
+__dateModified__ = "$dateModified: 2022-03-04 15:43:14 +0000 (Fri, March 04, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -112,13 +112,22 @@ class PeakPickerNd(PeakPickerABC):
                     # currently gaussian or lorentzian
                     if singularMode is True:
 
+                        _errorMsgs = []
                         # fit peaks individually in small regions
                         for peakArray, localRegionArray in zip(allPeaksArray, allRegionArrays):
                             peakArray = peakArray.reshape((1, self.dimensionCount))
                             peakArray = peakArray.astype(np.float32)
-                            localResult = CPeak.fitPeaks(data, localRegionArray, peakArray, method)
 
-                            result += tuple(localResult)
+                            try:
+                                localResult = CPeak.fitPeaks(data, localRegionArray, peakArray, method)
+                                result += tuple(localResult)
+                            except Exception as es:
+                                # catch all errors as a single report
+                                _errorMsgs.append(f'failed to fit peak: {peakArray}\n{es}')
+
+                        if _errorMsgs:
+                            getLogger().warning('\n'.join(_errorMsgs))
+
                     else:
 
                         # fit all peaks in one operation

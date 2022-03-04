a3beba7fc1164313692c3a979939c7520345d3a1
Ed Brooksbank <ejb66@le.ac.uk>
2022-03-04 00:17:35 +0000

    strange bug in Splitter stylesheet

--- src/python/ccpn/util/Url.py
+++ src/python/ccpn/util/Url.py
@@ -3,10 +3,10 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
@@ -14,8 +14,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-12-01 16:52:03 +0000 (Wed, December 01, 2021) $"
-__version__ = "$Revision: 3.0.4 $"
+__dateModified__ = "$dateModified: 2022-03-04 00:17:35 +0000 (Fri, March 04, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -125,14 +125,16 @@ def fetchHttpResponse(method, url, data=None, headers=None, proxySettings=None):
     return response
 
 
-def fetchUrl(url, data=None, headers=None, timeout=2.0, proxySettings=None, decodeResponse=True):
+def fetchUrl(url, data=None, headers=None, timeout=5, proxySettings=None, decodeResponse=True):
     """Fetch url request from the server
     """
     import logging
     from ccpn.core.lib.ContextManagers import Timeout as timer
+    from ccpn.util.Common import isWindowsOS
 
     timeoutMessage = 'Could not connect to server. Check connection'
-    with timer(seconds=timeout or 2, timeoutMessage=timeoutMessage):
+    if isWindowsOS():
+        # Windows does not have signal.SIGALRM - temporary fix
         urllib3_logger = logging.getLogger('urllib3')
         urllib3_logger.setLevel(logging.CRITICAL)
 
@@ -151,6 +153,28 @@ def fetchUrl(url, data=None, headers=None, timeout=2.0, proxySettings=None, deco
 
         response = fetchHttpResponse('POST', url, data=data, headers=headers,
                                      proxySettings=proxySettings)
+
+    else:
+        # use the timeout limited call
+        with timer(seconds=timeout or 5, timeoutMessage=timeoutMessage):
+            urllib3_logger = logging.getLogger('urllib3')
+            urllib3_logger.setLevel(logging.CRITICAL)
+
+            if not proxySettings:
+
+                # read the proxySettings from the preferences
+                from ccpn.util.UserPreferences import UserPreferences
+
+                _userPreferences = UserPreferences(readPreferences=True)
+                if _userPreferences.proxyDefined:
+                    proxyNames = ['useProxy', 'proxyAddress', 'proxyPort', 'useProxyPassword',
+                                  'proxyUsername', 'proxyPassword', 'verifySSL']
+                    proxySettings = {}
+                    for name in proxyNames:
+                        proxySettings[name] = _userPreferences._getPreferencesParameter(name)
+
+            response = fetchHttpResponse('POST', url, data=data, headers=headers,
+                                         proxySettings=proxySettings)
 
     return response.data.decode('utf-8') if decodeResponse else response
 

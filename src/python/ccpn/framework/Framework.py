5c2562d92c72b9ad740e5501724933a9713967fa
Ed Brooksbank <ejb66@le.ac.uk>
2022-03-21 17:49:18 +0000

    set increased maximum number of files open

--- src/python/ccpn/framework/Framework.py
+++ src/python/ccpn/framework/Framework.py
@@ -12,7 +12,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-03-21 17:47:40 +0000 (Mon, March 21, 2022) $"
+__dateModified__ = "$dateModified: 2022-03-21 17:49:19 +0000 (Mon, March 21, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -58,7 +58,7 @@ try:
         resource.setrlimit(resource.RLIMIT_NOFILE, (2048, hard))
 
 except Exception:
-    sys.stderr.write(f'Error setting maximum number of files the can be open')
+    sys.stderr.write(f'Error setting maximum number of files that can be open')
 
 faulthandler.enable()
 

466039ad5ed66747dbe506dee3410f8d53a53c5d
Ed Brooksbank <ejb66@le.ac.uk>
2022-03-22 11:25:37 +0000

    removed unnecessary mainWindow

--- src/python/ccpn/ui/gui/modules/DataTableModule.py
+++ src/python/ccpn/ui/gui/modules/DataTableModule.py
@@ -15,7 +15,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-03-11 15:52:47 +0000 (Fri, March 11, 2022) $"
+__dateModified__ = "$dateModified: 2022-03-22 11:25:37 +0000 (Tue, March 22, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -270,7 +270,6 @@ class _tableWidget(_SimplePandasTableView):
 
         # initialise the table
         super().__init__(parent=parent,
-                         mainWindow=self.mainWindow,
                          showHorizontalHeader=True,
                          showVerticalHeader=False,
                          grid=(3, 0), gridSpan=(1, 6))

760f90e0a3176533059e2336091fe3c743fe5fb0
Luca Mureddu <luca.mureddu@me.com>
2022-04-04 18:41:17 +0100

    MacroEditor: changed background colour for selected words

--- src/python/ccpn/ui/gui/modules/macroEditorUtil/QPythonEditor.py
+++ src/python/ccpn/ui/gui/modules/macroEditorUtil/QPythonEditor.py
@@ -1,18 +1,19 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
-__credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-02-04 12:07:38 +0000 (Thu, February 04, 2021) $"
-__version__ = "$Revision: 3.0.3 $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2022-04-04 18:41:17 +0100 (Mon, April 04, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -22,8 +23,7 @@ __date__ = "$Date: 2020-05-15 09:30:25 +0000 (Fri, May 15, 2020) $"
 # Start of code
 #=========================================================================================
 
-
-from PyQt5 import QtGui
+from PyQt5 import QtGui, QtWidgets
 from PyQt5 import QtPrintSupport
 from ccpn.ui.gui.widgets.Base import Base
 from ccpn.ui.gui.widgets.FileDialog import MacrosFileDialog
@@ -42,7 +42,7 @@ from pyqode.core.cache import Cache
 
 marginColour = QtGui.QColor('lightgrey')
 marginPosition = 100
-import sys
+SEL_BACKGROUND_COLOUR = (255, 219, 179)
 #########################################################################################
 ################################  Editor Widget  ########################################
 #########################################################################################
@@ -77,6 +77,17 @@ class PyCodeEditor(PyCodeEdit, Base):
         self.modes.remove(CalltipsMode)
         self.modes.append(CcpnCalltipsMode())
         # self.docPanel.setVisible(True)
+
+    def _init_style(self):
+        """ Inits style options """
+        self._background = QtGui.QColor('white')
+        self._foreground = QtGui.QColor('black')
+        self._whitespaces_foreground = QtGui.QColor('light gray')
+        app = QtWidgets.QApplication.instance()
+        self._sel_background = QtGui.QColor(*SEL_BACKGROUND_COLOUR)
+        self._sel_foreground = app.palette().highlightedText().color()
+        self._font_size = 10
+        self.font_name = ""
 
     def _requestCompletion(self):
         """

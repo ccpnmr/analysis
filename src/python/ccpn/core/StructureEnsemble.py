5f5fbb655b29d87af1b5f7d9f4eb44a1981cbc1c
Ed Brooksbank <ejb66@le.ac.uk>
2022-05-06 11:58:09 +0100

    spelling

--- src/python/ccpn/core/StructureEnsemble.py
+++ src/python/ccpn/core/StructureEnsemble.py
@@ -14,7 +14,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-05-05 10:40:29 +0100 (Thu, May 05, 2022) $"
+__dateModified__ = "$dateModified: 2022-05-06 11:58:09 +0100 (Fri, May 06, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -97,7 +97,7 @@ class StructureEnsemble(AbstractWrapperObject):
         """EnsembleData (Pandas DataFrame) with structure data.
 
         Note that modifying the data via setValues, 'data[column] = ' or 'data.column = '
-        will be echoed and put on the undo stack.
+        will be echoed and put on the undo-stack.
         Changing the data by direct pandas access will not."""
         apiObj = self._wrappedData.findFirstParameter(name='data')
         if apiObj is None:

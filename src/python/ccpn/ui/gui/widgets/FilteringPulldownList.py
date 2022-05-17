45cda9ebb6f9be0764933fee3cfb7adaee5cf86c
Luca Mureddu <luca.mureddu@me.com>
2022-05-17 18:57:39 +0100

    HitAnalysis: Added predefined comments. HitAnalysis: Simplified layout. Filtering pulldown: added background text.

--- src/python/ccpn/ui/gui/widgets/FilteringPulldownList.py
+++ src/python/ccpn/ui/gui/widgets/FilteringPulldownList.py
@@ -4,19 +4,19 @@ Module Documentation here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-06-29 13:31:41 +0100 (Tue, June 29, 2021) $"
-__version__ = "$Revision: 3.0.4 $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2022-05-17 18:57:39 +0100 (Tue, May 17, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -32,7 +32,7 @@ from ccpn.ui.gui.widgets.PulldownList import PulldownList
 
 
 class FilteringPulldownList(PulldownList):
-  def __init__(self, parent=None, **kwds):
+  def __init__(self, parent=None, backgroundText='', **kwds):
       super().__init__(parent, **kwds)
 
       self.setEditable(True)
@@ -51,6 +51,8 @@ class FilteringPulldownList(PulldownList):
       self.setCompleter(self._completer)
       self.completer().popup().setObjectName("CompleterList")
       self.lineEdit().textEdited.connect(self._proxy.setFilterFixedString)
+      if backgroundText:
+          self.lineEdit().setPlaceholderText(str(backgroundText))
 
   def onCompleterActivated(self, text):
       if not text: return

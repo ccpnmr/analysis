714778c9cec973c3800679c90eb1b07856711b89
Luca Mureddu <luca.mureddu@me.com>
2022-05-19 16:01:23 +0100

    HitAnalysis: minor layout chang on toolbar

--- src/python/ccpn/ui/gui/widgets/Label.py
+++ src/python/ccpn/ui/gui/widgets/Label.py
@@ -4,18 +4,19 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
-__credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-02-04 12:07:37 +0000 (Thu, February 04, 2021) $"
-__version__ = "$Revision: 3.0.3 $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2022-05-19 16:01:23 +0100 (Thu, May 19, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -93,6 +94,17 @@ class Label(QtWidgets.QLabel, Base):
             self._margins[3],
             )
                            )
+
+
+class DividerLabel(Label):
+
+    def __init__(self, parent=None, text='', icon=None, iconSize=(25, 25),  margins=[10, 1, 10, 1], hAlign='c', **kwds):
+        super().__init__(parent=parent, text=text, icon=icon, iconSize=iconSize, margins=margins, hAlign=hAlign, **kwds)
+
+        self.icon = icon
+        if not self.icon:
+            icon = Icon('icons/divider')
+            self.setPixmap(icon.pixmap(*iconSize))
 
 
 class ActiveLabel(Label):

8e538f45e6152ae99fd6ddac8506f10ad353bb54
VickyAH <vad5@leicester.ac.uk>
2022-06-28 14:12:17 +0100

    new CCPN macros

--- src/python/ccpn/macros/deleteOrphanedChemicalShifts.py
+++ src/python/ccpn/macros/deleteOrphanedChemicalShifts.py
@@ -11,16 +11,30 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
 
 #=========================================================================================
-# Macro Created by:
+# Macro Created by: Vicky Higman
 #=========================================================================================
 __author__ = "$Author: Ccpn $"
 __date__ = "$Date: 2021-05-20 12:34:46 +0100 (Thu, May 20, 2021) $"
 __version__ = "$Revision: 3.1.0 $"
-__Title__ = "The title"
-__Category__ = "Any,"
-__tags__ = ("Many", )
-__Description__ = """ A Short Description """
+__Title__ = "delete Orphaned Chemical Shifts"
+__Category__ = "Assignment"
+__tags__ = ("chemicalShifts", "orphans", )
+__Description__ = """ This macro will delete all the orphaned chemical shifts in your project """
 
 #=========================================================================================
 # Start of code
 #=========================================================================================
+
+for csl in project.chemicalShiftLists:
+    for cs in list(csl.chemicalShifts):
+        if cs.orphan:
+            csl.deleteChemicalShift(uniqueId=cs.uniqueId)
+
+# If you wanted a macro to remove the orphans in a specific ChemicalShiftList,
+# use this code instead, making sure to enter the correct ChemicalShiftList PID in the
+# first line. 
+# 
+# csl = get('CL:default')
+# for cs in list(csl.chemicalShifts):
+#     if cs.orphan:
+#         csl.deleteChemicalShift(uniqueId=cs.uniqueId)

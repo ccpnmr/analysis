42527de800f701ff11193bcbe8d8d703a723023e
Ed Brooksbank <ejb66@le.ac.uk>
2022-06-29 15:14:10 +0100

    chain/nmrChain popups check assignment and update as required

--- src/python/ccpn/ui/gui/popups/NmrChainPopup.py
+++ src/python/ccpn/ui/gui/popups/NmrChainPopup.py
@@ -4,9 +4,10 @@ Module Documentation here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2020"
-__credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
                  "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
@@ -14,8 +15,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2020-11-02 17:47:53 +0000 (Mon, November 02, 2020) $"
-__version__ = "$Revision: 3.0.1 $"
+__dateModified__ = "$dateModified: 2022-06-29 15:14:11 +0100 (Wed, June 29, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -27,6 +28,7 @@ __date__ = "$Date: 2017-03-30 11:28:58 +0100 (Thu, March 30, 2017) $"
 
 from ccpn.core.NmrChain import NmrChain
 from ccpn.ui.gui.popups.SimpleAttributeEditorPopupABC import SimpleAttributeEditorPopupABC
+from ccpn.ui.gui.widgets.MessageDialog import showMulti
 
 
 class NmrChainPopup(SimpleAttributeEditorPopupABC):
@@ -39,3 +41,33 @@ class NmrChainPopup(SimpleAttributeEditorPopupABC):
                   ('Is Connected', getattr, None, {}),
                   ('Chain', getattr, None, {}),
                   ]
+
+    _CANCEL = 'Cancel'
+    _OK = 'Ok'
+    _DONT_SAVE = "Don't Change"
+
+    def _applyAllChanges(self, changes):
+        """Apply all changes - update atom name
+        """
+        name = self.edits['name'].get()
+        if self.obj.chain is not None and name != self.obj.name:
+
+            reply = showMulti('Edit NmrChain',
+                              'You are changing the name of your nmrChain.\n'
+                              'Do you want to change the name of the associated Chain as well?',
+                              texts=[self._OK, self._CANCEL, self._DONT_SAVE],
+                              okText=self._OK, cancelText=self._CANCEL,
+                              parent=self)
+
+            if reply == self._CANCEL:
+                return
+
+            elif reply == self._OK:
+                # also rename the chain
+                chain = self.obj.chain
+
+                super()._applyAllChanges(changes)
+                chain.name = name
+                return
+
+        super()._applyAllChanges(changes)

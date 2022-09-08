41987272517527f522feaf195109be56a81baaa7
Ed Brooksbank <ejb66@le.ac.uk>
2022-09-08 11:37:39 +0100

    mirrors built-in

--- src/python/ccpn/ui/gui/lib/OpenGL/CcpnOpenGL.py
+++ src/python/ccpn/ui/gui/lib/OpenGL/CcpnOpenGL.py
@@ -51,12 +51,12 @@ __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliz
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-08-11 16:03:57 +0100 (Thu, August 11, 2022) $"
+__dateModified__ = "$dateModified: 2022-09-08 11:37:39 +0100 (Thu, September 08, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -1745,8 +1745,8 @@ class CcpnGLWidget(QOpenGLWidget):
                                            GLContext=self)
 
         # get the current buffering mode and set the required length to the number of buffers
-        format = self.format()
-        self._numBuffers = int(format.swapBehavior()) or 2
+        fmt = self.format()
+        self._numBuffers = int(fmt.swapBehavior()) or 2
         self._glCursorQueue = ()
         for buf in range(self._numBuffers):
             self._glCursorQueue += (GLVertexArray(numLists=1,

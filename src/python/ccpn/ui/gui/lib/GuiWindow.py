d9468357b348daf74e71bcf1ca6ad056fdc63750
Ed Brooksbank <ejb66@le.ac.uk>
2022-09-08 10:14:13 +0100

    missing parameter for tuple :|

--- src/python/ccpn/ui/gui/lib/GuiWindow.py
+++ src/python/ccpn/ui/gui/lib/GuiWindow.py
@@ -15,7 +15,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-09-01 19:12:18 +0100 (Thu, September 01, 2022) $"
+__dateModified__ = "$dateModified: 2022-09-08 10:14:13 +0100 (Thu, September 08, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -247,7 +247,7 @@ class GuiWindow():
 
             else:
                 # don't show the popup
-                self.project.deleteObjects(*(obj for _name, itms in deleteItems for obj in itms))
+                self.project.deleteObjects(*(obj for _name, itms, _check in deleteItems for obj in itms))
 
     # def deleteSelectedPeaks(self, parent=None):
     #

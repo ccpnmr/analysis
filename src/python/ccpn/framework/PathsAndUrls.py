30e0e6560ccf3591966b4325173145c308d6855e
VickyAH <vad5@leicester.ac.uk>
2022-09-16 14:14:02 +0100

    add new ssNMR tutorials to menu

--- src/python/ccpn/framework/PathsAndUrls.py
+++ src/python/ccpn/framework/PathsAndUrls.py
@@ -9,12 +9,12 @@ __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliz
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-06-29 15:20:31 +0100 (Wed, June 29, 2022) $"
+__modifiedBy__ = "$modifiedBy: VickyAH $"
+__dateModified__ = "$dateModified: 2022-09-16 14:14:02 +0100 (Fri, September 16, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -77,8 +77,8 @@ backboneAssignmentTutorialPath  = ccpnCodePath / 'tutorials' / 'BackboneAssignme
 screenTutorialPath              = ccpnCodePath / 'tutorials' / 'CcpNmr_AnalysisScreen_Tutorial_Beta2Release.pdf'
 cspTutorialPath                 = ccpnCodePath / 'tutorials' / 'CSPTutorial.pdf'
 solidStatePeptideTutorialPath   = ccpnCodePath / 'tutorials' / 'SolidStatePeptideAssignmentTutorial.pdf'
-# solidStateHETsTutorialPath       = ccpnCodePath / 'tutorials' / 'SolidStateHETsAssignmentTutorial.pdf'
-# solidStateSH3TutorialPath       = ccpnCodePath / 'tutorials' / 'SolidStateSH3AssignmentTutorial.pdf'
+solidStateHETsTutorialPath       = ccpnCodePath / 'tutorials' / 'SolidStateHETsAssignmentTutorial.pdf'
+solidStateSH3TutorialPath       = ccpnCodePath / 'tutorials' / 'SolidStateSH3AssignmentTutorial.pdf'
 macroWritingTutorialPath       = ccpnCodePath / 'tutorials' / 'MacroWritingTutorial.pdf'
 screeningTutorialPath      = ccpnCodePath / 'tutorials' / 'ScreeningTutorial.pdf'
 howTosPath                      = tutorialsPath / 'How-Tos'

93f750415247dc3b2200a94d9628b14e65a3852b
Ed Brooksbank <ejb66@le.ac.uk>
2022-09-20 10:10:24 +0100

    minor option

--- src/python/ccpn/ui/gui/popups/ProjectSummaryPopup.py
+++ src/python/ccpn/ui/gui/popups/ProjectSummaryPopup.py
@@ -15,7 +15,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-09-15 17:24:18 +0100 (Thu, September 15, 2022) $"
+__dateModified__ = "$dateModified: 2022-09-20 10:10:24 +0100 (Tue, September 20, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -190,7 +190,7 @@ class ProjectSummaryPopup(CcpnDialog):
     def _setTableOptions(table):
         """Set options for a table
         """
-        table.defaultEditable = False
+        table.setEditable(False)
         table.setDeleteEnabled(False)
         table.setSearchEnabled(False)
         table._enableActionCallback = False

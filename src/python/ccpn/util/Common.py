9dd27cdd7eafbc14aac8db9e48e950427df4b94e
Ed Brooksbank <ejb66@le.ac.uk>
2022-09-20 09:20:27 +0100

    added sentinel

--- src/python/ccpn/util/Common.py
+++ src/python/ccpn/util/Common.py
@@ -17,12 +17,12 @@ __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliz
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-08-07 15:04:06 +0100 (Sun, August 07, 2022) $"
+__dateModified__ = "$dateModified: 2022-09-20 09:20:27 +0100 (Tue, September 20, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -50,6 +50,8 @@ from ccpn.util import Constants
 
 # from ccpn.util.isotopes import isotopeRecords
 
+# define a simple sentinel
+NOTHING = object()
 
 # Max value used for random integer. Set to be expressible as a signed 32-bit integer.
 maxRandomInt = 2000000000

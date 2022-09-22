14cf3d0f97b26be3dc3a3b6ee00157bf1d7db213
Ed Brooksbank <ejb66@le.ac.uk>
2022-09-22 18:43:10 +0100

    cleaned-up indent

--- bin/paths.sh
+++ bin/paths.sh
@@ -9,10 +9,10 @@ export QT_PLUGIN_PATH=${QT_PLUGIN_PATH}:"${CONDA}"/plugins
 export QT_LOGGING_RULES="*=false;qt.qpa.*=false"
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
-      export DYLD_FALLBACK_LIBRARY_PATH=/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/ImageIO.framework/Versions/A/Resources:
-      export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}"${CONDA}"/lib:
-      export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}"${CONDA}"/lib/python3.9/site-packages/PyQt5:
-      export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}${HOME}/lib:/usr/local/lib:/usr/lib
+    export DYLD_FALLBACK_LIBRARY_PATH=/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/ImageIO.framework/Versions/A/Resources:
+    export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}"${CONDA}"/lib:
+    export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}"${CONDA}"/lib/python3.9/site-packages/PyQt5:
+    export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}${HOME}/lib:/usr/local/lib:/usr/lib
 elif [[ "$(uname -s)" == "Linux" ]]; then
     export PYOPENGL_PLATFORM=x11
     export QT_QPA_PLATFORM=xcb

6ae8f85b1b3e5dae1fee99cbc6f4770e1e72750a
Ed Brooksbank <ejb66@le.ac.uk>
2022-09-23 10:39:56 +0100

    minor update to scripts removed unneeded fix for Ubuntu22.02 - works with new conda env

--- bin/paths.sh
+++ bin/paths.sh
@@ -13,7 +13,4 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}"${CONDA}"/lib:
     export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}"${CONDA}"/lib/python3.9/site-packages/PyQt5:
     export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}${HOME}/lib:/usr/local/lib:/usr/lib
-elif [[ "$(uname -s)" == "Linux" ]]; then
-    export PYOPENGL_PLATFORM=x11
-    export QT_QPA_PLATFORM=xcb
 fi

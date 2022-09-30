f2da2d4947dc9c496f9840c8e38308ee3b3677fd
Ed Brooksbank <ejb66@le.ac.uk>
2022-09-30 15:19:12 +0100

    removed restriction on sequence-graph assignment

--- src/python/ccpn/core/NmrChain.py
+++ src/python/ccpn/core/NmrChain.py
@@ -14,7 +14,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-09-21 15:03:25 +0100 (Wed, September 21, 2022) $"
+__dateModified__ = "$dateModified: 2022-09-30 15:19:12 +0100 (Fri, September 30, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -214,8 +214,8 @@ class NmrChain(AbstractWrapperObject):
         if not apiNmrChain:
             return
 
-        if not self.isConnected:
-            raise ValueError("assignConnectedResidues only allowed for connected NmrChains")
+        # if not self.isConnected:  # unnecessary test?
+        #     raise ValueError("assignConnectedResidues only allowed for connected NmrChains")
 
         if isinstance(firstResidue, str):
             xx = project.getByPid(firstResidue)

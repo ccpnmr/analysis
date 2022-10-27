26bbb1b206859c1f272a6aa00a99f8f20417fd36
Ed Brooksbank <ejb66@le.ac.uk>
2022-10-27 15:22:44 +0100

    base-widget uses class attribute '_notifiers'

--- src/python/ccpn/ui/gui/widgets/CompoundBaseWidget.py
+++ src/python/ccpn/ui/gui/widgets/CompoundBaseWidget.py
@@ -1,23 +1,22 @@
 """
 Base class for compound widgets
-
 """
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-06-09 18:47:01 +0100 (Wed, June 09, 2021) $"
-__version__ = "$Revision: 3.0.4 $"
+__dateModified__ = "$dateModified: 2022-10-27 15:22:44 +0100 (Thu, October 27, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -27,26 +26,14 @@ __date__ = "$Date: 2017-04-18 15:19:30 +0100 (Tue, April 18, 2017) $"
 # Start of code
 #=========================================================================================
 
+import contextlib
 from PyQt5 import QtCore
 from ccpn.ui.gui.widgets.Frame import Frame
 from ccpn.core.lib.Notifiers import Notifier
 from ccpn.ui.gui.widgets.Base import SignalBlocking
-from ccpn.util.Logging import getLogger
-# from ccpn.ui.gui.widgets.CheckBox import CheckBox
-# from ccpn.ui.gui.widgets.ColourDialog import ColourDialog
-# from ccpn.ui.gui.widgets.LineEdit import LineEdit
-# from ccpn.ui.gui.widgets.PulldownList import PulldownList
-# from ccpn.ui.gui.widgets.DoubleSpinbox import DoubleSpinbox
-# from ccpn.ui.gui.widgets.RadioButton import RadioButton
-# from ccpn.ui.gui.widgets.RadioButtons import RadioButtons
-# from ccpn.ui.gui.widgets.Slider import Slider
-# from ccpn.ui.gui.widgets.Spinbox import Spinbox
-# from ccpn.ui.gui.widgets.TextEditor import TextEditor
 
 
 NULL = object()
-
-NOTIFIERS = '_notifiers'
 
 
 class CompoundBaseWidget(Frame, SignalBlocking):
@@ -54,6 +41,7 @@ class CompoundBaseWidget(Frame, SignalBlocking):
     Base widget for Compound classes; inherits from Frame (and hence Base)
     Implements the addNotifier and deleteNotifiers methods
     """
+    _notifiers = []
 
     def __init__(self, parent, layoutDict, orientation, showBorder, **kwds):
         """
@@ -66,20 +54,12 @@ class CompoundBaseWidget(Frame, SignalBlocking):
         """
         super().__init__(parent=parent, showBorder=showBorder, setLayout=True, **kwds)
 
-        if not orientation in layoutDict:
+        if orientation not in layoutDict:
             raise RuntimeError('Invalid parameter "orientation" (%s)' % orientation)
         self._orientation = orientation
-        self._gridding = layoutDict[orientation]  # list of grid tuples for all succesive widgets
+        self._gridding = layoutDict[orientation]  # list of grid tuples for all successive widgets
         self._widgets = []  # list of all the widgets; use addWidget to add using the layoutDict
         self._blockingLevel = 0
-
-        # notifiers
-        #TODO:GEERTEN: poor design?; rethink this over
-        if not hasattr(self, NOTIFIERS):
-            setattr(self, NOTIFIERS, [])  # list of all notifiers for this widget
-        nf = getattr(self, NOTIFIERS)
-        if not isinstance(nf, list):
-            raise RuntimeError('Invalid notifiers attribute (%s)' % nf)
 
     def _addWidget(self, widget):
         """Add widget, using the layout as defined previously by layoutDict and orientation"""
@@ -93,7 +73,7 @@ class CompoundBaseWidget(Frame, SignalBlocking):
         """Set minimumwidths of widgets"""
         if len(minimumWidths) < len(self._widgets):
             raise RuntimeError('Not enough values to set minimum widths of all widgets')
-        for i, width in enumerate(minimumWidths[0:len(self._widgets)]):
+        for i, width in enumerate(minimumWidths[:len(self._widgets)]):
             if width is not None:
                 self._widgets[i].setMinimumWidth(width)
 
@@ -101,7 +81,7 @@ class CompoundBaseWidget(Frame, SignalBlocking):
         """Set maximumWidths of widgets"""
         if len(maximumWidths) < len(self._widgets):
             raise RuntimeError('Not enough values to set maximum widths of all widgets')
-        for i, width in enumerate(maximumWidths[0:len(self._widgets)]):
+        for i, width in enumerate(maximumWidths[:len(self._widgets)]):
             if width is not None:
                 self._widgets[i].setMaximumWidth(width)
 
@@ -109,7 +89,7 @@ class CompoundBaseWidget(Frame, SignalBlocking):
         """Set maximumWidths of widgets"""
         if len(fixedWidths) < len(self._widgets):
             raise RuntimeError('Not enough values to set fixed widths of all widgets')
-        for i, width in enumerate(fixedWidths[0:len(self._widgets)]):
+        for i, width in enumerate(fixedWidths[:len(self._widgets)]):
             if width is not None:
                 self._widgets[i].setFixedWidth(width)
 
@@ -117,7 +97,7 @@ class CompoundBaseWidget(Frame, SignalBlocking):
         """Set fixed heights of widgets"""
         if len(fixedHeights) < len(self._widgets):
             raise RuntimeError('Not enough values to set fixed heights of all widgets')
-        for i, height in enumerate(fixedHeights[0:len(self._widgets)]):
+        for i, height in enumerate(fixedHeights[:len(self._widgets)]):
             if height is not None:
                 self._widgets[i].setFixedHeight(height)
 
@@ -138,34 +118,27 @@ class CompoundBaseWidget(Frame, SignalBlocking):
         return notifier
 
     def addNotifier(self, notifier):
-        """add a notifer to the widget"""
-        if not hasattr(self, NOTIFIERS):
-            raise RuntimeError('Widget has no notifiers attribute')
-        nf = getattr(self, NOTIFIERS)
-        nf.append(notifier)
-        getLogger().debug('Added notifier %s to widget %s' % (notifier, self))
+        """add a notifier to the widget"""
+        self._notifiers.append(notifier)
 
     def deleteNotifiers(self):
         """Delete all notifiers associated with the widget"""
         while len(self._notifiers) > 0:
-            notifier = self._notifiers.pop()
-            #print('>deleteNotifier>', notifier)
-            notifier.unRegister()
-            del (notifier)
+            if notifier := self._notifiers.pop():
+                notifier.unRegister()
+                del (notifier)
 
     def __del__(self):
         # The project and all its things are already disassembled when closing the program;
-        # hence, the deregistering of notifiers fails and needs to be caught
-        try:
+        # hence, the de-registering of notifiers fails and needs to be caught
+        with contextlib.suppress(Exception):
             self.deleteNotifiers()
-        except:
-            pass
 
     def setPreSelect(self, callBack=None):
         """
         Add a user callback to the pulldown that fires on a mouse click.
         facilitates populating the pulldown list just before it opens
-        :param callBack = method to call on click:
+        :param callBack: method to call on click
         """
         if callBack:
             self.pulldownList.installEventFilter(self)
@@ -178,4 +151,3 @@ class CompoundBaseWidget(Frame, SignalBlocking):
         if target == self.pulldownList and event.type() == QtCore.QEvent.MouseButtonPress:
             self._preSelectCallBack()
         return False
-

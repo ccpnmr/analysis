91e83e65b7eba7d37dabaef3eb56ade429868a46
Ed Brooksbank <ejb66@le.ac.uk>
2022-10-28 10:42:42 +0100

    moved line to the wrong place

--- src/python/ccpn/ui/gui/widgets/table/TableABC.py
+++ src/python/ccpn/ui/gui/widgets/table/TableABC.py
@@ -15,7 +15,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-10-26 15:20:04 +0100 (Wed, October 26, 2022) $"
+__dateModified__ = "$dateModified: 2022-10-28 10:42:43 +0100 (Fri, October 28, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -101,6 +101,8 @@ class TableABC(_TableHeaderColumns, _TableCopyCell, _TableExport, _TableSearch, 
 
     # define the default TableModel class
     defaultTableModel = _TableModel
+
+    _droppedNotifier = None
 
     def __init__(self, parent, df=None,
                  multiSelect=True, selectRows=True,

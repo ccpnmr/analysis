cd9f46555b114f8bfe80453546d1d7481b125486
Ed Brooksbank <ejb66@le.ac.uk>
2022-11-29 19:21:10 +0000

    duplicate key-error for duplicate 'None' nmrAtoms in shift-list, and bug in debug statement :|

--- src/python/ccpn/core/ChemicalShiftList.py
+++ src/python/ccpn/core/ChemicalShiftList.py
@@ -14,7 +14,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-11-17 16:23:31 +0000 (Thu, November 17, 2022) $"
+__dateModified__ = "$dateModified: 2022-11-29 19:21:10 +0000 (Tue, November 29, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -663,12 +663,13 @@ class ChemicalShiftList(AbstractWrapperObject):
             oldLen = len(_data)
 
             # drop any duplicates and merge back in the Nones which must be kept
+            _data.reset_index(drop=True, inplace=True)
             _noDupes = _data.drop_duplicates(CS_NMRATOM).merge(_data[_data[CS_NMRATOM].isnull()], how='outer')
             _noDupes.sort_values(CS_UNIQUEID, inplace=True, )
             if len(_noDupes) != oldLen:
                 # log a warning and update the dataFrame
                 getLogger().warning(f'Duplicate nmrAtoms have been removed from {chemicalShiftList}')
-                getLogger().debug(f'>>> Dropped rows\n{_data.drop(_noDupes[CS_UNIQUEID])}')
+                getLogger().debug(f'>>> Dropped rows\n{list(_data.drop(_noDupes.index)[CS_UNIQUEID])}')
                 _data = _noDupes
 
             if CS_STATIC not in _data.columns:

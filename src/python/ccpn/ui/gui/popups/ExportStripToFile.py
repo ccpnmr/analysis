3fb3cc26007099d010c86bbd62bfc95da95f6d5b
Ed Brooksbank <ejb66@le.ac.uk>
2022-12-07 16:09:12 +0000

    was not updating the colour from the pulldown correctly

--- src/python/ccpn/ui/gui/popups/ExportStripToFile.py
+++ src/python/ccpn/ui/gui/popups/ExportStripToFile.py
@@ -15,7 +15,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-12-06 18:36:20 +0000 (Tue, December 06, 2022) $"
+__dateModified__ = "$dateModified: 2022-12-07 16:09:12 +0000 (Wed, December 07, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -1531,6 +1531,13 @@ class ExportStripToFilePopup(ExportDialogABC):
         # stripPadding = self.stripPaddingBox.getValue()
         # exportDpi = self.exportDpiBox.getValue()
 
+        fcolName = colourNameNoSpace(self.foregroundColourBox.getText())
+        if fcolName in spectrumColours.values():
+            fcolName = list(spectrumColours.keys())[list(spectrumColours.values()).index(fcolName)]
+        bcolName = colourNameNoSpace(self.backgroundColourBox.getText())
+        if bcolName in spectrumColours.values():
+            bcolName = list(spectrumColours.keys())[list(spectrumColours.values()).index(bcolName)]
+
         if strip:
             # return the parameters
             params = {GLFILENAME              : self.exitFilename,
@@ -1540,8 +1547,8 @@ class ExportStripToFilePopup(ExportDialogABC):
                       GLPRINTTYPE             : self.printType.get(),
                       GLPAGETYPE              : self.pageOrientation.get(),
                       GLPAGESIZE              : self.pageSize.get(),
-                      GLFOREGROUND            : hexToRgbRatio(self.foregroundColour),
-                      GLBACKGROUND            : hexToRgbRatio(self.backgroundColour),
+                      GLFOREGROUND            : hexToRgbRatio(fcolName),
+                      GLBACKGROUND            : hexToRgbRatio(bcolName),
                       GLBASETHICKNESS         : self.baseThicknessBox.getValue(),
                       # unique per spectrumDisplay - may differ from preferences
                       GLSYMBOLTHICKNESS       : strip.symbolThickness,

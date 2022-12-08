dc3797b63d203a04300bf834470da6e742dc3c30
Luca Mureddu <luca.mureddu@me.com>
2022-12-08 16:51:19 +0000

    Bug fixes on closing popups and _raise()

--- src/python/ccpn/ui/gui/modules/SpectrumDisplay1d.py
+++ src/python/ccpn/ui/gui/modules/SpectrumDisplay1d.py
@@ -10,12 +10,12 @@ __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliz
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2022-06-08 22:26:12 +0100 (Wed, June 08, 2022) $"
+__dateModified__ = "$dateModified: 2022-12-08 16:51:19 +0000 (Thu, December 08, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -100,7 +100,7 @@ class SpectrumDisplay1d(GuiSpectrumDisplay):
             popup = SpectrumDisplayPropertiesPopup1d(parent=self.mainWindow, mainWindow=self.mainWindow,
                                                      orderedSpectrumViews=self.strips[0].getSpectrumViews())
             popup.exec_()
-            popup.raise_()
+            # popup.raise_()
 
     def raiseContourBase(self):
         """
--- src/python/ccpn/ui/gui/widgets/SpectrumToolBar.py
+++ src/python/ccpn/ui/gui/widgets/SpectrumToolBar.py
@@ -10,12 +10,12 @@ __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliz
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-08-17 11:11:51 +0100 (Wed, August 17, 2022) $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2022-12-08 16:51:19 +0000 (Thu, December 08, 2022) $"
 __version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
@@ -291,7 +291,7 @@ class SpectrumToolBar(ToolBar):
         if spectrum:  #not sure why It needs parent = mainWindow. copied from SideBar
             popup = SpectrumPropertiesPopup(parent=mainWindow, mainWindow=mainWindow, spectrum=spectrum)
             popup.exec_()
-            popup.raise_()
+            # popup.raise_() # no need to raise()
 
     def _updateGl(self, ):
         from ccpn.ui.gui.lib.OpenGL.CcpnOpenGL import GLNotifier

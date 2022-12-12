675b592d15a9ddace7f6d5009ae5a12978e41918
Luca Mureddu <luca.mureddu@me.com>
2022-12-12 11:22:16 +0000

    Bug fixes on back compatibility for 3.0.4 -> 3.1 for peak height scaling

--- src/python/ccpn/core/_implementation/updates/update_3_0_4.py
+++ src/python/ccpn/core/_implementation/updates/update_3_0_4.py
@@ -4,19 +4,19 @@ update 3.0.4 routines
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2022"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-01-21 11:22:07 +0000 (Fri, January 21, 2022) $"
-__version__ = "$Revision: 3.0.4 $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2022-12-12 11:22:17 +0000 (Mon, December 12, 2022) $"
+__version__ = "$Revision: 3.1.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -64,7 +64,8 @@ def _updateSpectrum_NC_proc(spectrum):
                 spectrum.negativeContourBase *= scale
 
         for pk in spectrum.peaks:
-            pk.height *= scale
+            if pk.height is not None:
+                pk.height *= scale
 
 
 def _updateSpectrum_settings(spectrum):

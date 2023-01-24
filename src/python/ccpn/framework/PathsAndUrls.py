d4712e27df760b9b6897247a0a1b772d86412e1f
Ed Brooksbank <ejb66@le.ac.uk>
2023-01-24 15:57:27 +0000

    minor alignment contents

--- src/python/ccpn/framework/PathsAndUrls.py
+++ src/python/ccpn/framework/PathsAndUrls.py
@@ -13,8 +13,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2023-01-11 15:49:41 +0000 (Wed, January 11, 2023) $"
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2023-01-24 15:57:27 +0000 (Tue, January 24, 2023) $"
 __version__ = "$Revision: 3.1.1 $"
 #=========================================================================================
 # Created
@@ -28,7 +28,7 @@ __date__ = "$Date: 2016-07-09 14:17:30 +0100 (Sat, 09 Jul 2016) $"
 from ccpn.util import Path
 
 # CCPN Urls
-ccpnUrl                             = 'https://www.ccpn.ac.uk'
+ccpnUrl                         = 'https://www.ccpn.ac.uk'
 ccpn2Url                        = 'https://www.ccpn.ac.uk'
 ccpnLicenceUrl                  = 'https://www.ccpn.ac.uk/software/licensing'
 ccpnTutorials                   = 'https://www.ccpn.ac.uk/support/tutorials'
@@ -41,7 +41,7 @@ ccpnDocumentationUrl            = 'https://www.ccpn.ac.uk/api-documentation/v3/h
 # CCPN code
 ccpnCodePath                    = Path.aPath(Path.getTopDirectory())
 ccpnConfigPath                  = ccpnCodePath / 'config'
-ccpnResourcesPath              = ccpnCodePath / 'resources'
+ccpnResourcesPath               = ccpnCodePath / 'resources'
 ccpnRunTerminal                 = ccpnCodePath / 'bin' / 'runTerminal.sh'
 defaultPreferencesPath          = ccpnConfigPath / 'defaultv3settings.json'
 tipOfTheDayConfig               = ccpnConfigPath / 'tipConfig.hjson'
@@ -73,8 +73,8 @@ peakPickerPath                  = ccpnPythonPath / 'core' / 'lib' / 'PeakPickers
 # Program tutorials and documentation
 shortcutsPath                   = ccpnCodePath / 'doc' / 'static' / 'AnalysisShortcuts.htm'
 tutorialsPath                   = ccpnCodePath / 'tutorials'
-beginnersTutorialPath                        = ccpnCodePath / 'tutorials' / 'BeginnersTutorial.pdf'
-backboneAssignmentTutorialPath     = ccpnCodePath / 'tutorials' / 'BackboneAssignmentTutorial.pdf'
+beginnersTutorialPath           = ccpnCodePath / 'tutorials' / 'BeginnersTutorial.pdf'
+backboneAssignmentTutorialPath  = ccpnCodePath / 'tutorials' / 'BackboneAssignmentTutorial.pdf'
 screenTutorialPath              = ccpnCodePath / 'tutorials' / 'CcpNmr_AnalysisScreen_Tutorial_Beta2Release.pdf'
 cspTutorialPath                 = ccpnCodePath / 'tutorials' / 'CSPTutorial.pdf'
 solidStatePeptideTutorialPath   = ccpnCodePath / 'tutorials' / 'SolidStatePeptideAssignmentTutorial.pdf'

2fd8a1727e5c718a6322ff6fcd016de43749210a
Geerten Vuister <gv29@leicester.ac.uk>
2023-01-25 18:31:00 +0000

    Single ui.gui.modules.SpectrumDisplay file

--- src/python/ccpn/ui/gui/modules/SpectrumDisplay1d.py
+++ src/python/ccpn/ui/gui/modules/SpectrumDisplay1d.py
@@ -4,7 +4,7 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
@@ -14,9 +14,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-12-21 12:16:44 +0000 (Wed, December 21, 2022) $"
-__version__ = "$Revision: 3.1.0 $"
+__modifiedBy__ = "$modifiedBy: Geerten Vuister $"
+__dateModified__ = "$dateModified: 2023-01-25 18:31:00 +0000 (Wed, January 25, 2023) $"
+__version__ = "$Revision: 3.1.1 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -30,9 +30,7 @@ from typing import Sequence
 
 from ccpn.ui.gui.lib.GuiSpectrumDisplay import GuiSpectrumDisplay
 from ccpn.ui.gui.widgets.Icon import Icon
-from ccpn.ui.gui.popups.SpectrumPropertiesPopup import SpectrumDisplayPropertiesPopup1d
 from ccpn.util.Logging import getLogger
-from ccpn.core.lib.ContextManagers import undoBlockWithoutSideBar
 
 
 class SpectrumDisplay1d(GuiSpectrumDisplay):
@@ -40,10 +38,9 @@ class SpectrumDisplay1d(GuiSpectrumDisplay):
     MAXPEAKSYMBOLTYPES = 4
 
     def __init__(self, mainWindow):
-        # if not apiSpectrumDisplay1d.strips:
-        #   apiSpectrumDisplay1d.newStrip1d()
 
         GuiSpectrumDisplay.__init__(self, mainWindow=mainWindow, useScrollArea=True)
+
         self._fillToolBar()
         # self.addSpinSystemSideLabel()
         self.setAcceptDrops(True)
@@ -63,19 +60,19 @@ class SpectrumDisplay1d(GuiSpectrumDisplay):
         self._spectrumUtilActions = {}
 
         toolBarItemsFor1d = [
-            #  action name,        icon,                 tooltip,                                   active, callback
+            #  action name,        icon,                 tooltip,                                       active, callback
 
-            ('increaseStripWidth', 'icons/range-expand', 'Increase the width of strips in display', True, self.increaseStripSize),
-            ('decreaseStripWidth', 'icons/range-contract', 'Decrease the width of strips in display', True, self.decreaseStripSize),
-            ('maximiseZoom', 'icons/zoom-full', 'Maximise Zoom (ZA)', True, self._resetAllZooms),
+            ('increaseStripWidth', 'icons/range-expand',   'Increase the width of strips in display',   True, self.increaseStripSize),
+            ('decreaseStripWidth', 'icons/range-contract', 'Decrease the width of strips in display',   True, self.decreaseStripSize),
+            ('maximiseZoom',       'icons/zoom-full',      'Maximise Zoom (ZA)',                        True, self._resetAllZooms),
 
-            ('maximiseHeight', 'icons/zoom-best-fit-1d', 'Maximise Height', True, self._resetYZooms),
-            ('maximiseWidth', 'icons/zoom-full-1d', 'Maximise Width', True, self._resetXZooms),
+            ('maximiseHeight',     'icons/zoom-best-fit-1d', 'Maximise Height',                         True, self._resetYZooms),
+            ('maximiseWidth',      'icons/zoom-full-1d',     'Maximise Width',                          True, self._resetXZooms),
 
-            ('storeZoom', 'icons/zoom-store', 'Store Zoom (ZS)', True, self._storeZoom),
-            ('restoreZoom', 'icons/zoom-restore', 'Restore Zoom (ZR)', True, self._restoreZoom),
-            ('undoZoom', 'icons/zoom-undo', 'Previous Zoom (ZP)', True, self._previousZoom),
-            ('redoZoom', 'icons/zoom-redo', 'Next Zoom (ZN)', True, self._nextZoom),
+            ('storeZoom',          'icons/zoom-store',   'Store Zoom (ZS)',                             True, self._storeZoom),
+            ('restoreZoom',        'icons/zoom-restore', 'Restore Zoom (ZR)',                           True, self._restoreZoom),
+            ('undoZoom',           'icons/zoom-undo',    'Previous Zoom (ZP)',                          True, self._previousZoom),
+            ('redoZoom',           'icons/zoom-redo',    'Next Zoom (ZN)',                              True, self._nextZoom),
             # ('addStrip', 'icons/plus', 'Duplicate the rightmost strip', True, self.addStrip),
             # ('removeStrip', 'icons/minus', 'Remove the current strip', True, self.removeCurrentStrip),
             ]
@@ -89,15 +86,21 @@ class SpectrumDisplay1d(GuiSpectrumDisplay):
             self._spectrumUtilActions[aName] = action
 
     def processSpectra(self, pids: Sequence[str], event):
-        """Display spectra defined by list of Pid strings"""
+        """Display spectra defined by list of Pid strings
+        """
         for ss in pids:
             getLogger().info('processing Spectrum %s' % ss)
         getLogger().info(str(self.parent()))
 
     def adjustContours(self):
-        # insert popup to modify contours
+        """Initiate a popup to modify  settings
+        """
+        #GWV: Very strange and really should not be here; called from GuiMainWindow TODO: move there
+        from ccpn.ui.gui.popups.SpectrumPropertiesPopup import SpectrumDisplayPropertiesPopup1d
+
         if self.strips:
-            popup = SpectrumDisplayPropertiesPopup1d(parent=self.mainWindow, mainWindow=self.mainWindow,
+            popup = SpectrumDisplayPropertiesPopup1d(parent=self.mainWindow,
+                                                     mainWindow=self.mainWindow,
                                                      orderedSpectrumViews=self.strips[0].getSpectrumViews())
             popup.exec_()
             # popup.raise_()
@@ -115,8 +118,6 @@ class SpectrumDisplay1d(GuiSpectrumDisplay):
         """
         # Currently not implemented
         pass
-
-
 
     def setVisibleAxes(self):
         # skip for 1D for the minute
--- src/python/ccpn/ui/gui/modules/SpectrumDisplayNd.py
+++ src/python/ccpn/ui/gui/modules/SpectrumDisplayNd.py
@@ -4,19 +4,19 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2022-06-08 22:26:12 +0100 (Wed, June 08, 2022) $"
-__version__ = "$Revision: 3.1.0 $"
+__modifiedBy__ = "$modifiedBy: Geerten Vuister $"
+__dateModified__ = "$dateModified: 2023-01-25 18:31:00 +0000 (Wed, January 25, 2023) $"
+__version__ = "$Revision: 3.1.1 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -27,12 +27,9 @@ __date__ = "$Date: 2017-04-07 10:28:41 +0000 (Fri, April 07, 2017) $"
 #=========================================================================================
 
 from ccpnmodel.ccpncore.api.ccpnmr.gui.Task import BoundDisplay as ApiBoundDisplay
-from ccpn.core.Project import Project
 from ccpn.core.lib.ContextManagers import undoBlockWithoutSideBar
 from ccpn.ui.gui.lib.GuiSpectrumDisplay import GuiSpectrumDisplay
 from ccpn.ui.gui.widgets.Icon import Icon
-from ccpn.ui.gui.popups.SpectrumPropertiesPopup import SpectrumDisplayPropertiesPopupNd
-from ccpn.util.decorators import logCommand
 
 
 class SpectrumDisplayNd(GuiSpectrumDisplay):
@@ -267,7 +264,11 @@ class SpectrumDisplayNd(GuiSpectrumDisplay):
             strip._updateTraces()
 
     def adjustContours(self):
-        # insert popup to modify contours
+        """Initiate a popup to modify  settings
+        """
+        #GWV: Very strange and really should not be here; called from GuiMainWindow TODO: move there
+        from ccpn.ui.gui.popups.SpectrumPropertiesPopup import SpectrumDisplayPropertiesPopupNd
+
         if self.strips:
             popup = SpectrumDisplayPropertiesPopupNd(parent=self.mainWindow, mainWindow=self.mainWindow,
                                                      orderedSpectrumViews=self.strips[0].getSpectrumViews())

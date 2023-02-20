a9b5facea16cb80fc1cf894106bb001210923cee
Luca Mureddu <luca.mureddu@me.com>
2023-02-20 12:45:31 +0000

    Progressing with Macros for the  MCB Practical 2023. Refactored macro 4

--- src/python/ccpn/macros/4_T1T2_contourScatter_plot.py
+++ src/python/ccpn/macros/4_T1T2_contourScatter_plot.py
@@ -1,5 +1,15 @@
 """
-A macro to plot  clusters of  T2/T1  relaxation  analysis results.
+A macro for creating a scatter plot of  T1 vs T2  with S2 and Tc as contouring lines
+
+This macro requires a  dataset  created after performing  the Reduced Spectral density Mapping calculation model
+on the Relaxation Analysis Module (alpha)
+Which is a dataset that contains a table with the following (mandatory) columns:
+    -  nmrResidueCode
+    -  R1 and  R1_err
+    - R2 and  R2_err
+    - HetNoe and  HetNoe_err
+
+Macro created for Analysis Version 3.1.1
 
 """
 #=========================================================================================
@@ -16,7 +26,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2023-02-17 17:35:26 +0000 (Fri, February 17, 2023) $"
+__dateModified__ = "$dateModified: 2023-02-20 12:45:31 +0000 (Mon, February 20, 2023) $"
 __version__ = "$Revision: 3.1.1 $"
 #=========================================================================================
 # Created
@@ -27,37 +37,17 @@ __date__ = "$Date: 2023-02-03 10:04:03 +0000 (Fri, February 03, 2023) $"
 # Start of code
 #=========================================================================================
 
-import numpy as np
-import ccpn.framework.lib.experimentAnalysis.SeriesAnalysisVariables as sv
-import ccpn.framework.lib.experimentAnalysis.fitFunctionsLib as lf
-import pandas as pd
-import matplotlib.pyplot as plt
-import math
-import time
-import datetime
-from matplotlib.backends.backend_pdf import PdfPages
-from ccpn.util.Logging import getLogger
-from matplotlib.ticker import MultipleLocator
-from ccpn.ui.gui.widgets.DrawSS import createBlocksFromSequence, plotSS
-from ccpn.util.floatUtils import fExp, fMan
-import ccpn.framework.lib.experimentAnalysis.experimentConstants as constants
-import ccpn.framework.lib.experimentAnalysis.spectralDensityLib as sdl
-from math import atan2,degrees
 
+############################################################
+#####################    User Settings      #######################
+############################################################
 
-####### User's Settings  ###################
-fileName = 'RelaxClustersAnalysisResults'
-filePath = f'/Users/luca/Documents/V3-testings/{fileName}'
+dataTableName = 'RSDM_results'
 
-#### Data #####
-RSDMResultsDataTable = get('DT:RSDM_results')
-data =  RSDMResultsDataTable.data
-x = data[sv.NMRRESIDUECODE]
-x = x.astype(int)
-x = x.values
-R1 = data[sv.R1].values
-R2 = data[sv.R2].values
-NOE = data[sv.HETNOE_VALUE]
+##  demo sequence for the GB1 protein . Replace with an empty str if not available, e.g.: sequence  = ''
+sequence  = 'KLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDAATKTFTVTE'
+##  secondary structure  for the above sequence  using the DSSP nomenclature.  Replace with an empty str if not available. e.g.: ss_sequence  = ''
+ss_sequence   =  'BBBBBCCCCBBBBBBCCCCHHHHHHHHHHHHHHCCCCCBBBBCCCCCBBBBBC'
 
 
 ####### S2_contours
@@ -67,109 +57,55 @@ S2_contours_maxValue = 1.0
 S2_contours_stepValue = 0.1
 
 ##  Tm  Contours Lines (rotational correlation Time Lines)
-Tm_line_minValue = 5.0
-Tm_line_maxValue = 14.0
-Tm_line_stepValue = 1.0
+Tm_line_minValue = 3
+Tm_line_maxValue = 8
+Tm_line_stepValue = 0.5
 
 ##  Physical  Params
-SPECTROMETERFREQUENCY = 600.130
+spectrometerFrequency=600.130
 NH_bondLenght = 1.0150 # Armstrong
 InternalCorrelationTimeTe = itc =  50.0
 CSA15N = -160.0 # ppm
+rctScalingFactor = 1e-9
+ictScalingFactor = 1e-12
 
-##  GUI - T1 vs T2 scatter  X-Y limits
-Cluster_maxDifferenceMs = 20.0
-Cluster_minSize  = 5.0
-Cluster_MinX_T1 = 300
-Cluster_MaxX_T1 = 1000
-Cluster_MinY_T2 = 20
-Cluster_MaxY_T2 = 700
+## Some Graphics Settings
 
-ss_sequence = 'BBBBBCCCCBBBBBBCCCCHHHHHHHHHHHHHHCCCCCBBBBCCCCCBBBBBC'
-sequence = 'KLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDAATKTFTVTE'
+titlePdf  = 'GB1 Relaxation Rates with Isotropic-Model Contouring Lines '
+showInteractivePlot = False # True if you want the plot to popup as a new windows, to allow the zooming and panning of the figure.
 
-# graphics
-fontTitleSize = 8
-fontXSize = 6
-fontYSize = 6
-scatterFontSize = 5
-labelMajorSize=6
-labelMinorSize=5
+fontTitleSize = 6
+fontXSize = 4
+fontYSize =  4
+labelMajorSize=4
+labelMinorSize=3
 titleColor = 'blue'
-hspace= 0.5
+hspace= 1
+figureTitleFontSize = 10
+
+rctLineColour =  'blue'
+s2LineColour =  'red'
+
+# exporting to pdf: Default save to same location and name.pdf as this macro
+#  alternatively, provide a full valid path
+exportingFilePath = None
+
+############################################################
+##################   End User Settings     ########################
+############################################################
 
 
-
-
-
-def findNearestIndex(array, value):
-    idx = (np.abs(array - value)).argmin()
-    return idx
-
-
-def labelLine(line,x,label=None,align=True,**kwargs):
-    ax = line.axes
-    xdata = line.get_xdata()
-    ydata = line.get_ydata()
-    if (x < xdata[0]) or (x > xdata[-1]):
-        print('x label location is outside data range!')
-        return
-    #Find corresponding y co-ordinate and angle of the line
-    ip = 1
-    for i in range(len(xdata)):
-        if x < xdata[i]:
-            ip = i
-            break
-    y = ydata[ip-1] + (ydata[ip]-ydata[ip-1])*(x-xdata[ip-1])/(xdata[ip]-xdata[ip-1])
-    if not label:
-        label = line.get_label()
-    if align:
-        #Compute the slope
-        dx = xdata[ip] - xdata[ip-1]
-        dy = ydata[ip] - ydata[ip-1]
-        ang = degrees(atan2(dy,dx))
-        #Transform to screen co-ordinates
-        pt = np.array([x,y]).reshape((1,2))
-        trans_angle = ax.transData.transform_angles(np.array((ang,)),pt)[0]
-    else:
-        trans_angle = 0
-    if 'color' not in kwargs:
-        kwargs['color'] = line.get_color()
-    if ('horizontalalignment' not in kwargs) and ('ha' not in kwargs):
-        kwargs['ha'] = 'center'
-    if ('verticalalignment' not in kwargs) and ('va' not in kwargs):
-        kwargs['va'] = 'center'
-    if 'backgroundcolor' not in kwargs:
-        kwargs['backgroundcolor'] = ax.get_facecolor()
-    if 'clip_on' not in kwargs:
-        kwargs['clip_on'] = True
-    if 'zorder' not in kwargs:
-        kwargs['zorder'] = 2.5
-    color = kwargs.get('color', )
-    fontsize = kwargs.get('fontsize', 5)
-    ax.text(x, y, label, rotation=0, color=color, fontsize=fontsize, )
-    # ax.text(x,y,label,rotation=trans_angle,**kwargs)
-
-def labelLines(lines, align=True,xvals=None,**kwargs):
-    ax = lines[0].axes
-    labLines = []
-    labels = []
-    for line in lines:
-        label = line.get_label()
-        if "_line" not in label:
-            labLines.append(line)
-            label = label.lstrip('_')
-            labels.append(label)
-    if xvals is None:
-        xmin,xmax = ax.get_xlim()
-        xvals = np.linspace(xmin,xmax,len(labLines)+2)[1:-1]
-    for line,x,label in zip(labLines,xvals,labels):
-        labelLine(line,x,label,align,**kwargs)
-
+import numpy as np
+import ccpn.framework.lib.experimentAnalysis.SeriesAnalysisVariables as sv
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import ccpn.framework.lib.experimentAnalysis.spectralDensityLib as sdl
+import  ccpn.ui.gui.modules.experimentAnalysis._macrosLib as macrosLib
+from ccpn.util.Common import percentage
 
 def _plotClusters(pdf, xMaxLim=800):
     fig = plt.figure(dpi=300)
-    fig.suptitle(' R2 vs R1 Isotropic Analysis Scatter', fontsize=10)
+    fig.suptitle(titlePdf, fontsize=fontTitleSize)
 
     ax = plt.subplot(111)
     ## R1 vs R2
@@ -177,7 +113,12 @@ def _plotClusters(pdf, xMaxLim=800):
     T2 = 1 / R2*1e3
     ax.scatter(T1, T2, s=1, color='black', )
     for i, txt in enumerate(x):
-        ax.annotate(str(txt), (T1[i], T2[i]), fontsize=2)
+        extraY = percentage(0.5, T2[i])
+        yPos = T2[i] + extraY
+        extraX = percentage(0.5, T1[i])
+        xPos = T1[i] + extraX
+        ax.annotate(str(txt), xy=(T1[i], T2[i]), xytext=(xPos, yPos), fontsize=3.5, arrowprops=dict(facecolor='grey',arrowstyle="-",lw=0.3  ))
+
     plt.subplots_adjust(hspace=hspace)
     ax.set_title('T2 vs T1',  fontsize=fontTitleSize, color=titleColor)
     ax.set_xlabel('T$_{1} (ms)$', fontsize=fontYSize, )
@@ -190,8 +131,6 @@ def _plotClusters(pdf, xMaxLim=800):
     ax.xaxis.get_offset_text().set_size(5)
     plt.tight_layout()
 
-    rctLines, s2Lines = sdl.calculateSpectralDensityContourLines(minRct = 3.0,
-                                         maxRct=8, stepRct=0.5)
     for i in rctLines:
         n, lines = i
         rctArray = np.array(lines)
@@ -199,11 +138,11 @@ def _plotClusters(pdf, xMaxLim=800):
         xRct, yRct = rct
         n = float(n)
         if n.is_integer():
-            ax.plot(xRct, yRct, 'blue', linewidth=0.5, label='_'+str(round(n, 2)))
+            ax.plot(xRct, yRct, rctLineColour, linewidth=0.5, label='_'+str(round(n, 2)))
         else:
-            ax.plot(xRct, yRct, 'blue', linewidth=0.2, label='_' + str(round(n, 2)))
+            ax.plot(xRct, yRct, rctLineColour, linewidth=0.2, label='_' + str(round(n, 2)))
         # add labels
-    labelLines(plt.gca().get_lines(), xvals=[xMaxLim+10]*len(rctLines), color='blue', align=False, clip_on=False, fontsize=4, zorder=None)
+    macrosLib.labelLines(plt.gca().get_lines(), xvals=[xMaxLim+10]*len(rctLines), color=rctLineColour, align=False, clip_on=False, fontsize=4, zorder=None)
 
     for j in s2Lines:
         n, lines = j
@@ -212,25 +151,61 @@ def _plotClusters(pdf, xMaxLim=800):
         xSe, ySe = s2L
         ax.plot(xSe, ySe, 'r',  linewidth=0.5)
         # add labels
-        ixMinxSe = findNearestIndex(xSe, np.min(xSe))
+        ixMinxSe = macrosLib.findNearestIndex(xSe, np.min(xSe))
         lXse = xSe[ixMinxSe]
         lYse = ySe[ixMinxSe]
-        ax.annotate(str(round(n, 2)), (lXse, lYse), color='r', fontsize=5)
-    ax.plot([],[],  'blue', linewidth=0.5, label='$Tm$ rotational correlation time') # just a  placeholder
-    ax.plot([],[],  'r', linewidth=0.5, label='$S^2$ order parameter')
+        ax.annotate(str(round(n, 2)), (lXse, lYse), color=s2LineColour, fontsize=5)
+    ax.plot([],[], rctLineColour, linewidth=0.5, label='$T_m$ rotational correlation time') # just a  placeholder
+    ax.plot([],[],  s2LineColour, linewidth=0.5, label='$S^2$ order parameter')
 
     ax.set_xlim(300, xMaxLim+10)
     ax.set_ylim(0, 500)
     ax.legend(prop={'size': 6})
 
     pdf.savefig()
-    plt.close()
+    return fig
+
+###################      start inline macro       #####################
 
 
-# init pdf
-with PdfPages(f'{filePath}.pdf') as pdf:
-    _plotClusters(pdf)
+
+#### Data #####
+dataTable = macrosLib._getDataTableForMacro(dataTableName)
+data =  dataTable.data
+x = data[sv.NMRRESIDUECODE]
+x = x.astype(int)
+x = x.values
+
+R1 = data[sv.R1].values
+R2 = data[sv.R2].values
+NOE = data[sv.HETNOE_VALUE]
+
+rctLines, s2Lines = sdl.calculateSpectralDensityContourLines(
+                                                            spectrometerFrequency=spectrometerFrequency,
+                                                            lenNh=NH_bondLenght,
+                                                            ict=InternalCorrelationTimeTe,
+                                                            csaN=CSA15N,
+                                                            minS2=S2_contours_minValue,
+                                                            maxS2=S2_contours_maxValue,
+                                                            stepS2=S2_contours_stepValue,
+                                                            rctScalingFactor=rctScalingFactor,
+                                                            ictScalingFactor=ictScalingFactor,
+                                                            minRct=Tm_line_minValue,
+                                                            maxRct=Tm_line_maxValue,
+                                                            stepRct=Tm_line_stepValue
+                                                        )
 
 
+filePath = macrosLib._getExportingPath(__file__, exportingFilePath)
+with PdfPages(filePath) as pdf:
+    fig1 = _plotClusters(pdf)
+    info(f'Report saved in {filePath}')
+
+if showInteractivePlot:
+    plt.show()
+else:
+    plt.close(fig1)
+
+application._showHtmlFile("Show Plots", filePath)
 
 
--- src/python/ccpn/ui/gui/modules/experimentAnalysis/_macrosLib.py
+++ src/python/ccpn/ui/gui/modules/experimentAnalysis/_macrosLib.py
@@ -1,5 +1,7 @@
 """
 A set of private functions called for building custom macros.
+- DO NOT CHANGE without proper deprecation warnings and info
+- use at own risk
 
 """
 #=========================================================================================
@@ -16,7 +18,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2023-02-20 12:00:40 +0000 (Mon, February 20, 2023) $"
+__dateModified__ = "$dateModified: 2023-02-20 12:45:32 +0000 (Mon, February 20, 2023) $"
 __version__ = "$Revision: 3.1.1 $"
 #=========================================================================================
 # Created
@@ -110,3 +112,70 @@ def _getDataTableForMacro(dataTableName):
         showWarning('Datatable not found', errorMess)
         raise RuntimeError(errorMess)
     return dataTable
+
+
+##  Used for the Contouring lines
+
+def findNearestIndex(array, value):
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+def labelLine(line,x,label=None,align=True,**kwargs):
+    from math import atan2, degrees
+    ax = line.axes
+    xdata = line.get_xdata()
+    ydata = line.get_ydata()
+    if (x < xdata[0]) or (x > xdata[-1]):
+        print('x label location is outside data range!')
+        return
+    #Find corresponding y co-ordinate and angle of the line
+    ip = 1
+    for i in range(len(xdata)):
+        if x < xdata[i]:
+            ip = i
+            break
+    y = ydata[ip-1] + (ydata[ip]-ydata[ip-1])*(x-xdata[ip-1])/(xdata[ip]-xdata[ip-1])
+    if not label:
+        label = line.get_label()
+    if align:
+        #Compute the slope
+        dx = xdata[ip] - xdata[ip-1]
+        dy = ydata[ip] - ydata[ip-1]
+        ang = degrees(atan2(dy,dx))
+        #Transform to screen co-ordinates
+        pt = np.array([x,y]).reshape((1,2))
+        trans_angle = ax.transData.transform_angles(np.array((ang,)),pt)[0]
+    else:
+        trans_angle = 0
+    if 'color' not in kwargs:
+        kwargs['color'] = line.get_color()
+    if ('horizontalalignment' not in kwargs) and ('ha' not in kwargs):
+        kwargs['ha'] = 'center'
+    if ('verticalalignment' not in kwargs) and ('va' not in kwargs):
+        kwargs['va'] = 'center'
+    if 'backgroundcolor' not in kwargs:
+        kwargs['backgroundcolor'] = ax.get_facecolor()
+    if 'clip_on' not in kwargs:
+        kwargs['clip_on'] = True
+    if 'zorder' not in kwargs:
+        kwargs['zorder'] = 2.5
+    color = kwargs.get('color', )
+    fontsize = kwargs.get('fontsize', 5)
+    ax.text(x, y, label, rotation=0, color=color, fontsize=fontsize, )
+    # ax.text(x,y,label,rotation=trans_angle,**kwargs)
+
+def labelLines(lines, align=True,xvals=None,**kwargs):
+    ax = lines[0].axes
+    labLines = []
+    labels = []
+    for line in lines:
+        label = line.get_label()
+        if "_line" not in label:
+            labLines.append(line)
+            label = label.lstrip('_')
+            labels.append(label)
+    if xvals is None:
+        xmin,xmax = ax.get_xlim()
+        xvals = np.linspace(xmin,xmax,len(labLines)+2)[1:-1]
+    for line,x,label in zip(labLines,xvals,labels):
+        labelLine(line,x,label,align,**kwargs)

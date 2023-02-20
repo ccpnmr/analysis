570a6612bdf7fac7044e1826f7d4fb7048057163
Luca Mureddu <luca.mureddu@me.com>
2023-02-20 13:04:21 +0000

    Progressing with Macros for the  MCB Practical 2023. Refactored macro 5

--- src/python/ccpn/macros/5_IsotropicAnalysis_plots.py
+++ src/python/ccpn/macros/5_IsotropicAnalysis_plots.py
@@ -1,8 +1,20 @@
 """
-A Demo report for relaxation isotropic analysis results.
+A macro for creating plots of R1, R2 and NOE  isotropic analysis results for GB1.
+Note. This macro is for demonstration purpose only.
+It uses the original LZ model from Model-Free which is wrong for the current example of GB1.
 
+This macro requires a  dataset  created after performing  the Reduced Spectral density Mapping calculation model
+on the Relaxation Analysis Module (alpha)
+Which is a dataset that contains a table with the following (mandatory) columns:
+    -  nmrResidueCode
+    -  R1 and  R1_err
+    - R2 and  R2_err
+    - HetNoe and  HetNoe_err
+
+Macro created for Analysis Version 3.1.1
 
 """
+
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
@@ -17,7 +29,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2023-02-17 17:35:26 +0000 (Fri, February 17, 2023) $"
+__dateModified__ = "$dateModified: 2023-02-20 13:04:21 +0000 (Mon, February 20, 2023) $"
 __version__ = "$Revision: 3.1.1 $"
 #=========================================================================================
 # Created
@@ -27,108 +39,83 @@ __date__ = "$Date: 2023-02-03 10:04:03 +0000 (Fri, February 03, 2023) $"
 #=========================================================================================
 # Start of code
 #=========================================================================================
-import os.path
+
+
+
+
+############################################################
+#####################    User Settings      #######################
+############################################################
+
+dataTableName = 'RSDM_results'
+
+##  demo sequence for the GB1 protein . Replace with an empty str if not available, e.g.: sequence  = ''
+sequence  = 'KLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDAATKTFTVTE'
+##  secondary structure  for the above sequence  using the DSSP nomenclature.  Replace with an empty str if not available. e.g.: ss_sequence  = ''
+ss_sequence   =  'BBBBBCCCCBBBBBBCCCCHHHHHHHHHHHHHHCCCCCBBBBCCCCCBBBBBC'
+
+NOE_limitExclusion = 0.65
+spectrometerFrequency=600.13
+
+## Some Graphics Settings
+titlePdf  = 'Anisotropy and Chemical Exchange Determination of GB1'
+figureTitleFontSize = 8
+showInteractivePlot = False # True if you want the plot to popup as a new windows, to allow the zooming and panning of the figure.
+barColour='black'
+barErrorColour='red'
+barErrorLW = 1
+barErrorCapsize=2
+barErrorCapthick=0.5
+fontTitleSize = 6
+fontXSize = 4
+fontYSize =  4
+labelMajorSize=4
+labelMinorSize=3
+titleColor = 'blue'
+hspace= 1
+scatterColor = 'navy'
+scatterColorError = 'darkred'
+scatterExcludedByNOEColor = 'orange'
+scatterSize = 3
+scatterErrorLinewidth=0.1
+scatterErrorCapSize=0.8
+TrimmedLineColour = 'black'
+scatterFontSize = 5
+
+# exporting to pdf: Default save to same location and name.pdf as this macro
+#  alternatively, provide a full valid path
+exportingFilePath = None
+
+############################################################
+##################   End User Settings     ########################
+############################################################
 
 import numpy as np
 import ccpn.framework.lib.experimentAnalysis.SeriesAnalysisVariables as sv
-import ccpn.framework.lib.experimentAnalysis.fitFunctionsLib as lf
 import ccpn.framework.lib.experimentAnalysis.spectralDensityLib as sdl
-import pandas as pd
 import matplotlib.pyplot as plt
-import math
-import time
-import datetime
 from matplotlib.backends.backend_pdf import PdfPages
-from ccpn.util.Logging import getLogger
 from matplotlib.ticker import MultipleLocator
-from ccpn.ui.gui.widgets.DrawSS import createBlocksFromSequence, plotSS
-from ccpn.util.floatUtils import fExp, fMan
+from ccpn.ui.gui.widgets.DrawSS import plotSS
+import  ccpn.ui.gui.modules.experimentAnalysis._macrosLib as macrosLib
 
-# #########
-
-###################
-
-
-exportingFileName = 'RelaxationIsotropicAnalysisResults'
-exportingDirectoryPath = '/Users/luca/Documents/V3-testings/'
-
-RSDMResultsDataTable = get('DT:RSDM_results')
-
-if RSDMResultsDataTable is None:
-    errorMess = 'Cannot display results. Ensure you have the RSDMResults dataTable in the project or you inserted the correct pid in the macro'
-    raise RuntimeError(errorMess)
-
-ss_sequence   =  'BBBBBCCCCBBBBBBCCCCHHHHHHHHHHHHHHCCCCCBBBBCCCCCBBBBBC'
-sequence  = 'KLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDAATKTFTVTE'
-
-
-data =  RSDMResultsDataTable.data
-x = data[sv.NMRRESIDUECODE]
-x = x.astype(int)
-x = x.values
-startSequenceCode = x[0]
-endSequenceCode = startSequenceCode + len(ss_sequence)
-startOffsetExtraXTicks = 3 # used to allign the ss to the plots
-xSequence = np.arange(startSequenceCode, endSequenceCode)
-
-R1 = data[sv.R1].values
-R2 = data[sv.R2].values
-NOE = data[sv.HETNOE_VALUE]
-
-R1_ERR = data[sv.R1_ERR]
-R2_ERR  = data[sv.R2_ERR]
-NOE_ERR = data[sv.HETNOE_VALUE_ERR]
-
-
-otherAnalysis, meanAnalysis = sdl._fitIsotropicModelFromT1T2NOE(data, spectrometerFrequency=600.130)
-S2 = otherAnalysis[sv.S2].values
-TE = otherAnalysis[sv.TE].values
-REX = otherAnalysis[sv.REX].values
-
-## lax labels
-L_JWN = '$J(\omega_{N})$'
-L_JWH = '$J(\omega_{H})$'
-L_SR = '(sec/rad)'
-
-blocks = createBlocksFromSequence(ss_sequence=ss_sequence)
-
-# graphics
-fontTitleSize = 8
-fontXSize = 6
-fontYSize = 6
-scatterFontSize = 5
-labelMajorSize=6
-labelMinorSize=5
-titleColor = 'blue'
-hspace= 0.5
-
-
-def _prettyFormat4Legend(value, rounding=3):
-    """ Format mantissa to (rounding) round  and exponent for matplotlib """
-    return '$%s^{%s}$' %(round(fMan(value),rounding),  fExp(value))
-
-
-def _closeFig(fig, pdf, plt):
-    pdf.savefig()
-    plt.close()
-    plt.close(fig)
 
 def _plotIsotropicAnalysisPage1(pdf):
     #  S2, Te, Tm, Rex
-
     fig = plt.figure(dpi=300)
     fig.suptitle('Isotropic Analysis Results', fontsize=10)
     axSe = plt.subplot(411)
     axTe = plt.subplot(412)
     axRex = plt.subplot(413)
     axss = fig.add_subplot(414)
-    plotSS(axss, xSequence, blocks, sequence, startSequenceCode=startSequenceCode, fontsize=5)
+    plotSS(axss, xSequence, sequence, ss_sequence=ss_sequence, startSequenceCode=startSequenceCode, fontsize=5,
+           showSequenceNumber=False, )
 
-    axSe.errorbar(x, S2, yerr=None, fmt="o", color='black', ms=2, ecolor='red', elinewidth=1, capsize=2, )
-    axTe.errorbar(x, TE, yerr=None, fmt="o", color='black', ms=2, ecolor='red', elinewidth=1, capsize=2, )
-    axRex.errorbar(x, REX, yerr=None, fmt="o", color='black', ms=2, ecolor='red', elinewidth=1, capsize=2, )
+    axSe.bar(x, S2, yerr=None, color=barColour, ecolor=barErrorColour, error_kw=dict(lw=barErrorLW, capsize=barErrorCapsize, capthick=barErrorCapthick))
+    axTe.bar(x, TE, yerr=None, color=barColour, ecolor=barErrorColour, error_kw=dict(lw=barErrorLW, capsize=barErrorCapsize, capthick=barErrorCapthick))
+    axRex.bar(x, REX, yerr=None, color=barColour, ecolor=barErrorColour, error_kw=dict(lw=barErrorLW, capsize=barErrorCapsize, capthick=barErrorCapthick))
 
-    axSe.set_ylim(ymin=0)
+    axSe.set_ylim(ymin=0, ymax=1)
     axTe.set_ylim(ymin=0)
     axRex.set_ylim(ymin=0)
 
@@ -158,14 +145,48 @@ def _plotIsotropicAnalysisPage1(pdf):
     bestGct = otherAnalysis.TM.values[0]
     plt.figtext(0.5, 0.01, f'Global Molecular Tumbling CorrelationTime Tm: {round(bestGct, 3)} ns',
                 ha="center", fontsize=6,)
-    _closeFig(fig, pdf, plt)
 
-# init pdf
-filePath = os.path.join(exportingDirectoryPath, exportingFileName)
-with PdfPages(f'{filePath}.pdf') as pdf:
-    _plotIsotropicAnalysisPage1(pdf)
+    pdf.savefig()
+    return fig
 
+###################      start inline macro       #####################
 
+## data preparation
+## get the various values  and perform the needed calculations
+dataTable = macrosLib._getDataTableForMacro(dataTableName)
+data =  dataTable.data
+x = data[sv.NMRRESIDUECODE]
+x = x.astype(int)
+x = x.values
+startSequenceCode = x[0]
+endSequenceCode = startSequenceCode + len(ss_sequence)
+xSequence = np.arange(startSequenceCode, endSequenceCode)
+R1 = data[sv.R1].values
+R2 = data[sv.R2].values
+NOE = data[sv.HETNOE_VALUE].values
+R1_ERR = data[sv.R1_ERR]
+R2_ERR  = data[sv.R2_ERR]
+NOE_ERR = data[sv.HETNOE_VALUE_ERR]
 
+otherAnalysis, meanAnalysis = sdl._fitIsotropicModelFromT1T2NOE(data, spectrometerFrequency=spectrometerFrequency)
+S2 = otherAnalysis[sv.S2].values
+TE = otherAnalysis[sv.TE].values
+REX = otherAnalysis[sv.REX].values
+
+##  init the plot and save to pdf
+filePath = macrosLib._getExportingPath(__file__, exportingFilePath)
+
+with PdfPages(filePath) as pdf:
+    fig1 = _plotIsotropicAnalysisPage1(pdf)
+    info(f'Report saved in {filePath}')
+
+if showInteractivePlot:
+    plt.show()
+else:
+    plt.close(fig1)
+
+application._showHtmlFile("Show Plots", filePath)
+
+###################      end macro        #########################
 
 

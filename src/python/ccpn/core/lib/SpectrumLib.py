126c051c45173d2bf620d1e99878ca2cd21603ff
Ed Brooksbank <ejb66@le.ac.uk>
2023-03-02 11:59:59 +0000

    raised max aliasing index limit to 6

--- src/python/ccpn/core/lib/SpectrumLib.py
+++ src/python/ccpn/core/lib/SpectrumLib.py
@@ -14,7 +14,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2023-03-02 11:05:22 +0000 (Thu, March 02, 2023) $"
+__dateModified__ = "$dateModified: 2023-03-02 11:59:59 +0000 (Thu, March 02, 2023) $"
 __version__ = "$Revision: 3.1.1 $"
 #=========================================================================================
 # Created
@@ -149,7 +149,7 @@ isotope2Tolerance = {
 SPECTRUM_POSITIONS = 'positions'
 SPECTRUM_INTENSITIES = 'intensities'
 
-MAXALIASINGRANGE = 3  # change to 6
+MAXALIASINGRANGE = 6
 
 
 def splitPseudo3DSpectrumIntoPlanes(spectrum, seriesUnits='ms'):

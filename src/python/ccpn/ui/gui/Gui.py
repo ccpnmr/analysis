ee71517ec634868eb036ef2e3eaeb1d3a137cf23
Luca Mureddu <luca.mureddu@me.com>
2023-03-31 13:27:32 +0100

    removed the too invasive message dialog on save

--- src/python/ccpn/ui/gui/Gui.py
+++ src/python/ccpn/ui/gui/Gui.py
@@ -14,8 +14,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2023-02-24 09:25:25 +0000 (Fri, February 24, 2023) $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2023-03-31 13:27:32 +0100 (Fri, March 31, 2023) $"
 __version__ = "$Revision: 3.1.1 $"
 #=========================================================================================
 # Created
@@ -674,7 +674,7 @@ class Gui(Ui):
                     return False
 
         successMessage = 'Project successfully saved to "%s"' % self.project.path
-        MessageDialog.showInfo("Project Save", successMessage, parent=self.mainWindow)
+        # MessageDialog.showInfo("Project Save", successMessage, parent=self.mainWindow) # This popup has been flagged as annoying by users
         self.mainWindow.statusBar().showMessage(successMessage)
         getLogger().info(successMessage)
 

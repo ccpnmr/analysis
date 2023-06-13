59e3a1947b3e04e5bbd15a165ca02160390f01ae
Luca Mureddu <luca.mureddu@me.com>
2023-06-13 11:55:48 +0100

    Bug fix for moving 1d Peaks.  after selecting a peak from label it didn't move!

--- src/python/ccpn/ui/gui/widgets/GLWidgets.py
+++ src/python/ccpn/ui/gui/widgets/GLWidgets.py
@@ -15,8 +15,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2023-06-09 18:15:52 +0100 (Fri, June 09, 2023) $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2023-06-13 11:55:48 +0100 (Tue, June 13, 2023) $"
 __version__ = "$Revision: 3.1.1 $"
 #=========================================================================================
 # Created
@@ -1552,8 +1552,8 @@ class Gui1dWidget(CcpnGLWidget):
                                         (minX < xPosition - xOffset < maxX and minY < yPosition - yOffset < maxY):
 
                                     peaks.append(peak)
-                                    if firstOnly:
-                                        return peaks if peak in self.current.peaks else []
+                                    # if firstOnly:
+                                    #     return peaks if peak in self.current.peaks else []
 
                             except Exception:
                                 # NOTE:ED - skip for now

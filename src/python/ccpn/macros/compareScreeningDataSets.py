08e982ef35c5ae54b8f51bdb536bf88dbd1e8c78
Luca Mureddu <luca.mureddu@me.com>
2023-06-23 16:04:23 +0100

    Fixed some synthax errors generated by the post-commits scrips

--- src/python/ccpn/macros/compareScreeningDataSets.py
+++ src/python/ccpn/macros/compareScreeningDataSets.py
@@ -3,26 +3,26 @@
 # General CCPN Licence, Reference and Credits
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 
 #=========================================================================================
 # Macro Created by:
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: VickyAH $"
-__dateModified__ = "$dateModified: 2022-05-17 12:23:33 +0100 (Tue, May 17, 2022) $"
-__version__ = "$Revision: 3.1.0 $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2023-06-23 16:04:23 +0100 (Fri, June 23, 2023) $"
+__version__ = "$Revision: 3.1.1 $"
 #=========================================================================================
 # Created
 #=========================================================================================
-__author__ = "$Author__author__      =   "$Author: Luca Mureddu $"
-__date__ = "$Date__date__        =   "$Date: 2021-06-09 14:02:46 +0100 (Wed, June 09, 2021) $"
+__author__ = "$Author__author__ =  "$Author: Luca Mureddu $"
+__date__ = "$Date__date__ =    "$Date: 2021-06-09 14:02:46 +0100 (Wed, June 09, 2021) $"
 #=========================================================================================
 # Start of code
 #=========================================================================================

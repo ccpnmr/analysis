450403ba91198a1c143db2c5fabb431ebd35466f
Luca Mureddu <luca.mureddu@me.com>
2023-07-07 12:19:18 +0100

    Bug fix for 3.2 env for Chembuild plugin

--- src/python/ccpn/ui/gui/widgets/CompoundView.py
+++ src/python/ccpn/ui/gui/widgets/CompoundView.py
@@ -5,18 +5,19 @@ Credits to Tim Stevens, University of Cambridge December 2010-2012
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
-__credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-03-08 16:27:02 +0000 (Mon, March 08, 2021) $"
-__version__ = "$Revision: 3.0.3 $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2023-07-07 12:19:19 +0100 (Fri, July 07, 2023) $"
+__version__ = "$Revision: 3.2.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -2991,7 +2992,7 @@ class BondItem(QtWidgets.QGraphicsItem):
                      QPointF(dx, dy))
 
         elif bondType == 'triple':
-            drawLine(0.0, 0.0, dx, dy)
+            drawLine(0, 0, int(dx), int(dy))
             xg *= 2.0
             yg *= 2.0
             drawLine(QPointF(xg, yg),

058511bab79b5caebedd0d9251fb13bc06dd311b
Geerten Vuister <gv29@leicester.ac.uk>
2023-07-25 09:38:23 +0100

    Commit after merge of 3.2.0-edge and 3.1.1-edge_gwv5

--- src/python/ccpn/ui/gui/widgets/TestModule.py
+++ src/python/ccpn/ui/gui/widgets/TestModule.py
@@ -1,18 +1,19 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
-__credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-02-04 12:07:39 +0000 (Thu, February 04, 2021) $"
-__version__ = "$Revision: 3.0.3 $"
+__modifiedBy__ = "$modifiedBy: Geerten Vuister $"
+__dateModified__ = "$dateModified: 2023-07-25 09:38:23 +0100 (Tue, July 25, 2023) $"
+__version__ = "$Revision: 3.2.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -22,7 +23,7 @@ __date__ = "$Date: 2017-04-07 10:28:41 +0000 (Fri, April 07, 2017) $"
 # Start of code
 #=========================================================================================
 
-
+from ccpn import core
 from ccpn.ui.gui.modules.CcpnModule import CcpnModule
 from ccpn.ui.gui.widgets.Label import Label
 
@@ -33,7 +34,7 @@ class MyModule(CcpnModule):
     """
     includeSettingsWidget = True
     maxSettingsState = 2
-    settingsPosition = 'top'
+    settingsPosition = 'left'
 
     className = 'MyModule'
 
@@ -44,8 +45,11 @@ class MyModule(CcpnModule):
         self.aLabel = Label(parent=self.mainWidget, text='Testing my module', grid=(0, 0))
 
         # settingsWidget
-        self.testingLabel = Label(parent=self.settingsWidget, text='Testing my settings space', grid=(0, 0), hAlign='c')
-
+        row = 0
+        self.testingLabel = Label(parent=self.settingsWidget, text='Testing my settings space', grid=(row, 0), hAlign='c')
+        row += 1
+        cb = EditableCheckBox(self.settingsWidget, text='HELLO', checked=False, grid=(row, 0))
+        cb.setChecked(True)
 
 '''
 To test a ccpn Module graphic :
@@ -68,11 +72,9 @@ if __name__ == '__main__':
     moduleArea = CcpnModuleArea(mainWindow=None)
 
     module = MyModule(mainWindow=None, name='My Module')
-    cb = EditableCheckBox(module.settingsWidget, text='HELLO', checked=False, grid=(0, 0))
 
-    state = module.widgetsState
-    cb.setChecked(True)
-    module.restoreWidgetsState(**state)
+    # state = module.widgetsState
+    # module.restoreWidgetsState(**state)
 
     moduleArea.addModule(module)
 

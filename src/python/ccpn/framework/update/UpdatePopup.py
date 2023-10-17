ca0b43839dd06377f24efa8fae12183e951a2d79
Ed Brooksbank <ejb66@le.ac.uk>
2023-10-17 15:51:21 +0100

    strange duplicated 'main'

--- src/python/ccpn/framework/update/UpdatePopup.py
+++ src/python/ccpn/framework/update/UpdatePopup.py
@@ -4,7 +4,7 @@ Module Documentation Here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2022"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
@@ -15,8 +15,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2022-10-27 18:13:14 +0100 (Thu, October 27, 2022) $"
-__version__ = "$Revision: 3.1.0 $"
+__dateModified__ = "$dateModified: 2023-10-17 15:51:22 +0100 (Tue, October 17, 2023) $"
+__version__ = "$Revision: 3.2.0 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -254,10 +254,6 @@ def main():
     popup.exec_()
 
     # os._exit(qtApp.exec_()) - not required with exec_
-
-
-if __name__ == '__main__':
-    main()
 
 
 if __name__ == '__main__':

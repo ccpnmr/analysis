d77f995f78e782ca3d8ff71fa0e5e778f12dcd2b
Daniel Thompson <dt264@le.ac.uk>
2023-11-24 17:03:41 +0000

    Restructure and increase test coverage.

--- src/python/ccpn/core/testing/Test_IntegralList.py
+++ src/python/ccpn/core/testing/Test_IntegralList.py
@@ -4,18 +4,19 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
-__credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2021-04-14 19:56:58 +0100 (Wed, April 14, 2021) $"
-__version__ = "$Revision: 3.0.3 $"
+__modifiedBy__ = "$modifiedBy: Daniel Thompson $"
+__dateModified__ = "$dateModified: 2023-11-24 17:03:42 +0000 (Fri, November 24, 2023) $"
+__version__ = "$Revision: 3.2.1 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -36,24 +37,25 @@ class IntegralListTest(WrapperTesting):
     def setUp(self):
         with self.initialSetup():
             self.spectrum = self.project.newEmptySpectrum(isotopeCodes=('1H',), name='H1D-tst')
+            self.integralList = self.spectrum.newIntegralList()
 
     def test_new1dIntegralList(self):
-        self.assertEqual(len(self.spectrum.integralLists), 0)
-
-        integralList = self.spectrum.newIntegralList()
-
         self.assertEqual(len(self.spectrum.integralLists), 1)
-        self.assertEqual(integralList.className, 'IntegralList')
-        self.assertIs(self.spectrum.integralLists[0], integralList)
+        new_integralList = self.spectrum.newIntegralList(comment='test comment')
+
+        self.assertIs(self.spectrum.integralLists[1], new_integralList)
+        self.assertEqual(len(self.spectrum.integralLists), 2)
+
+        self.assertEqual('test comment', new_integralList.comment)
+        self.assertIn('IL', new_integralList.pid)
+        self.assertEqual(new_integralList.className, 'IntegralList')
+
 
     def test_new1dIntegralList_UndoRedo(self):
-        integralList = self.spectrum.newIntegralList()
-
         self.assertEqual(len(self.spectrum.integralLists), 1)
         self.undo.undo()
         self.assertEqual(len(self.spectrum.integralLists), 0)
 
         self.undo.redo()
         self.assertEqual(len(self.spectrum.integralLists), 1)
-        self.assertIs(self.spectrum.integralLists[0], integralList)
-
+        self.assertIs(self.spectrum.integralLists[0], self.integralList)

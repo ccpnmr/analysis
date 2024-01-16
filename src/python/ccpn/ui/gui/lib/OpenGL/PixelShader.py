790dd4f897f96f7af6a87bad84a8c885f20a3a54
Ed Brooksbank <ejb66@le.ac.uk>
2024-01-16 17:10:44 +0000

    minor change to make attributes and uniforms consistent

--- src/python/ccpn/ui/gui/lib/OpenGL/PixelShader.py
+++ src/python/ccpn/ui/gui/lib/OpenGL/PixelShader.py
@@ -4,7 +4,7 @@ Module Documentation here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
 __credits__ = ("Ed Brooksbank, Joanna Fox, Morgan Hayward, Victoria A Higman, Luca Mureddu",
                "Eliza Płoskoń, Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
@@ -15,7 +15,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2023-12-20 15:19:07 +0000 (Wed, December 20, 2023) $"
+__dateModified__ = "$dateModified: 2024-01-16 17:10:45 +0000 (Tue, January 16, 2024) $"
 __version__ = "$Revision: 3.2.1 $"
 #=========================================================================================
 # Created
@@ -156,9 +156,9 @@ class AliasedPixelShader(PixelShader):
     _ALIASENABLED = 'aliasEnabled'
 
     # attribute/uniform lists for shader
-    attributes = {_ALIAS: (1, np.float32)}
+    attributes = dict(PixelShader.attributes)
+    attributes |= {_ALIAS: (1, np.float32)}
     uniforms = dict(PixelShader.uniforms)
-
     uniforms |= {_ALIASPOSITION: (1, np.float32),  # change to a set?
                  _BACKGROUND   : (4, np.float32),
                  _ALIASSHADE   : (1, np.float32),
@@ -183,6 +183,8 @@ class AliasedPixelShader(PixelShader):
             gl_Position = pMatrix * mvMatrix * vec4(gl_Vertex.xy, 0.0, 1.0);
             fragCol = gl_Color;
             aliased = (aliasPosition - gl_Vertex.z);
+
+            viewport;
         }
         """
 

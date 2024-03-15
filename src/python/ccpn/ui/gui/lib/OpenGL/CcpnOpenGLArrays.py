07f05702a716753bbb157536f06799e6fc7302ee
Ed Brooksbank <ejb66@le.ac.uk>
2024-03-15 12:57:12 +0000

    comments

--- src/python/ccpn/ui/gui/lib/OpenGL/CcpnOpenGLArrays.py
+++ src/python/ccpn/ui/gui/lib/OpenGL/CcpnOpenGLArrays.py
@@ -15,7 +15,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-03-15 12:53:36 +0000 (Fri, March 15, 2024) $"
+__dateModified__ = "$dateModified: 2024-03-15 12:57:12 +0000 (Fri, March 15, 2024) $"
 __version__ = "$Revision: 3.2.2 $"
 #=========================================================================================
 # Created
@@ -1491,7 +1491,8 @@ class _GLVertexArray:
 
 
 #=========================================================================================
-# Windows again - doesn't appear to correctly accelerate all GL objects
+# Windows - doesn't appear to correctly accelerates gl-VBO classes
+# Redhat  - crashes on None/VOIDPTR(0) in standard calls :|
 #=========================================================================================
 
 if isRHEL(version = None):  # check for all RHEL versions

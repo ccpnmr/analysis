ddf527603b00236ebab72c89539b0bb6effaae50
Ed Brooksbank <ejb66@le.ac.uk>
2024-03-15 12:54:03 +0000

    changed to an int

--- src/python/ccpn/util/Common.py
+++ src/python/ccpn/util/Common.py
@@ -20,7 +20,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-03-15 12:53:36 +0000 (Fri, March 15, 2024) $"
+__dateModified__ = "$dateModified: 2024-03-15 12:54:03 +0000 (Fri, March 15, 2024) $"
 __version__ = "$Revision: 3.2.2 $"
 #=========================================================================================
 # Created
@@ -246,7 +246,7 @@ def isUbuntuVersion(value: str = ''):
     return False
 
 
-def isRHEL(version: int | None = '8'):
+def isRHEL(version: int | None = 8):
     import csv
     import pathlib
 

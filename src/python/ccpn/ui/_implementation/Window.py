f4738d5ebd85bca6fde3cf4a8581b559fe3b340b
Daniel Thompson <dt264@le.ac.uk>
2024-04-30 14:39:14 +0100

    Fix opening wrong file bug by avoiding restoring widget when not wanted.

--- src/python/ccpn/ui/_implementation/Window.py
+++ src/python/ccpn/ui/_implementation/Window.py
@@ -4,9 +4,9 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
-__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
-               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
+__credits__ = ("Ed Brooksbank, Joanna Fox, Morgan Hayward, Victoria A Higman, Luca Mureddu",
+               "Eliza Płoskoń, Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
@@ -14,9 +14,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2023-08-07 16:56:15 +0100 (Mon, August 07, 2023) $"
-__version__ = "$Revision: 3.2.0 $"
+__modifiedBy__ = "$modifiedBy: Daniel Thompson $"
+__dateModified__ = "$dateModified: 2024-04-30 14:39:15 +0100 (Tue, April 30, 2024) $"
+__version__ = "$Revision: 3.2.1 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -322,7 +322,7 @@ class Window(AbstractWrapperObject):
         from ccpn.ui.gui.modules.MacroEditor import MacroEditor
 
         path = str(path) if path is not None else None
-        macroEditor = MacroEditor(mainWindow=self, filePath=path)
+        macroEditor = MacroEditor(mainWindow=self, filePath=path, restore=False)
         self.moduleArea.addModule(macroEditor, position=position, relativeTo=relativeTo)
         return macroEditor
 

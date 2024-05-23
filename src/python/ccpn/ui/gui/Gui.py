eb04c9b78387b40d32e4e68aa3bea1871c76429f
Ed Brooksbank <ejb66@le.ac.uk>
2024-05-23 14:17:11 +0100

    new projects are now always called 'newProject'

--- src/python/ccpn/ui/gui/Gui.py
+++ src/python/ccpn/ui/gui/Gui.py
@@ -5,8 +5,9 @@ The top-level Gui class for all user interactions
 # Licence, Reference and Credits
 #=========================================================================================
 __copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
-__credits__ = ("Ed Brooksbank, Joanna Fox, Morgan Hayward, Victoria A Higman, Luca Mureddu",
-               "Eliza Płoskoń, Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Daniel Thompson",
+               "Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
@@ -15,7 +16,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-05-10 17:11:57 +0100 (Fri, May 10, 2024) $"
+__dateModified__ = "$dateModified: 2024-05-23 14:17:11 +0100 (Thu, May 23, 2024) $"
 __version__ = "$Revision: 3.2.5 $"
 #=========================================================================================
 # Created
@@ -755,7 +756,7 @@ class Gui(Ui, _Gui):
     #-----------------------------------------------------------------------------------------
 
     @logCommand('application.')
-    def newProject(self, name: str = 'default') -> (Project, None):
+    def newProject(self, name: str = 'newProject') -> (Project, None):
         """Create a new project instance with name; create default project if name=None
         :return a Project instance or None
         """

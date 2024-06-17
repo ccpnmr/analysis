19267e17fd3eac243b218fd2e3ce6af7cf2ca088
Daniel Thompson <dt264@le.ac.uk>
2024-06-17 10:31:37 +0100

    Fix NmrAtom unique name after changes to standardise (git: d267ddaf).

--- src/python/ccpn/core/NmrAtom.py
+++ src/python/ccpn/core/NmrAtom.py
@@ -14,8 +14,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-05-30 13:45:36 +0100 (Thu, May 30, 2024) $"
+__modifiedBy__ = "$modifiedBy: Daniel Thompson $"
+__dateModified__ = "$dateModified: 2024-06-17 10:31:38 +0100 (Mon, June 17, 2024) $"
 __version__ = "$Revision: 3.2.3 $"
 #=========================================================================================
 # Created
@@ -669,19 +669,19 @@ def _newNmrAtom(self: NmrResidue, name: str = None, isotopeCode: str = None, com
     if not isinstance(comment, (str, type(None))):
         raise TypeError(f'Comment {comment} must be of type string (or None)')
 
-    # if not name:
-    #     # generate (temporary) default name, to be changed later after we created the object
-    #     _name = NmrAtom._uniqueName(self.project)
-    #
-    # else:
-    #     # Check for name clashes
-    #     _name = name
-    #     previous = self.getNmrAtom(_name.translate(Pid.remapSeparators))
-    #     if previous is not None:
-    #         raise ValueError(f'newNmrAtom: name {_name!r} clashes with {previous}')
+    if not name:
+        # generate (temporary) default name, to be changed later after we created the object
+        _name = NmrAtom._uniqueName(self.project)
 
-    # ensure uniqueName
-    _name = NmrAtom._uniqueName(self, name=name)
+    else:
+        # Check for name clashes
+        _name = name
+        previous = self.getNmrAtom(_name.translate(Pid.remapSeparators))
+        if previous is not None:
+            raise ValueError(f'newNmrAtom: name {_name!r} clashes with {previous}')
+
+    # # ensure uniqueName
+    # _name = NmrAtom._uniqueName(self, name=name)
 
     # Create the api object
     # Always create first with unknown isotopeCode

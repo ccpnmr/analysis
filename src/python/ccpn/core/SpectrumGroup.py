bf4a2405ca48bf512c4c724c3d6482a0e085a40c
Ed Brooksbank <ejb66@le.ac.uk>
2024-07-02 14:41:02 +0100

    sidebar bug

--- src/python/ccpn/core/SpectrumGroup.py
+++ src/python/ccpn/core/SpectrumGroup.py
@@ -3,9 +3,10 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
-__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
-               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
+__credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Daniel Thompson",
+               "Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
@@ -14,8 +15,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2023-10-10 17:49:14 +0100 (Tue, October 10, 2023) $"
-__version__ = "$Revision: 3.2.0 $"
+__dateModified__ = "$dateModified: 2024-07-02 14:41:03 +0100 (Tue, July 02, 2024) $"
+__version__ = "$Revision: 3.2.2.1 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -105,13 +106,12 @@ class SpectrumGroup(AbstractWrapperObject):
         """ CCPN Project SpectrumGroup"""
         return self._wrappedData
 
-    def _getSpectrumGroupChildrenByClass(self, klass):
+    def _getSpectrumGroupChildrenByClass(self, klass) -> tuple | list:
         """Return the list of spectra attached to the spectrumGroup.
         """
-        if klass is Spectrum:
+        if klass is Spectrum and not self.isDeleted:
             return tuple(spectrum for spectrum in self.spectra)
-        else:
-            return []
+        return []
 
     @property
     def _key(self) -> str:

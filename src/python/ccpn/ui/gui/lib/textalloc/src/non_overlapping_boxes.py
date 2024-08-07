4ba1dcf3d8c277d940cb88763a5d21e1d5f226e3
Ed Brooksbank <ejb66@le.ac.uk>
2024-08-07 16:25:09 +0100

    updated licencing information for textalloc

--- src/python/ccpn/ui/gui/lib/textalloc/src/non_overlapping_boxes.py
+++ src/python/ccpn/ui/gui/lib/textalloc/src/non_overlapping_boxes.py
@@ -2,10 +2,33 @@
 Routines to align text-boxes.
 Moves positions to give non-overlapping boxes/rectangles/ellipses and connecting arrows.
 
-Modified again by Ed Brooksbank
-Expanded from the original by Christoffer Kjellson.
-Original available from github; https://github.com/ckjellson/textalloc
+Expanded from the original.
+Original by Christoffer Kjellson.
+Available from github; https://github.com/ckjellson/textalloc
 """
+
+# MIT License
+
+# Copyright (c) 2022 Christoffer Kjellson
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
@@ -21,12 +44,12 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-07-04 18:52:00 +0100 (Thu, July 04, 2024) $"
+__dateModified__ = "$dateModified: 2024-08-07 16:25:10 +0100 (Wed, August 07, 2024) $"
 __version__ = "$Revision: 3.2.5 $"
 #=========================================================================================
 # Created
 #=========================================================================================
-__author__ = "$Author: Ed Brooksbank $"
+__author__ = "$Author: Original by Christoffer Kjellson $"
 __date__ = "$Date: 2022 $"
 
 #=========================================================================================

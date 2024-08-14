ad1b2e31a1ee63ff9bfdf087b98920ae49701344
Geerten Vuister <gv29@leicester.ac.uk>
2024-08-14 18:58:50 +0100

    Menu: annotations

--- src/python/ccpn/ui/gui/Menus.py
+++ src/python/ccpn/ui/gui/Menus.py
@@ -5,11 +5,6 @@
       signature:
         Action(name, callback, checkEnabled=None, **options)
 
-      deprecated tuple-based signature:
-        (name, callable) tuple or
-        (name, callable, options) tuple or
-        (name, callable, options, checkEnabled) tuple
-
       options is a dict of (option, value) pairs.
       Valid options (from Action widget):
             :param shortcut: optional two letter shortcut
@@ -118,7 +113,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Geerten Vuister $"
-__dateModified__ = "$dateModified: 2024-08-14 18:44:18 +0100 (Wed, August 14, 2024) $"
+__dateModified__ = "$dateModified: 2024-08-14 18:58:50 +0100 (Wed, August 14, 2024) $"
 __version__ = "$Revision: 3.2.5 $"
 #=========================================================================================
 # Created

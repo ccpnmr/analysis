25a8a472c5fde755c169c627857308328d0c3008
Geerten Vuister <gv29@leicester.ac.uk>
2024-09-05 14:06:05 +0200

    Bugfix in case of undefined spectrum.dataSource

--- src/python/ccpn/ui/gui/popups/SpectrumProjectionPopup.py
+++ src/python/ccpn/ui/gui/popups/SpectrumProjectionPopup.py
@@ -5,8 +5,9 @@ Module Documentation here
 # Licence, Reference and Credits
 #=========================================================================================
 __copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
-__credits__ = ("Ed Brooksbank, Joanna Fox, Morgan Hayward, Victoria A Higman, Luca Mureddu",
-               "Eliza Płoskoń, Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Daniel Thompson",
+               "Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
@@ -14,9 +15,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-04-18 14:07:53 +0100 (Thu, April 18, 2024) $"
-__version__ = "$Revision: 3.2.4 $"
+__modifiedBy__ = "$modifiedBy: Geerten Vuister $"
+__dateModified__ = "$dateModified: 2024-09-05 14:06:05 +0200 (Thu, September 05, 2024) $"
+__version__ = "$Revision: 3.2.5 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -146,10 +147,14 @@ class SpectrumProjectionPopup(CcpnDialogWithOutputPathPopupABC):  # ExportDialog
         super(SpectrumProjectionPopup, self).populate(userFrame)
 
     def getInfoString(self) -> str:
-        """Return a string for the info widget field
+        """Return a string for the info widget field or empty string
+        if no valid path is defined for spectrum.
         Should be subclassed
         """
-        return self.spectrum.dataSource._fileInfoString2
+        if self.spectrum.hasValidPath():
+            return self.spectrum.dataSource._fileInfoString2
+        else:
+            return ''
 
     def getName(self) -> str:
         """Return a string for the name of the file

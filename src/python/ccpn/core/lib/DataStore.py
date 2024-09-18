90a22a87c03691537091deb5317618338df4f8c3
Geerten Vuister <gv29@leicester.ac.uk>
2024-09-18 16:32:45 +0100

    ANalysisv4 compatibility

--- src/python/ccpn/core/lib/DataStore.py
+++ src/python/ccpn/core/lib/DataStore.py
@@ -14,8 +14,9 @@ Replaced:
 # Licence, Reference and Credits
 #=========================================================================================
 __copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
-__credits__ = ("Ed Brooksbank, Joanna Fox, Morgan Hayward, Victoria A Higman, Luca Mureddu",
-               "Eliza Płoskoń, Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Daniel Thompson",
+               "Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
@@ -23,9 +24,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-04-18 14:07:47 +0100 (Thu, April 18, 2024) $"
-__version__ = "$Revision: 3.2.4 $"
+__modifiedBy__ = "$modifiedBy: Geerten Vuister $"
+__dateModified__ = "$dateModified: 2024-09-18 16:32:46 +0100 (Wed, September 18, 2024) $"
+__version__ = "$Revision: 3.2.5 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -465,6 +466,7 @@ class DataStore(CcpNmrJson):
     def _getPathRedirections(self) -> dict:
         """Get the redirection paths;
         :return the pathRedirection dict
+        #CCPNINTERNAL: V4 Spectrum._postRestore
         """
         redirections = PathRedirections().getPaths()
         for redirection, path in redirections.items():
--- src/python/ccpn/ui/gui/lib/TipOfTheDayManager.py
+++ src/python/ccpn/ui/gui/lib/TipOfTheDayManager.py
@@ -17,7 +17,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Geerten Vuister $"
-__dateModified__ = "$dateModified: 2024-09-18 14:14:00 +0100 (Wed, September 18, 2024) $"
+__dateModified__ = "$dateModified: 2024-09-18 16:32:46 +0100 (Wed, September 18, 2024) $"
 __version__ = "$Revision: 3.2.5 $"
 #=========================================================================================
 # Created
@@ -82,7 +82,7 @@ class TipOfTheDayManager():
 
     def start(self):
         if self.show_tips:
-            self._initial_show_timer = QTimer(parent=self.gui.mainWindow)
+            self._initial_show_timer = QTimer(parent=self.gui.mainWindow._widget)
             self._initial_show_timer.timeout.connect(self._startupDisplayTipOfTheDayCallback)
             self._initial_show_timer.setInterval(0)
             self._initial_show_timer.start()

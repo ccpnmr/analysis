ca7a1cd818db4f009d16f4bfbdb432c95db9d9d5
Ed Brooksbank <ejb66@le.ac.uk>
2024-09-19 13:57:50 +0100

    minor clean-up

--- src/python/ccpn/ui/gui/lib/SpectrumView.py
+++ src/python/ccpn/ui/gui/lib/SpectrumView.py
@@ -4,9 +4,10 @@ This file contains the SpectrumView classes (1D and nD versions)
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
-__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
-               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
+__credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Daniel Thompson",
+               "Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
@@ -14,9 +15,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Geerten Vuister $"
-__dateModified__ = "$dateModified: 2023-02-02 13:48:27 +0000 (Thu, February 02, 2023) $"
-__version__ = "$Revision: 3.1.1 $"
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2024-09-19 13:57:50 +0100 (Thu, September 19, 2024) $"
+__version__ = "$Revision: 3.2.7 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -43,7 +44,6 @@ class SpectrumView(_CoreClassSpectrumView):
         if 'intensity' in apiObj.strip.spectrumDisplay.axisCodes:
             # 1D display
             return SpectrumView1d(project, apiObj)
-
         else:
             # ND display
             return SpectrumViewNd(project, apiObj)
@@ -79,23 +79,3 @@ class SpectrumViewNd(SpectrumView, _GuiSpectrumViewNd):
 
         getLogger().debug('SpectrumViewNd>> self=%s strip=%s' % (self, self.strip))
         _GuiSpectrumViewNd.__init__(self)
-
-
-# #=========================================================================================
-# # For Registering
-# #=========================================================================================
-#
-#
-#
-# def _factoryFunction(project: Project, wrappedData):
-#     """create SpectrumView, dispatching to subtype depending on wrappedData
-#     """
-#     if 'intensity' in wrappedData.strip.spectrumDisplay.axisCodes:
-#         # 1D display
-#         return SpectrumView1d(project, wrappedData)
-#     else:
-#         # ND display
-#         return SpectrumViewNd(project, wrappedData)
-#
-#
-# # _CoreClassSpectrumView._registerCoreClass(factoryFunction=_factoryFunction)

8c66c903db1b5ee5b381f0e4c13859abf65f872e
Ed Brooksbank <ejb66@le.ac.uk>
2024-10-22 15:38:13 +0100

    small bug for numpy numbers in the table

--- src/python/ccpn/util/floatUtils.py
+++ src/python/ccpn/util/floatUtils.py
@@ -17,9 +17,10 @@ Solution taken from: https://stackoverflow.com/questions/45332056/decompose-a-fl
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
-__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
-               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
+__credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Daniel Thompson",
+               "Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
@@ -27,9 +28,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
-__dateModified__ = "$dateModified: 2023-05-22 11:50:14 +0100 (Mon, May 22, 2023) $"
-__version__ = "$Revision: 3.1.1 $"
+__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
+__dateModified__ = "$dateModified: 2024-10-22 15:38:13 +0100 (Tue, October 22, 2024) $"
+__version__ = "$Revision: 3.2.7 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -65,4 +66,4 @@ def fRound(number) -> float:
 
 def numZeros(decimal):
     """ For floats less the 1, Count the number of zeros after the . """
-    return math.inf if decimal == 0 else -math.floor(math.log10(abs(decimal))) - 1
+    return 1 if decimal == 0 else -math.floor(math.log10(abs(decimal))) - 1

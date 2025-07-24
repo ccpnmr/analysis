f6deedf91f81c18eae3c25ffa8e1fd12549ce430
Ed Brooksbank <ejb66@le.ac.uk>
2025-07-24 13:56:59 +0100

    update version

--- src/python/ccpn/framework/Version.py
+++ src/python/ccpn/framework/Version.py
@@ -19,8 +19,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2025-05-12 16:10:49 +0100 (Mon, May 12, 2025) $"
-__version__ = "$Revision: 3.3.2.1 $"
+__dateModified__ = "$dateModified: 2025-07-24 13:56:59 +0100 (Thu, July 24, 2025) $"
+__version__ = "$Revision: 3.3.2.2 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -299,8 +299,8 @@ class VersionString(str):
 # - previous is included as a reference (not currently used)
 #=========================================================================================
 
-_previousApplicationVersion = VersionString('3.3.2')
-applicationVersion = VersionString('3.3.2.1')
+_previousApplicationVersion = VersionString('3.3.2.1')
+applicationVersion = VersionString('3.3.2.2')
 _lastApplicationVersion = VersionString('4.0.0')  # hide any messages beyond this point
 revision = '3'
 

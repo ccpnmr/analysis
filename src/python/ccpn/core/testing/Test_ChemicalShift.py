68a18e782492f88c86245268999d858007a08cde
Ed Brooksbank <ejb66@le.ac.uk>
2025-09-08 15:54:40 +0100

    tweak: to test case

--- src/python/ccpn/core/testing/Test_ChemicalShift.py
+++ src/python/ccpn/core/testing/Test_ChemicalShift.py
@@ -4,19 +4,20 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
-__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
-               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2025"
+__credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Daniel Thompson",
+               "Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-11-01 11:20:56 +0000 (Mon, November 01, 2021) $"
-__version__ = "$Revision: 3.0.4 $"
+__dateModified__ = "$dateModified: 2025-09-08 15:54:40 +0100 (Mon, September 08, 2025) $"
+__version__ = "$Revision: 3.3.2.3 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -141,14 +142,17 @@ class ChemicalShiftTestNew(WrapperTesting):
         self.undo.undo()
         self.assertEqual(sh.nmrAtom, None)
         self.undo.redo()
+        self.assertEqual(sh.nmrAtom, nmrAtom)
 
         with self.assertRaisesRegex(ValueError, 'must be of type NmrAtom'):
             sh.nmrAtom = 42
         for atr in (CS_CHAINCODE, CS_SEQUENCECODE, CS_RESIDUETYPE, CS_ATOMNAME):
             with self.assertRaisesRegex(ValueError, 'must be of type str or None'):
                 setattr(sh, atr, 42)
-            with self.assertRaisesRegex(RuntimeError, 'cannot modify'):
-                setattr(sh, atr, None)
+            if getattr(sh, atr, '_Undefined_') != None:
+                # setters are skipped of the value hasn't changed; hence no raised error
+                with self.assertRaisesRegex(RuntimeError, 'cannot modify'):
+                    setattr(sh, atr, None)
 
         for atr in (CS_NMRATOM, CS_CHAINCODE, CS_SEQUENCECODE, CS_RESIDUETYPE, CS_ATOMNAME):
             setattr(sh, atr, None)

12282d20db0dad968318e98ea19760e1bb2a3a23
Luca Mureddu <luca.mureddu@me.com>
2025-09-10 10:57:13 +0100

    Updated macro extractSliceAtPeakPosition to save spectra on the project directory

--- src/python/ccpn/macros/extractSliceAtPeakPosition.py
+++ src/python/ccpn/macros/extractSliceAtPeakPosition.py
@@ -6,23 +6,29 @@ Select a peak, Run the macro.
 
 import numpy as np
 from ccpn.core.lib.ContextManagers import undoBlockWithSideBar
+from ccpn.util.Path import fetchDir, joinPath, aPath
+from ccpn.core.Spectrum import _extractRegionToFile
+
+## ~ New spectrum  name formatting
+decimalSeparator = '-' # replace the . (dot) with an underscore. Specify here an alternative. Not Allowed: . % $ @ ! | \ ' ; : ? ~ `
+ppmRounding = 2
+namesSeparator = '_'
 
 
 peak = current.peak
 spectrum = peak.spectrum
+if project.spectraPath is None:
+    raise RuntimeError('You need to set your project path Directory in Preferences.')
+projectSpectraPath = project.spectraPath
 
 for dim in spectrum.dimensions:
     pointPositions = np.array(peak.pointPositions, dtype=int)
-    data = spectrum.getSliceData(position=list(pointPositions), sliceDim=dim)
-    isotopeCodes = spectrum.getByDimensions('isotopeCodes',dimensions=[dim])
-    pointCounts = spectrum.getByDimensions('pointCounts', dimensions=[dim])
-    dd = {}
-    properties = ['spectralWidths', 'spectralWidthsHz', 'spectrometerFrequencies', 'referencePoints', 'referenceValues']
-    for p in properties:
-        dd[p] = spectrum.getByDimensions(p, [dim])
+    ppmPositionsStr = f'{namesSeparator}'.join([f'{i:.{ppmRounding}f}'.replace('.', decimalSeparator) for i in peak.ppmPositions])
+    axisCodes = spectrum.getByDimensions('axisCodes',dimensions=[dim])
     with undoBlockWithSideBar():
         # set everything as a single undo-operation
-        sp = project.newEmptySpectrum(name=f'{spectrum.name}-slice-{dim}',isotopeCodes=isotopeCodes, pointCounts=pointCounts, **dd)
+        spectrumName = f'{spectrum.name}{namesSeparator}{axisCodes[0]}{namesSeparator}[{ppmPositionsStr}_ppm]'
+        savingPath = aPath(joinPath(project.spectraPath, spectrumName, ))
+        savingPath = savingPath.assureSuffix('ndf5')
+        sp = spectrum.extractSliceToFile(axisCodes[0], pointPositions, path=savingPath)
         sp.noiseLevel = spectrum.noiseLevel
-        sp.setBuffering(True)
-        sp.setSliceData(data, )

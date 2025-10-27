ac990c48ea617907d871ea5fc6e7a320edc8e269
Morgan Hayward <mh653@leicester.ac.uk>
2025-10-27 23:06:19 +0000

    Created an official macro for importing csv, json or xlsx files into DataTables, using a file dialog.

---
+++ src/python/ccpn/macros/importDataTable.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from ccpn.ui.gui.widgets.FileDialog import TablesFileDialog
+from pathlib import Path
+from ccpn.ui.gui.widgets.MessageDialog import showWarning, showInfo
+
+
+dialog = TablesFileDialog()
+dialog.exec_()
+path = Path(dialog.selectedFile())
+stem = path.stem
+dfs = {}
+while dialog.selectedFile() is not None and path.suffix not in ['.csv', '.json', '.xlsx']:
+    showWarning('Incompatible File',
+                'Please choose a csv, json or xlsx file.')
+    dialog.exec_()
+    path = Path(dialog.selectedFile())
+if path.suffix == '.csv':
+    dfs = {stem: pd.read_csv(path)}
+elif path.suffix == '.json':
+    dfs = {stem: pd.read_json(path)}
+elif path.suffix == '.xlsx':
+    dfs = pd.read_excel(path, sheet_name=None)
+
+for key, value in dfs.items():
+    key = key.replace(' ', '_')
+    project.newDataTable(name=key, data=value)
